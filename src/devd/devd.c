5b776e206ef6014d1491cfe8157372c3aea5ee17
BigfootACA <bigfoot@classfun.cn>
2021-05-14 09:29:07 +0800

    devd: devd.c: log message when modules load or unload

--- src/devd/devd.c
+++ src/devd/devd.c
@@ -7,14 +7,27 @@
 #include"system.h"
 #include"defines.h"
 #include"devd.h"
+#include"init.h"
 #define TAG "devd"
 
 static int devdfd=-1;
+
+int process_module(uevent*event){
+	if(!event->devpath)return -1;
+	char*mod=strchr(event->devpath+1,'/')+1;
+	switch(event->action){
+		case ACTION_ADD:tlog_debug("loaded module '%s'",mod);break;
+		case ACTION_REMOVE:tlog_debug("unloaded module '%s'",mod);break;
+		default:break;
+	}
+	return 0;
+}
 
 int process_uevent(){
 	uevent event;
 	uevent_parse_x(environ,&event);
 	if(strcmp(event.subsystem,"firmware")==0)process_firmware_load(&event);
+	if(strcmp(event.subsystem,"module")==0)process_module(&event);
 	if(event.major>=0&&event.minor>=0)process_new_node(devdfd,&event);
 	#ifdef ENABLE_KMOD
 	if(event.modalias)insmod(event.modalias,false);

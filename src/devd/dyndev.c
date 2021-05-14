74808eedf0357e76881c8fe612953e422096f1b1
BigfootACA <bigfoot@classfun.cn>
2021-05-14 23:33:27 +0800

    devd: dyndev.c: use simple logger functions

--- src/devd/dyndev.c
+++ src/devd/dyndev.c
@@ -18,17 +18,8 @@ int process_new_node(int devdfd,uevent*event){
 		case ACTION_REMOVE:
 			if(faccessat(devdfd,event->devname,F_OK,0)!=0)return -errno;
 			if(unlinkat(devdfd,event->devname,0)!=0)
-				return return_logger_printf_warning(
-					-errno,TAG,
-					"unlink %s/%s: %m",
-					_PATH_DEV,
-					event->devname
-				);
-			return return_logger_printf_debug(
-				0,TAG,
-				"remove device %s/%s",
-				_PATH_DEV,event->devname
-			);
+				return terlog_warn(-errno,"unlink %s/%s",_PATH_DEV,event->devname);
+			return trlog_debug(0,"remove device %s/%s",_PATH_DEV,event->devname);
 		default:return -2;
 	}
 }

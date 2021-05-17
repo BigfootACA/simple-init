344e0a393c678e6c0651285b0b8fd3fe31e110df
BigfootACA <bigfoot@classfun.cn>
2021-05-17 22:13:52 +0800

    include: devd.h: add devd daemon support

--- include/devd.h
+++ include/devd.h
@@ -2,6 +2,10 @@
 #define DEVFS_H
 #include"uevent.h"
 
+#define DEFAULT_DEVD _PATH_RUN"/initdevd.sock"
+
+extern int devfd;
+
 // src/devd/devtmpfs.c: create all device node when no CONFIG_DEVTMPFS
 extern int init_devtmpfs(char*path);
 
@@ -13,6 +17,30 @@ extern int process_new_node(int devdfd,uevent*event);
 
 // src/devd/firmware.c: handle firmware_class helper, search cmdline.h:firmware_list to load
 extern int process_firmware_load(uevent*event);
+
+// src/devd/devd.c: process uevent data
+extern int process_uevent(uevent*event);
+
+// src/loggerd/devd.c: connect and set devd fd
+extern int open_devd_socket(char*tag,char*path);
+
+// src/loggerd/devd.c: close current devd fd
+extern void close_devd_socket();
+
+// src/loggerd/server.c: launch devd
+extern int start_devd(char*tag,pid_t*p);
+
+// src/loggerd/devd.c: call DEV_INIT to init devtmpfs
+extern int devd_call_init();
+
+// src/loggerd/devd.c: call DEV_MODALIAS to load modalias
+extern int devd_call_modalias();
+
+// src/loggerd/devd.c: call DEV_QUIT to terminate devd
+extern int devd_call_quit();
+
+// open default devd socket
+#define open_default_devd_socket(tag) open_devd_socket(tag,DEFAULT_DEVD)
 
 #ifdef ENABLE_KMOD
 // src/devd/modalias.c: search modalias in /sys/devices to load all modules

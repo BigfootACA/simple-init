b8a78ba4cd7af30e29cfbd4440e3a82408e87de0
BigfootACA <bigfoot@classfun.cn>
2021-05-18 09:30:26 +0800

    lib: add pool.c
    support for thread pool

---
+++ include/pool.h
@@ -0,0 +1,32 @@
+#ifndef _POOL_H
+#define _POOL_H
+#include<stdbool.h>
+#include<pthread.h>
+
+// job struct
+struct job{
+	void*(*callback)(void *arg);
+	void*arg;
+	struct job*next;
+};
+
+// pool struct
+struct pool{
+	int t_size,q_max,q_cur;
+	bool q_closed,p_closed;
+	struct job *first,*last;
+	pthread_t *pthreads;
+	pthread_mutex_t mutex;
+	pthread_cond_t q_empty,q_nempty,q_nfull;
+};
+
+// src/lib/pool.c: init a thread pool
+extern struct pool*pool_init(int t_size,int q_max);
+
+// src/lib/pool.c: add to thread pool
+extern int pool_add(struct pool*pool,void*(*callback)(void*arg),void*arg);
+
+// src/lib/pool.c: destroy thread pool
+extern int pool_destroy(struct pool*pool);
+
+#endif
---
+++ src/lib/pool.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<pthread.h>
+#include"pool.h"
+
+
+static void*_pool_main(void*arg){
+	struct pool*pool=(struct pool*)arg;
+	struct job*pjob=NULL;
+	for(;;){
+		pthread_mutex_lock(&(pool->mutex));
+		while((pool->q_cur==0)&&!pool->p_closed)
+			pthread_cond_wait(&(pool->q_nempty),&(pool->mutex));
+		if(pool->p_closed){
+			pthread_mutex_unlock(&(pool->mutex));
+			pthread_exit(NULL);
+		}
+		pool->q_cur--;
+		pjob=pool->first;
+		pool->first=pool->q_cur==0?(pool->last=NULL):pjob->next;
+		if(pool->q_cur==0)pthread_cond_signal(&(pool->q_empty));
+		if(pool->q_cur==pool->q_max-1)pthread_cond_broadcast(&(pool->q_nfull));
+		pthread_mutex_unlock(&(pool->mutex));
+		(*(pjob->callback))(pjob->arg);
+		free(pjob);
+		pjob=NULL;
+	}
+}
+
+struct pool*pool_init(int t_size,int q_max){
+	struct pool*pool=NULL;
+	if(!(pool=malloc(sizeof(struct pool))))goto fail;
+	if(!(pool->pthreads=malloc(sizeof(pthread_t)*t_size)))goto fail;
+	pool->t_size=t_size;
+	pool->q_max=q_max,pool->q_cur=0;
+	pool->first=NULL,pool->last=NULL;
+	pool->q_closed=false,pool->p_closed=false;
+	if(pthread_mutex_init(&(pool->mutex),NULL))goto fail;
+	if(pthread_cond_init(&(pool->q_empty),NULL))goto fail;
+	if(pthread_cond_init(&(pool->q_nempty),NULL))goto fail;
+	if(pthread_cond_init(&(pool->q_nfull),NULL))goto fail;
+	int i;
+	for(i=0;i<pool->t_size;++i)
+		pthread_create(&(pool->pthreads[i]),NULL,_pool_main,(void*)pool);
+	return pool;
+	fail:
+	if(pool){
+		if(pool->pthreads)free(pool->pthreads);
+		free(pool);
+	}
+	return NULL;
+}
+
+int pool_add(struct pool*pool,void*(*callback)(void*arg),void*arg){
+	if(!pool||!callback)return -1;
+	pthread_mutex_lock(&(pool->mutex));
+	while((pool->q_cur==pool->q_max)&&!(pool->q_closed||pool->p_closed))
+		pthread_cond_wait(&(pool->q_nfull),&(pool->mutex));
+	if(pool->q_closed||pool->p_closed){
+		pthread_mutex_unlock(&(pool->mutex));
+		return -1;
+	}
+	struct job*pjob;
+	if(!(pjob=(struct job*)malloc(sizeof(struct job)))){
+		pthread_mutex_unlock(&(pool->mutex));
+		return -1;
+	}
+	pjob->callback=callback;
+	pjob->arg=arg;
+	pjob->next=NULL;
+	if(!pool->first){
+		pool->first=pool->last=pjob;
+		pthread_cond_broadcast(&(pool->q_nempty));
+	}else{
+		pool->last->next=pjob;
+		pool->last=pjob;
+	}
+	pool->q_cur++;
+	pthread_mutex_unlock(&(pool->mutex));
+	return 0;
+}
+
+int pool_destroy(struct pool*pool){
+	if(!pool)return -1;
+	pthread_mutex_lock(&(pool->mutex));
+	if(pool->q_closed||pool->p_closed){
+		pthread_mutex_unlock(&(pool->mutex));
+		return -1;
+	}
+	pool->q_closed=true;
+	while(pool->q_cur!=0)
+		pthread_cond_wait(&(pool->q_empty),&(pool->mutex));
+	pool->p_closed=true;
+	pthread_mutex_unlock(&(pool->mutex));
+	pthread_cond_broadcast(&(pool->q_nempty));
+	pthread_cond_broadcast(&(pool->q_nfull));
+	for(int i=0;i<pool->t_size;++i)
+		pthread_join(pool->pthreads[i],NULL);
+	pthread_mutex_destroy(&(pool->mutex));
+	pthread_cond_destroy(&(pool->q_empty));
+	pthread_cond_destroy(&(pool->q_nempty));
+	pthread_cond_destroy(&(pool->q_nfull));
+	free(pool->pthreads);
+	struct job*p;
+	while(pool->first){
+		p=pool->first;
+		pool->first=p->next;
+		free(p);
+	}
+	free(pool);
+	return 0;
+}

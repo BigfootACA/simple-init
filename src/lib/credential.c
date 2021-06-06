dd6a9868c295abdfcf4bc3efb74804c8ac94d766
BigfootACA <bigfoot@classfun.cn>
2021-06-06 16:12:49 +0800

    lib: credential.c: fix buffer size

--- src/lib/credential.c
+++ src/lib/credential.c
@@ -6,17 +6,17 @@
 char*get_username(uid_t uid,char*buff,size_t size){
 	memset(buff,0,size);
 	struct passwd*pw=getpwuid(uid);
-	if(pw)strncpy(buff,pw->pw_name,size);
-	else if(uid==0)strncpy(buff,"root",size);
-	else snprintf(buff,size,"%d",uid);
+	if(pw)strncpy(buff,pw->pw_name,size-1);
+	else if(uid==0)strncpy(buff,"root",size-1);
+	else snprintf(buff,size-1,"%d",uid);
 	return buff;
 }
 
 char*get_groupname(gid_t gid,char*buff,size_t size){
 	memset(buff,0,size);
 	struct group*gr=getgrgid(gid);
-	if(gr)strncpy(buff,gr->gr_name,size);
-	else if(gid==0)strncpy(buff,"root",size);
-	else snprintf(buff,size,"%d",gid);
+	if(gr)strncpy(buff,gr->gr_name,size-1);
+	else if(gid==0)strncpy(buff,"root",size-1);
+	else snprintf(buff,size-1,"%d",gid);
 	return buff;
 }

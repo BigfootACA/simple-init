7214241c6fd74616243cb7604da7f0b06017d41c
BigfootACA <bigfoot@classfun.cn>
2021-06-07 12:11:29 +0800

    lib: credential.c: add get_commname

--- src/lib/credential.c
+++ src/lib/credential.c
@@ -2,6 +2,9 @@
 #include<grp.h>
 #include<stdio.h>
 #include<string.h>
+#include"defines.h"
+#include"pathnames.h"
+#include"system.h"
 
 char*get_username(uid_t uid,char*buff,size_t size){
 	memset(buff,0,size);
@@ -18,5 +21,13 @@ char*get_groupname(gid_t gid,char*buff,size_t size){
 	if(gr)strncpy(buff,gr->gr_name,size-1);
 	else if(gid==0)strncpy(buff,"root",size-1);
 	else snprintf(buff,size-1,"%d",gid);
+	return buff;
+}
+
+char*get_commname(pid_t pid,char*buff,size_t size){
+	if(pid<=0)return NULL;
+	memset(buff,0,size);
+	if(read_file(buff,size-1,false,_PATH_PROC"/%d/comm",pid)>0)return buff;
+	snprintf(buff,size-1,"%d",pid);
 	return buff;
 }

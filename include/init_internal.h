9285e6a358a3b6b9bda7d9ff2b610d35f12b45ef
BigfootACA <bigfoot@classfun.cn>
2021-06-18 02:21:29 +0800

    include: init_internal.h: add exported functions

--- include/init_internal.h
+++ include/init_internal.h
@@ -1,7 +1,5 @@
 #ifndef INIT_INTERNAL_H
 #define INIT_INTERNAL_H
-#define _GNU_SOURCE
-#include"init.h"
 #include<sys/socket.h>
 #define DEFAULT_INITD _PATH_RUN"/initd.sock"
 
@@ -42,4 +40,36 @@ struct init_msg{
 	enum init_action action;
 	union action_data data;
 };
+
+#include"init.h"
+
+#ifdef _GNU_SOURCE
+// src/initd/protocol.c: process init_msg
+extern int init_process_data(int cfd,struct ucred*u,struct init_msg*msg);
+
+// src/initd/protocol.c: check ucred permission
+extern bool init_check_privilege(enum init_action act,struct ucred*cred);
+#endif
+
+// src/initd/protocol.c: check init_msg validity
+extern bool init_check_msg(struct init_msg*msg);
+
+// src/initd/protocol.c: initialize init_msg
+extern void init_initialize_msg(struct init_msg*msg,enum init_action act);
+
+// src/initd/protocol.c: send init_msg
+extern int init_send_data(int fd,struct init_msg*send);
+
+// src/initd/protocol.c: receive init_msg
+extern int init_recv_data(int fd,struct init_msg*response);
+
+// src/initd/protocol.c: convert init_action to string
+extern const char*action2string(enum init_action action);
+
+// src/initd/socket.c: listen init socket
+extern int listen_init_socket();
+
+// src/initd/socket.c: process socket epoll
+extern int init_process_socket(int sfd);
+
 #endif

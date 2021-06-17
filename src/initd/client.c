260cd9b3b8c5e75f9b280267f7f3818488179392
BigfootACA <bigfoot@classfun.cn>
2021-06-18 02:15:09 +0800

    initd: add client.c

---
+++ src/initd/client.c
@@ -0,0 +1,56 @@
+#include<string.h>
+#include<sys/un.h>
+#include<sys/socket.h>
+#include"defines.h"
+#include"output.h"
+#include"init_internal.h"
+
+int initfd=-1;
+
+int set_initfd(int fd){
+	return initfd=fd<0?initfd:fd;
+}
+
+void close_initfd(){
+	if(initfd<0)return;
+	close(initfd);
+	initfd=-1;
+}
+
+int open_socket_initfd(char*path){
+	if(!path)ERET(EINVAL);
+	struct sockaddr_un addr;
+	int sock;
+	if((sock=socket(AF_UNIX,SOCK_STREAM,0))<0){
+		stderr_perror("cannot create socket");
+		return -1;
+	}
+	memset(&addr,0,sizeof(addr));
+	addr.sun_family=AF_UNIX;
+	strncpy(addr.sun_path,path,sizeof(addr.sun_path)-1);
+	if(connect(sock,(struct sockaddr*)&addr,sizeof(addr))<0){
+		stderr_perror("cannot connect socket %s",path);
+		close(sock);
+		return -1;
+	}
+	return set_initfd(sock);
+}
+
+int init_send_raw(struct init_msg*send){
+	return init_send_data(initfd,send);
+}
+
+int init_recv_raw(struct init_msg*response){
+	return init_recv_data(initfd,response);
+}
+
+int init_send(struct init_msg*send,struct init_msg*response){
+	if(!send||!response)ERET(EINVAL);
+	if(init_send_raw(send)!=0)return -errno;
+	do{if(init_recv_raw(response))return -errno;}
+	while(
+		response->action!=ACTION_OK&&
+		response->action!=ACTION_FAIL
+	);
+	return 0;
+}

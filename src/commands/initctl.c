a1bd4ae19dbb3324c77ad3cc0d565d963ca0fe72
BigfootACA <bigfoot@classfun.cn>
2021-06-19 09:49:14 +0800

    commands: initctl.c: add setenv for init call ACTION_ADDENV

--- src/commands/initctl.c
+++ src/commands/initctl.c
@@ -18,6 +18,8 @@ static int usage(int e){
 		"\thalt                      Halt system\n"
 		"\treboot [STRING]           Reboot system\n"
 		"\tswitchroot <ROOT> [INIT]  Switch to new root\n"
+		"\tsetenv <KEY> <VALUE>      Add environment variable\n"
+		"\naddenv <KEY>=<VALUE>      Add environment variable\n"
 		"Options:\n"
 		"\t-h, --help  display this help and exit\n"
 	);
@@ -79,6 +81,34 @@ static int cmd_switchroot(int argc,char**argv){
 	return cmd_wrapper(&msg,argv[0]);
 }
 
+static int cmd_setenv(int argc,char**argv){
+	if(argc>3)return re_printf(2,"too many arguments\n");
+	if(argc<2)return re_printf(2,"missing arguments\n");
+	#define xkey msg.data.env.key
+	#define xvalue msg.data.env.value
+	char*key,*value;
+	struct init_msg msg;
+	size_t klen=0,vlen=0;
+	size_t s1=sizeof(xkey),s2=sizeof(xvalue);
+	switch(argc){
+		case 2:
+			if(!(value=strchr(argv[1],'=')))
+				return re_printf(2,"missing arguments\n");
+			klen=value-argv[1],vlen=strlen(++value),key=argv[1];
+		break;
+		case 3:
+			key=argv[1],value=argv[2];
+			klen=strlen(key),vlen=strlen(value);
+		break;
+	}
+	if(klen<=0)return re_printf(2,"invalid environ name\n");
+	if(klen>=s1||vlen>=s2)return re_printf(2,"arguments too long\n");
+	init_initialize_msg(&msg,ACTION_ADDENV);
+	strncpy(xkey,key,klen);
+	strncpy(xvalue,value,vlen);
+	return cmd_wrapper(&msg,argv[0]);
+}
+
 int initctl_main(int argc,char**argv){
 	static const struct option lo[]={
 		{"help",    no_argument,       NULL,'h'},
@@ -103,6 +133,8 @@ int initctl_main(int argc,char**argv){
 	else if(strcmp(vn,"halt")==0)return cmd_halt(ac,av);
 	else if(strcmp(vn,"reboot")==0)return cmd_reboot(ac,av);
 	else if(strcmp(vn,"switchroot")==0)return cmd_switchroot(ac,av);
+	else if(strcmp(vn,"setenv")==0)return cmd_setenv(ac,av);
+	else if(strcmp(vn,"addenv")==0)return cmd_setenv(ac,av);
 	else return re_printf(1,"unknown command: %s\n",vn);
 	conflict:return re_printf(2,"too many arguments\n");
 }

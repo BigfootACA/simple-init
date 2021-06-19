a37e458368a9333d0a583497c6e9702f56d928ed
BigfootACA <bigfoot@classfun.cn>
2021-06-19 10:02:13 +0800

    lib: switchroot.c: call init_do_exit to cleanup

--- src/lib/switchroot.c
+++ src/lib/switchroot.c
@@ -127,9 +127,7 @@ int run_switch_root(char*root,char*init){
 	if(stat(root,&buf)!=0)return terlog_error(-EINVAL,"cannot stat rootfs");
 	if(!S_ISDIR(buf.st_mode))return trlog_error(-EINVAL,"rootfs not a folder");
 	tlog_alert("switch to new root %s and execute new init %s",root,init);
-	devd_call_quit();
-	logger_exit();
-	close_all_fd(NULL,0);
+	init_do_exit();
 	kill_all();
 	if(switchroot(root)!=0)return -1;
 	if(run_init(init)!=0){

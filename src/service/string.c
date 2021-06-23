504c35265fb0e8abbd73b05415025a41c9293378
BigfootACA <bigfoot@classfun.cn>
2021-06-24 06:47:15 +0800

    service: add string.c
    add service manager, but it's not stable now

---
+++ src/service/string.c
@@ -0,0 +1,37 @@
+#include"service.h"
+#define TAG "service"
+
+const char*svc_exec_type_string(enum svc_exec_type type){
+	switch(type){
+		case TYPE_UNKNOWN:return  "Unknown";
+		case TYPE_FUNCTION:return "Function";
+		case TYPE_COMMAND:return  "Command";
+		case TYPE_LIBRARY:return  "Library";
+	}
+	return NULL;
+}
+
+const char*svc_status_string(enum svc_status status){
+	switch(status){
+		case STATUS_UNKNOWN:return  "Unknown";
+		case STATUS_STOPPED:return  "Stopped";
+		case STATUS_STARTING:return "Starting";
+		case STATUS_STARTED:return  "Started";
+		case STATUS_RUNNING:return  "Running";
+		case STATUS_STOPPING:return "Stopping";
+		case STATUS_FAILED:return   "Failed";
+	}
+	return NULL;
+}
+
+
+const char*svc_work_string(enum svc_work work){
+	switch(work){
+		case WORK_UNKNOWN:return    "Unknown";
+		case WORK_FAKE:return       "Fake";
+		case WORK_FOREGROUND:return "Foreground";
+		case WORK_DAEMON:return     "Daemon";
+		case WORK_ONCE:return       "Once";
+	}
+	return NULL;
+}

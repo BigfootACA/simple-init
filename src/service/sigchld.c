2b13d4e0e885ac048147c2cfae2a09a003eb6dd4
BigfootACA <bigfoot@classfun.cn>
2021-06-30 10:55:58 +0800

    service: sigchld.c: fix service failed detect

--- src/service/sigchld.c
+++ src/service/sigchld.c
@@ -1,4 +1,5 @@
 #include<errno.h>
+#include<signal.h>
 #include<stddef.h>
 #include<stdlib.h>
 #include<string.h>
@@ -10,13 +11,13 @@
 #include"service.h"
 #define TAG "service"
 
-static int svc_on_exit_main(struct service*svc){
+static int svc_on_exit_main(struct service*svc,bool fail){
 	char*name=svc_get_desc(svc);
 	switch(svc->mode){
 		case WORK_UNKNOWN:return -1;
 		case WORK_FAKE:
 			tlog_warn("fake service %s should not have main",name);
-			svc->status=STATUS_STARTED;
+			svc->status=fail?STATUS_FAILED:STATUS_STARTED;
 			memset(&svc->process,0,sizeof(struct proc_status));
 		break;
 		case WORK_DAEMON:
@@ -28,32 +29,32 @@ static int svc_on_exit_main(struct service*svc){
 				tlog_warn("service %s does not running %s",name,svc_status_string(svc->status));
 				memset(&svc->process,0,sizeof(struct proc_status));
 			}else{
-				tlog_notice("Stopped service %s",name);
+				tlog_notice(fail?"Service %s failed":"Stopped service %s",name);
 				if(svc->auto_restart&&auto_restart){
 					tlog_info("Trigger service %s auto restart",name);
 					service_start(svc);
 				}
 			}
-			svc->status=STATUS_STOPPED;
+			svc->status=fail?STATUS_FAILED:STATUS_STOPPED;
 		break;
 		case WORK_ONCE:
 			if(svc->status!=STATUS_STARTING){
 				tlog_warn("service %s does not starting",name);
 				memset(&svc->process,0,sizeof(struct proc_status));
-			}else tlog_notice("Started service %s",name);
-			svc->status=STATUS_STARTED;
+			}else tlog_notice(fail?"Service %s failed":"Started service %s",name);
+			svc->status=fail?STATUS_FAILED:STATUS_STARTED;
 		break;
 	}
 	return 0;
 }
 
-static int svc_on_exit_start(struct svc_exec*exec,struct service*svc){
+static int svc_on_exit_start(struct svc_exec*exec,struct service*svc,bool fail){
 	if(exec->prop.svc!=svc)ERET(EINVAL);
 	char*name=svc_get_desc(svc);
 	if(svc->status!=STATUS_STARTING){
 		tlog_warn("service %s is not starting but start exec exited",name);
 		memset(&svc->process,0,sizeof(struct proc_status));
-		svc->status=STATUS_STOPPED;
+		svc->status=fail?STATUS_FAILED:STATUS_STOPPED;
 		return 0;
 	}
 	switch(svc->mode){
@@ -61,7 +62,7 @@ static int svc_on_exit_start(struct svc_exec*exec,struct service*svc){
 			if(svc_daemon_get_pid(svc)<0)return -1;
 			break;
 		case WORK_ONCE:
-			svc->status=STATUS_RUNNING;
+			svc->status=fail?STATUS_FAILED:STATUS_RUNNING;
 			svc->process.running=false;
 			svc->process.start=exec->status.start;
 			svc->process.active=svc->process.start;
@@ -69,10 +70,10 @@ static int svc_on_exit_start(struct svc_exec*exec,struct service*svc){
 		default:
 			tlog_warn("service %s is not daemon or once mode, but in starting status",name);
 			memset(&svc->process,0,sizeof(struct proc_status));
-			svc->status=STATUS_STOPPED;
+			svc->status=fail?STATUS_FAILED:STATUS_STOPPED;
 			return 0;
 	}
-	tlog_notice("Started service %s",name);
+	tlog_notice(fail?"Service %s failed":"Started service %s",name);
 	return 0;
 }
 
@@ -105,19 +106,25 @@ static int svc_exec_on_exit(pid_t p,struct svc_exec*exec,struct service*svc,int 
 	int c;
 	pthread_mutex_lock(&svc->lock);
 	if(exec)pthread_mutex_lock(&exec->lock);
+	bool fail=false;
 	if(WIFEXITED(st)){
 		c=WEXITSTATUS(st);
 		status->exit_code=c;
 		status->exit_signal=0;
+		fail=c!=0;
 	}else if(WIFSIGNALED(st)){
 		c=WTERMSIG(st);
 		status->exit_code=128|c;
 		status->exit_signal=c;
+		switch(c){
+			case SIGTERM:case SIGHUP:case SIGINT:case SIGQUIT:break;
+			default:fail=true;
+		}
 	}else goto finish;
 	time(&status->finish);
 	status->running=false;
-	if(!exec)svc_on_exit_main(svc);
-	else if(svc->start==exec)svc_on_exit_start(exec,svc);
+	if(!exec)svc_on_exit_main(svc,fail);
+	else if(svc->start==exec)svc_on_exit_start(exec,svc,fail);
 	else if(svc->stop==exec)svc_on_exit_stop(exec,svc);
 	finish:
 	pthread_mutex_unlock(&svc->lock);

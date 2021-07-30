42b821a22d34de358384e45fceae204bd20a5b25
BigfootACA <bigfoot@classfun.cn>
2021-07-30 22:50:15 +0800

    hardware: led.c: add led_get_max_brightness led_{get,set}_brightness{,_percent}

--- include/hardware.h
+++ include/hardware.h
@@ -2,6 +2,11 @@
 #define HARDWARE_H
 #include<lvgl.h>
 #include<stdbool.h>
+extern int led_get_max_brightness(int fd);
+extern int led_get_brightness(int fd);
+extern int led_set_brightness(int fd,int value);
+extern int led_get_brightness_percent(int fd);
+extern int led_set_brightness_percent(int fd,int percent);
 extern int set_brightness_percent(char*name,int percent);
 extern void vibrate(char*dev,int time);
 #endif
--- src/hardware/led.c
+++ src/hardware/led.c
@@ -5,8 +5,36 @@
 #include<sys/stat.h>
 #define TAG "led"
 #include"str.h"
+#include"system.h"
 #include"logger.h"
 #include"pathnames.h"
+
+int led_get_max_brightness(int fd){
+	return fd_read_int(fd,"max_brightness");
+}
+
+int led_get_brightness(int fd){
+	return fd_read_int(fd,"brightness");
+}
+
+int led_set_brightness(int fd,int value){
+	return fd_write_int(fd,"brightness",value,true);
+}
+
+int led_get_brightness_percent(int fd){
+	if(fd<0)ERET(EBADF);
+	int max,cur;
+	if((max=led_get_max_brightness(fd))<=0)return -1;
+	if((cur=led_get_brightness(fd))<0)return -1;
+	return (int)(((float)cur)/((float)max)*100);
+}
+
+int led_set_brightness_percent(int fd,int percent){
+	if(percent<0||percent>100)ERET(EINVAL);
+	if(fd<0)ERET(EBADF);
+	int max=led_get_max_brightness(fd);
+	return max>0?led_set_brightness(fd,max/100*percent):-1;
+}
 
 int set_brightness_percent(char*name,int percent){
 	if(!name||percent<0||percent>100||strcmp(name,"..")==0)return -1;

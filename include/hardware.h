0d2c733c4af5e2ea98d4bf1a55aec614417bd516
BigfootACA <bigfoot@classfun.cn>
2021-07-31 00:00:46 +0800

    hardware: led.c: rewrite led_set_brightness_percent_by_name

--- include/hardware.h
+++ include/hardware.h
@@ -7,6 +7,6 @@ extern int led_get_brightness(int fd);
 extern int led_set_brightness(int fd,int value);
 extern int led_get_brightness_percent(int fd);
 extern int led_set_brightness_percent(int fd,int percent);
-extern int set_brightness_percent(char*name,int percent);
+extern int led_set_brightness_percent_by_name(char*name,int percent);
 extern void vibrate(char*dev,int time);
 #endif
--- src/hardware/led.c
+++ src/hardware/led.c
@@ -36,48 +36,25 @@ int led_set_brightness_percent(int fd,int percent){
 	return max>0?led_set_brightness(fd,max/100*percent):-1;
 }
 
-int set_brightness_percent(char*name,int percent){
-	if(!name||percent<0||percent>100||strcmp(name,"..")==0)return -1;
-	int c,dir=-1,dmax=-1,dcur=-1,r=-1;
-	long max;
+int led_set_brightness_percent_by_name(char*name,int percent){
+	if(!name)return -1;
+	int c,dir,r;
 	struct stat s;
-	char path[512]={0},buff[64]={0};
+	char path[512]={0};
 
 	for(c=0;name[c]!=0;c++)
 		if(name[c]=='/'||strncmp(name+c,"..",2)==0)
 			return -1;
 
 	snprintf(path,511,_PATH_SYS_CLASS"/leds/%s",name);
-	if(stat(path,&s)<0||!S_ISLNK(s.st_mode)){
-		telog_error("not a valid led device %s",name);
-		goto end;
-	}
 
-	if((dir=open(path,O_DIRECTORY))<0){
-		telog_error("failed to open device %s",name);
-		goto end;
-	}
+	if(stat(path,&s)<0||!S_ISLNK(s.st_mode))
+		return terlog_error(-1,"not a valid led device %s",name);
 
-	if((dmax=openat(dir,"max_brightness",O_RDONLY))<0||read(dmax,buff,64)<=0){
-		telog_error("failed to read max brightness of device %s",name);
-		goto end;
-	}
+	if((dir=open(path,O_DIR))<0)
+		return terlog_error(-1,"failed to open device %s",name);
 
-	if((max=parse_int(buff,-1))<0){
-		telog_error("invalid max brightness of device %s",name);
-		goto end;
-	}
-	memset(buff,0,63);
-	snprintf(buff,63,"%ld\n",max/100*percent);
-	c=(int)strlen(buff);
-	if((dcur=openat(dir,"brightness",O_WRONLY))<0||write(dcur,buff,c)!=c){
-		telog_error("failed to write brightness of device %s",name);
-		goto end;
-	}
-	r=0;
-	end:
-	if(dmax>=0)close(dmax);
-	if(dcur>=0)close(dcur);
-	if(dir>=0)close(dir);
+	r=led_set_brightness_percent(dir,percent);
+	close(dir);
 	return r;
 }

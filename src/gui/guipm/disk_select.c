fdf89eb086dc4cc353188c32d479fd9b3bd4259c
BigfootACA <bigfoot@classfun.cn>
2021-08-01 15:36:54 +0800

    gui: guipm: disk_select.c: add ok msgbox

--- src/gui/guipm/disk_select.c
+++ src/gui/guipm/disk_select.c
@@ -265,6 +265,43 @@ static void refresh_click(lv_obj_t*obj,lv_event_t e){
 	guipm_disk_reload();
 }
 
+static void ok_msg_click(lv_obj_t*obj,lv_event_t e){
+	if(e==LV_EVENT_DELETE){
+		lv_obj_del_async(lv_obj_get_parent(obj));
+	}else if(e==LV_EVENT_VALUE_CHANGED){
+		switch(lv_msgbox_get_active_btn(obj)){
+			case 0:
+			break;
+		}
+		lv_msgbox_start_auto_close(obj,0);
+	}
+}
+
+static void ok_click(lv_obj_t*obj,lv_event_t e){
+	if(e!=LV_EVENT_CLICKED||obj!=btn_ok||!selected)return;
+	tlog_debug("ok clicked");
+	static lv_style_t bg,btn;
+	static const char*btns[3];
+	btns[0]=_("Yes"),btns[1]=_("No"),btns[2]="";
+	lv_style_init(&bg);
+	lv_style_set_bg_opa(&bg,LV_STATE_DEFAULT,LV_OPA_50);
+	lv_style_set_bg_color(&bg,LV_STATE_DEFAULT,LV_COLOR_BLACK);
+	lv_style_init(&btn);
+	lv_style_set_margin_all(&btn,LV_STATE_DEFAULT,0);
+	lv_style_set_pad_all(&btn,LV_STATE_DEFAULT,gui_dpi/10);
+	lv_style_set_radius(&btn,LV_STATE_DEFAULT,gui_dpi/10);
+	lv_obj_t*mask=lv_objmask_create(selscr,NULL);
+	lv_obj_set_size(mask,gui_sw,gui_sh);
+	lv_obj_add_style(mask,LV_OBJMASK_PART_MAIN,&bg);
+	lv_obj_t*msg=lv_msgbox_create(mask,NULL);
+	lv_obj_set_width(msg,gui_sw/6*5);
+	lv_msgbox_set_text_fmt(msg,_("Are you sure you want to operate on disk %s?"),selected->name);
+	lv_msgbox_add_btns(msg,btns);
+	lv_obj_add_style(msg,LV_MSGBOX_PART_BTN,&btn);
+	lv_obj_set_event_cb(msg,ok_msg_click);
+	lv_obj_align(msg,NULL,LV_ALIGN_CENTER,0,0);
+}
+
 static void show_all_click(lv_obj_t*obj,lv_event_t e){
 	if(e!=LV_EVENT_VALUE_CHANGED||obj!=show_all)return;
 	is_show_all=lv_checkbox_is_checked(obj);
@@ -353,6 +390,7 @@ void guipm_draw_disk_sel(lv_obj_t*screen){
 	lv_obj_set_size(btn_ok,btw,bth);
 	lv_obj_add_style(btn_ok,0,&btn_style);
 	lv_obj_add_state(btn_ok,LV_STATE_CHECKED|LV_STATE_DISABLED);
+	lv_obj_set_event_cb(btn_ok,ok_click);
 	lv_label_set_text(lv_label_create(btn_ok,NULL),_("OK"));
 
 	// refresh button

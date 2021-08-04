983e42a88a85ebbc72f2c13af81c6aefebce8a03
BigfootACA <bigfoot@classfun.cn>
2021-08-04 14:04:10 +0800

    gui: interface: guiapp.c: add backlight_menu_draw

--- src/gui/interface/guiapp.c
+++ src/gui/interface/guiapp.c
@@ -113,6 +113,7 @@ static void _draw(lv_obj_t*scr){
 	add_button(_("Multi-Boot Manage"),      "bootmgr.png",   NULL);
 	add_button(_("Reboot Menu"),            "reboot.png",    reboot_menu_draw);
 	add_button(_("Loggerd Viewer"),         "logviewer.png", NULL);
+	add_button(_("Backlight"),              "backlight.png", backlight_menu_draw);
 
 	guiact_register_activity(&(struct gui_activity){
 		.name="guiapp",

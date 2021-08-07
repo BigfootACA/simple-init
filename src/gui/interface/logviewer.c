e5a34b2547092e9e466da9310563201c99ce755a
BigfootACA <bigfoot@classfun.cn>
2021-08-07 10:17:13 +0800

    gui: interface: logviewer.c: add go top, reload, go bottom buttons

--- src/gui/interface/logviewer.c
+++ src/gui/interface/logviewer.c
@@ -7,6 +7,7 @@
 #define TAG "logviewer"
 
 static lv_obj_t*scr,*view;
+static lv_obj_t*btn_top,*btn_bottom,*btn_reload;
 
 void logviewer_draw(lv_obj_t*screen){
 
@@ -24,8 +25,10 @@ void logviewer_draw(lv_obj_t*screen){
 	lv_style_init(&style);
 	lv_style_set_text_font(&style,LV_STATE_DEFAULT,gui_font_small);
 
+	int bth=gui_font_size+(gui_dpi/8),btw=gui_sw/3-(gui_dpi/8);
+
 	view=lv_textarea_create(scr,NULL);
-	lv_obj_set_size(view,gui_sw-(gui_font_size*2),gui_sh-lv_obj_get_height(txt)-gui_font_size*3);
+	lv_obj_set_size(view,gui_sw-(gui_font_size*2),gui_sh-lv_obj_get_height(txt)-gui_font_size*4-bth);
 	lv_obj_align(view,txt,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
 	lv_obj_add_style(view,LV_TEXTAREA_PART_BG,&style);
 	lv_textarea_set_cursor_hidden(view,true);
@@ -36,6 +39,35 @@ void logviewer_draw(lv_obj_t*screen){
 	lv_textarea_set_scrollbar_mode(view,LV_SCROLLBAR_MODE_DRAG);
 	lv_textarea_ext_t*e=lv_obj_get_ext_attr(view);
 	lv_label_set_long_mode(e->label,LV_LABEL_LONG_EXPAND);
+
+	static lv_style_t btn_style;
+	lv_style_init(&btn_style);
+	lv_style_set_radius(&btn_style,LV_STATE_DEFAULT,2);
+	lv_style_set_outline_width(&btn_style,LV_STATE_PRESSED,0);
+
+	btn_top=lv_btn_create(scr,NULL);
+	lv_obj_set_size(btn_top,btw,bth);
+	lv_obj_align(btn_top,view,LV_ALIGN_OUT_BOTTOM_LEFT,0,gui_font_size);
+	lv_obj_add_style(btn_top,LV_BTN_PART_MAIN,&btn_style);
+	lv_obj_add_state(btn_top,LV_STATE_CHECKED);
+	lv_label_set_text(lv_label_create(btn_top,NULL),_("Go top"));
+	lv_group_add_obj(gui_grp,btn_top);
+
+	btn_reload=lv_btn_create(scr,NULL);
+	lv_obj_set_size(btn_reload,btw,bth);
+	lv_obj_align(btn_reload,view,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
+	lv_obj_add_style(btn_reload,LV_BTN_PART_MAIN,&btn_style);
+	lv_obj_add_state(btn_reload,LV_STATE_CHECKED);
+	lv_label_set_text(lv_label_create(btn_reload,NULL),_("Reload"));
+	lv_group_add_obj(gui_grp,btn_reload);
+
+	btn_bottom=lv_btn_create(scr,NULL);
+	lv_obj_set_size(btn_bottom,btw,bth);
+	lv_obj_align(btn_bottom,view,LV_ALIGN_OUT_BOTTOM_RIGHT,0,gui_font_size);
+	lv_obj_add_style(btn_bottom,LV_BTN_PART_MAIN,&btn_style);
+	lv_obj_add_state(btn_bottom,LV_STATE_CHECKED);
+	lv_label_set_text(lv_label_create(btn_bottom,NULL),_("Go bottom"));
+	lv_group_add_obj(gui_grp,btn_bottom);
 
 	int fd=open(_PATH_DEV"/logger.log",O_RDONLY);
 	if(fd<0)telog_warn("open logger.log failed");

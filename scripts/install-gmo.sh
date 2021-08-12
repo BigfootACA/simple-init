3a5f1b895b5565cc628807807715dfb5951c1dab
BigfootACA <bigfoot@classfun.cn>
2021-08-12 15:21:30 +0800

    root: package gmo files into rootfs

---
+++ scripts/install-gmo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+WORKSPACE="$(realpath "$(dirname $0)"/..)"
+BUILD="/tmp"
+ROOT="${WORKSPACE}/root"
+[ -n "${1}" ]&&WORKSPACE="${1}"
+[ -n "${2}" ]&&BUILD="${2}"
+[ -n "${3}" ]&&ROOT="${3}"
+for i in "${BUILD}"/*.gmo
+do	LOCALE="$(basename "$i" .gmo)"
+	TARGET="${ROOT}/usr/share/locale/${LOCALE}/LC_MESSAGES/simple-init.mo"
+	install -Dm644 "${i}" "${TARGET}"
+done

6fcca8502818d74dbe743460f17f42cd8f2f15e2
BigfootACA <bigfoot@classfun.cn>
2021-08-13 11:20:21 +0800

    include: language.h: add lang_gettext

--- include/language.h
+++ include/language.h
@@ -10,4 +10,11 @@ extern void lang_load_locale(const char*dir,const char*lang,const char*domain);
 // src/lib/locale.c: get current locale
 extern char*lang_get_locale(char*def);
 
+// src/lib/locale.c: gettext implementation
+extern char*lang_gettext(const char*msgid) __attribute_format_arg__(1);
+
+#ifndef _
+#define _ lang_gettext
+#endif
+
 #endif

35f1f10cdef1f46806dab8411bc4b5ce5eda4c09
BigfootACA <bigfoot@classfun.cn>
2021-08-14 23:03:59 +0800

    scripts: install-gmo.sh: exit when gmo files not found

--- scripts/install-gmo.sh
+++ scripts/install-gmo.sh
@@ -6,7 +6,11 @@ ROOT="${WORKSPACE}/root"
 [ -n "${2}" ]&&BUILD="${2}"
 [ -n "${3}" ]&&ROOT="${3}"
 for i in "${BUILD}"/*.gmo
-do	LOCALE="$(basename "$i" .gmo)"
+do	if ! [ -f "${i}" ]
+	then	echo "${i} not found">&2
+		exit 1
+	fi
+	LOCALE="$(basename "$i" .gmo)"
 	TARGET="${ROOT}/usr/share/locale/${LOCALE}/LC_MESSAGES/simple-init.mo"
 	install -Dm644 "${i}" "${TARGET}"
 done

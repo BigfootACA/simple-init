c9c2ffca55d3a3c807d94e4ebeb45609562255d3
BigfootACA <bigfoot@classfun.cn>
2021-08-16 00:15:16 +0800

    gui: interface: filemgr: add filetab.c

---
+++ src/gui/include/filetab.h
@@ -0,0 +1,19 @@
+#ifndef FILEMGR_H
+#define FILEMGR_H
+#include"defines.h"
+struct filetab;
+typedef void(*filetab_on_change_dir)(struct filetab*,char*old,char*new);
+extern struct filetab*filetab_create(lv_obj_t*view,char*path);
+extern void filetab_add_group(struct filetab*tab,lv_group_t*grp);
+extern void filetab_remove_group(struct filetab*tab);
+extern void filetab_free(struct filetab*tab);;
+extern uint16_t filetab_get_id(struct filetab*tab);
+extern lv_obj_t*filetab_get_tab(struct filetab*tab);
+extern char*filetab_get_path(struct filetab*tab);
+extern void filetab_set_path(struct filetab*tab,char*path);
+extern void filetab_set_show_parent(struct filetab*tab,bool parent);
+extern bool filetab_is_active(struct filetab*tab);
+extern bool filetab_is_top(struct filetab*tab);
+extern void filetab_go_back(struct filetab*tab);
+extern void filetab_set_on_change_dir(struct filetab*tab,filetab_on_change_dir cb);
+#endif
---
+++ src/gui/interface/filemgr/filetab.c
@@ -0,0 +1,105 @@
+#include<libgen.h>
+#include<stdlib.h>
+#include<string.h>
+#include"lvgl.h"
+#include"logger.h"
+#include"filetab.h"
+#include"fileview.h"
+#define TAG "filetab"
+
+struct filetab{
+	uint16_t tab_id;
+	lv_obj_t*tab;
+	lv_obj_t*view;
+	struct fileview*fv;
+	filetab_on_change_dir on_change_dir;
+};
+
+static void on_change_dir(struct fileview*fv,char*old,char*new){
+	struct filetab*ft=fileview_get_data(fv);
+	char path[PATH_MAX]={0};
+	strcpy(path,new);
+	lv_tabview_set_tab_name(ft->view,ft->tab_id,basename(path));
+	if(ft->on_change_dir)ft->on_change_dir(ft,old,new);
+}
+
+struct filetab*filetab_create(lv_obj_t*view,char*path){
+	struct filetab*tb=malloc(sizeof(struct filetab));
+	if(!tb)return NULL;
+	memset(tb,0,sizeof(struct filetab));
+	tb->view=view;
+	if(!(tb->tab=lv_tabview_add_tab(view,_("File Manager")))){
+		free(tb);
+		return NULL;
+	}
+	if(!(tb->fv=fileview_create(tb->tab))){
+		free(tb);
+		tlog_error("initialize fileview failed");
+		abort();
+	}
+	fileview_set_data(tb->fv,tb);
+	fileview_set_on_change_dir(tb->fv,on_change_dir);
+	for(uint16_t i=0;i<lv_tabview_get_tab_count(view);i++){
+		if(lv_tabview_get_tab(view,i)!=tb->tab)continue;
+		tb->tab_id=i;
+		fileview_set_path(tb->fv,path);
+		return tb;
+	}
+	tlog_error("cannot get new tab");
+	abort();
+}
+
+void filetab_add_group(struct filetab*tab,lv_group_t*grp){
+	if(tab)fileview_add_group(tab->fv,grp);
+}
+
+void filetab_remove_group(struct filetab*tab){
+	if(tab)fileview_remove_group(tab->fv);
+}
+
+void filetab_free(struct filetab*tab){
+	if(!tab)return;
+	if(tab->fv)fileview_free(tab->fv);
+	if(tab->tab)lv_tabview_clean_tab(tab->tab);
+}
+
+uint16_t filetab_get_id(struct filetab*tab){
+	return tab?tab->tab_id:0;
+}
+
+lv_obj_t*filetab_get_tab(struct filetab*tab){
+	return tab?tab->tab:NULL;
+}
+
+char*filetab_get_path(struct filetab*tab){
+	return tab?fileview_get_path(tab->fv):NULL;
+}
+
+bool filetab_is_top(struct filetab*tab){
+	return tab?fileview_is_top(tab->fv):true;
+}
+
+bool filetab_is_active(struct filetab*tab){
+	if(!tab||!tab->view)return false;
+	return lv_tabview_get_tab_act(tab->view)==tab->tab_id;
+}
+
+void filetab_set_on_change_dir(struct filetab*tab,filetab_on_change_dir cb){
+	tab->on_change_dir=cb;
+}
+
+void filetab_set_show_parent(struct filetab*tab,bool parent){
+	if(!tab)return;
+	fileview_set_show_parent(tab->fv,parent);
+}
+
+void filetab_go_back(struct filetab*tab){
+	if(!tab)return;
+	fileview_go_back(tab->fv);
+}
+
+void filetab_set_path(struct filetab*tab,char*path){
+	if(!tab)return;
+	fileview_set_path(tab->fv,path);
+
+}

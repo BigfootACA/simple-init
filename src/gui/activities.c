63f9218503b0d61e38774d520704e10501b1aaeb
BigfootACA <bigfoot@classfun.cn>
2021-08-24 16:42:15 +0800

    gui: activities.c: fix macros

--- src/gui/activities.c
+++ src/gui/activities.c
@@ -10,15 +10,18 @@ extern struct gui_register guireg_filemgr;
 extern struct gui_register guireg_reboot;
 extern struct gui_register guireg_guiapp;
 struct gui_register*guiact_register[]={
-	&guireg_guipm_disk_select,
-	&guireg_guipm_partitions,
 	#ifdef ENABLE_UEFI
 	&guireg_uefi_bootmenu,
+	#else
+	#ifdef ENABLE_FDISK
+	&guireg_guipm_disk_select,
+	&guireg_guipm_partitions,
 	#endif
+	&guireg_filemgr,
 	&guireg_backlight,
 	&guireg_logviewer,
+	#endif
 	&guireg_language,
-	&guireg_filemgr,
 	&guireg_reboot,
 	&guireg_guiapp,
 	NULL

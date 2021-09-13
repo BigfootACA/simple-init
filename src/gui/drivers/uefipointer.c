2e19707cc9d48b22d009548b3804eb891405a2fe
BigfootACA <bigfoot@classfun.cn>
2021-09-13 22:03:51 +0800

    gui: drivers: uefipointer.c: scale mouse speed from DPI

--- src/gui/drivers/uefipointer.c
+++ src/gui/drivers/uefipointer.c
@@ -15,6 +15,10 @@
 #include"lvgl.h"
 #include"gui.h"
 #include"guidrv.h"
+#ifndef MOUSE_SCALE
+#define MOUSE_SCALE 1
+#endif
+static int scale=(DISPLAY_DPI/200)*MOUSE_SCALE;
 static lv_indev_t*dev=NULL;
 static EFI_SIMPLE_POINTER_PROTOCOL*mouse=NULL;
 static bool pointer_read(lv_indev_drv_t*indev_drv,lv_indev_data_t*data){
@@ -23,8 +27,8 @@ static bool pointer_read(lv_indev_drv_t*indev_drv,lv_indev_data_t*data){
 	if(!dev->cursor)lv_indev_set_cursor(dev,gui_cursor);
 	EFI_SIMPLE_POINTER_STATE p;
 	if(!EFI_ERROR(mouse->GetState(mouse,&p))){
-		INT64 ix=lx+(p.RelativeMovementX/(INT64)mouse->Mode->ResolutionX);
-		INT64 iy=ly+(p.RelativeMovementY/(INT64)mouse->Mode->ResolutionY);
+		INT64 ix=lx+(p.RelativeMovementX*scale/(INT64)mouse->Mode->ResolutionX);
+		INT64 iy=ly+(p.RelativeMovementY*scale/(INT64)mouse->Mode->ResolutionY);
 		lx=MIN(MAX(ix,0),gui_w);
 		ly=MIN(MAX(iy,0),gui_h);
 		lp=p.LeftButton==1;

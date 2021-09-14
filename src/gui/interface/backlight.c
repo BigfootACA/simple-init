a5ebbdc8f5a59183da0fee81ccc0c2d50437fe6b
BigfootACA <bigfoot@classfun.cn>
2021-09-14 17:40:53 +0800

    gui: interface: backlight.c: add arrow keys

--- src/gui/interface/backlight.c
+++ src/gui/interface/backlight.c
@@ -6,15 +6,28 @@
 #include"gui.h"
 #include"tools.h"
 #include"guidrv.h"
-#define TAG "reboot"
+#define TAG "backlight"
 
-static lv_obj_t*box,*value,*slider;
+static lv_obj_t*box,*value,*slider,*arr_left,*arr_right;
 
 static void backlight_change(lv_obj_t*obj,lv_event_t e){
 	if(obj!=slider||e!=LV_EVENT_VALUE_CHANGED)return;
 	int16_t val=lv_slider_get_value(slider);
 	if(val<=0)val=1;
 	lv_label_set_text_fmt(value,"%d%%",val);
+	guidrv_set_brightness(val);
+}
+
+static void backlight_click(lv_obj_t*obj,lv_event_t e){
+	if(e!=LV_EVENT_CLICKED)return;
+	int16_t val=lv_slider_get_value(slider);
+	if(obj==arr_left)val--;
+	else if(obj==arr_right)val++;
+	else return;
+	if(val<=1)val=1;
+	if(val>=100)val=100;
+	lv_label_set_text_fmt(value,"%d%%",val);
+	lv_slider_set_value(slider,val,LV_ANIM_ON);
 	guidrv_set_brightness(val);
 }
 
@@ -27,6 +40,7 @@ static void ok_msg_click(lv_obj_t*obj,lv_event_t e){
 }
 
 static int backlight_menu_draw(struct gui_activity*act){
+	int bts=gui_font_size+(gui_dpi/8);
 	int level=guidrv_get_brightness();
 	if(level<0)lv_create_ok_msgbox(act->page,ok_msg_click,_("get brightness failed: %m"));
 	else{
@@ -57,9 +71,58 @@ static int backlight_menu_draw(struct gui_activity*act){
 		lv_obj_align(slider,value,LV_ALIGN_OUT_BOTTOM_MID,gui_font_size,gui_font_size);
 		lv_obj_set_width(slider,lv_obj_get_width(box)-gui_font_size*4);
 
-		lv_obj_set_height(box,lv_obj_get_y(slider)+lv_obj_get_height(slider)+(gui_font_size*2));
+		static lv_style_t btn_style;
+		lv_style_init(&btn_style);
+		lv_style_set_radius(&btn_style,LV_STATE_DEFAULT,2);
+		lv_style_set_outline_width(&btn_style,LV_STATE_PRESSED,0);
+
+		arr_left=lv_btn_create(box,NULL);
+		lv_obj_set_size(arr_left,bts,bts);
+		lv_obj_set_event_cb(arr_left,backlight_click);
+		lv_obj_align(arr_left,slider,LV_ALIGN_OUT_BOTTOM_LEFT,0,gui_font_size);
+		lv_obj_add_style(arr_left,LV_BTN_PART_MAIN,&btn_style);
+		lv_obj_add_state(arr_left,LV_STATE_CHECKED);
+		lv_label_set_text(lv_label_create(arr_left,NULL),LV_SYMBOL_LEFT);
+
+		arr_right=lv_btn_create(box,NULL);
+		lv_obj_set_size(arr_right,bts,bts);
+		lv_obj_set_event_cb(arr_right,backlight_click);
+		lv_obj_align(arr_right,slider,LV_ALIGN_OUT_BOTTOM_RIGHT,0,gui_font_size);
+		lv_obj_add_style(arr_right,LV_BTN_PART_MAIN,&btn_style);
+		lv_obj_add_state(arr_right,LV_STATE_CHECKED);
+		lv_label_set_text(lv_label_create(arr_right,NULL),LV_SYMBOL_RIGHT);
+
+		lv_obj_set_height(box,
+			lv_obj_get_y(slider)+
+			lv_obj_get_height(slider)+
+			(gui_font_size*2)+
+			bts
+		);
 		lv_obj_align(box,NULL,LV_ALIGN_CENTER,0,0);
 	}
+	return 0;
+}
+
+static int backlight_get_focus(struct gui_activity*d __attribute__((unused))){
+	if(guidrv_get_brightness()<0){
+		lv_group_add_msgbox(gui_grp,d->page,true);
+		return 0;
+	}
+	lv_group_add_obj(gui_grp,slider);
+	lv_group_add_obj(gui_grp,arr_left);
+	lv_group_add_obj(gui_grp,arr_right);
+	lv_group_focus_obj(slider);
+	return 0;
+}
+
+static int backlight_lost_focus(struct gui_activity*d __attribute__((unused))){
+	if(guidrv_get_brightness()<0){
+		lv_group_remove_msgbox(d->page);
+		return 0;
+	}
+	lv_group_remove_obj(slider);
+	lv_group_remove_obj(arr_left);
+	lv_group_remove_obj(arr_right);
 	return 0;
 }
 
@@ -69,6 +132,8 @@ struct gui_register guireg_backlight={
 	.icon="backlight.png",
 	.show_app=true,
 	.draw=backlight_menu_draw,
+	.lost_focus=backlight_lost_focus,
+	.get_focus=backlight_get_focus,
 	.back=true,
 	.mask=true,
 };

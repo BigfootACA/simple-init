e24b74afe5d8eec1ce7e3dbd4d4e31f20783ebb2
BigfootACA <bigfoot@classfun.cn>
2021-09-30 18:54:15 +0800

    gadget: service.c: rewrite to support confd

--- src/gadget/service.c
+++ src/gadget/service.c
@@ -1,45 +1,130 @@
 #define _GNU_SOURCE
 #include<string.h>
+#include<stdlib.h>
 #include"service.h"
 #include"version.h"
 #include"gadget.h"
 #include"logger.h"
+#include"confd.h"
 #include"adbd.h"
 #define TAG "gadget"
 
-static struct gadget g={
-	.name    = "gadget",
-	.vendor  = 0x05F9,
-	.product = 0x5016,
-	.strings = GADGET_STRARRAY{GADGET_STR(0x409,"Linux",NAME,"1234567890"),NULL},
-	.configs = GADGET_CFGARRAY{
-		&(gadget_cfg){
-			.strings=GADGET_CFGSTRARRAY{GADGET_CFGSTR(0x409,NAME),NULL},
-			.max_power=500,
-			.type="a",
-			.id=1
-		},NULL
-	}
-};
+static char*base="gadget.func";
+
+static void init_gadget_conf(){
+	char*serial;
+	if(!(serial=confd_get_string("cmdline.serial","1234567890")))return;
+
+	confd_set_string("gadget.name","gadget");
+	confd_set_integer("gadget.id_vendor",0x0519);
+	confd_set_integer("gadget.id_product",0x5016);
+	confd_set_string("gadget.manufacturer","Linux");
+	confd_set_string("gadget.product",NAME);
+	confd_set_string("gadget.serial",serial);
+	confd_set_string("gadget.config",NAME);
+
+	confd_set_string_array(base,0,"name","usb0");
+	confd_set_string_array(base,0,"func","rndis");
+	confd_set_string_array(base,0,"mode","generic");
+
+	confd_set_string_array(base,1,"name","ttyGS0");
+	confd_set_string_array(base,1,"func","acm");
+	confd_set_string_array(base,1,"mode","generic");
+
+	confd_set_string_array(base,2,"name","adb");
+	confd_set_string_array(base,2,"func","ffs");
+	confd_set_string_array(base,2,"path",_PATH_RUN"/adb");
+	confd_set_string_array(base,2,"mode","adbd");
+
+	free(serial);
+}
 
 static int gadget_startup(struct service*svc __attribute__((unused))){
+	struct gadget_string gs={
+		.id=0x409,.serialnumber=NULL,
+		.product=NULL,.manufacturer=NULL,
+	};
+	gadget_cfgstr gcs={.id=0x409,.configuration=NULL};
+	gadget_cfg gc={
+		.strings=GADGET_CFGSTRARRAY{&gcs,NULL},
+		.max_power=500,.type="a",.id=1,
+	};
+	struct gadget g={
+		.strings = GADGET_STRARRAY{&gs,NULL},
+		.configs = GADGET_CFGARRAY{&gc,NULL},
+	};
+	char**items,*item,*udc;
+	open_default_confd_socket(TAG);
+	if(confd_get_type("gadget")!=TYPE_KEY)init_gadget_conf();
+	if(
+		!(g.name=confd_get_string("gadget.name",NULL))||
+		(g.vendor=confd_get_integer("gadget.id_vendor",0))==0||
+		(g.product=confd_get_integer("gadget.id_product",0))==0||
+		!(gs.manufacturer=confd_get_string("gadget.manufacturer",NULL))||
+		!(gs.product=confd_get_string("gadget.product",NULL))||
+		!(gs.serialnumber=confd_get_string("gadget.serial",NULL))||
+		!(gcs.configuration=confd_get_string("gadget.config",NULL))
+	){
+		tlog_error("invalid config");
+		goto fail;
+	}
 	tlog_info("register gadget");
-	if(gadget_register(&g)<0)
-		return trlog_error(-1,"failed to register gadget");
-	if(gadget_add_function(&g,&(gadget_func){
-		.name="usb0",
-		.function="rndis"
-	})<0)tlog_warn("failed to add rndis function");
-	if(gadget_add_function(&g,&(gadget_func){
-		.name="ttyGS0",
-		.function="acm"
-	})<0)tlog_warn("failed to add serial function");
-	if(gadget_add_func_adbd(&g,"adb",_PATH_RUN"/adb")<0)
-		tlog_warn("failed to add adbd function");
-	if(gadget_start_auto(&g)<0)
-		return trlog_error(-1,"failed to start gadget");
+	if(gadget_register(&g)<0){
+		tlog_error("failed to register gadget");
+		goto fail;
+	}
+	if(!(items=confd_ls(base))){
+		tlog_error("failed to read functions list");
+		goto fail;
+	}
+	for(int i=0;(item=items[i]);i++){
+		gadget_func f={0};
+		char*path,*mode;
+		f.name=confd_get_string_dict(base,item,"name",NULL);
+		f.function=confd_get_string_dict(base,item,"func",NULL);
+		path=confd_get_string_dict(base,item,"path",NULL);
+		mode=confd_get_string_dict(base,item,"mode","generic");
+		if(!f.name||!f.function||!mode){
+			tlog_warn("invalid name or func, skip func %d",i);
+			goto cont;
+		}
+		if(strcmp(mode,"generic")==0){
+			if(gadget_add_function(&g,&f)<0){
+				tlog_warn("add gadget func %s failed",item);
+			}
+		}else if(strcmp(mode,"adbd")==0){
+			if(!path){
+				tlog_warn("no path specified for adbd");
+				goto cont;
+			}
+			if(gadget_add_func_adbd(&g,f.name,path)<0){
+				tlog_warn("add gadget adbd func %s failed",item);
+			}
+		}else tlog_warn("unknown gadget mode %s, skip func %s",mode,item);
+		cont:
+		if(f.name)free(f.name);
+		if(f.function)free(f.function);
+		if(path)free(path);
+		if(mode)free(mode);
+	}
+	if((udc=confd_get_string("cmdline.udc",NULL))){
+		if(gadget_start(&g,udc)<0)
+			tlog_warn("start gadget failed with %s",udc);
+		free(udc);
+	}
+	if(gadget_start_auto(&g)<0){
+		tlog_error("failed to start gadget");
+		goto fail;
+	}
 	tlog_info("usb gadget initialized");
 	return 0;
+	fail:
+	if(g.name)free(g.name);
+	if(gs.product)free(gs.product);
+	if(gs.manufacturer)free(gs.manufacturer);
+	if(gs.serialnumber)free(gs.serialnumber);
+	if(gcs.configuration)free(gcs.configuration);
+	return -1;
 }
 
 int register_gadget_service(){

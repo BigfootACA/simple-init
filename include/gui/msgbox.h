420823c1e7bd07c2794f23a9d6c6aa12e50ad0e7
BigfootACA <bigfoot@classfun.cn>
2021-10-08 00:25:41 +0800

    include: gui: msgbox.h: add format attributes

--- include/gui/msgbox.h
+++ include/gui/msgbox.h
@@ -2,8 +2,8 @@
 #define _MSGBOX_H
 #include"gui.h"
 typedef bool(*msgbox_callback)(uint16_t id,const char*btn);
-extern void msgbox_create_yesno(msgbox_callback callback,const char*content,...);
-extern void msgbox_create_ok(msgbox_callback callback,const char*content,...);
-extern void msgbox_create_custom(msgbox_callback callback,const char**btn,const char*content,...);
-extern void msgbox_alert(const char*content,...);
+extern void msgbox_create_yesno(msgbox_callback callback,const char*content,...) __attribute__((format(printf,2,3)));
+extern void msgbox_create_ok(msgbox_callback callback,const char*content,...) __attribute__((format(printf,2,3)));
+extern void msgbox_create_custom(msgbox_callback callback,const char**btn,const char*content,...) __attribute__((format(printf,3,4)));
+extern void msgbox_alert(const char*content,...) __attribute__((format(printf,1,2)));
 #endif

e5b322a39773f11044c4120c9d96a9e1c4321a08
BigfootACA <bigfoot@classfun.cn>
2021-10-13 02:34:03 +0800

    confd: store.c: add store lock

--- src/confd/store.c
+++ src/confd/store.c
@@ -3,6 +3,18 @@
 #include<stdlib.h>
 #include"confd_internal.h"
 #include"logger.h"
+#ifdef ENABLE_UEFI
+static void*store_lock;
+#define LOCK_INIT(lock) (void)lock
+#define LOCK(lock) (void)lock
+#define UNLOCK(lock) (void)lock
+#else
+#include<pthread.h>
+static pthread_mutex_t store_lock;
+#define LOCK_INIT(lock) pthread_mutex_init(&lock,NULL)
+#define LOCK(lock) pthread_mutex_lock(&lock)
+#define UNLOCK(lock) pthread_mutex_unlock(&lock)
+#endif
 
 static struct conf conf_store={.type=TYPE_KEY};
 
@@ -45,24 +57,39 @@ static struct conf*conf_lookup(const char*path,bool create,enum conf_type type){
 	if(strcmp(path,"/")==0&&type==0)return &conf_store;
 	else strncpy(xpath,path,PATH_MAX-1);
 	if(!path[0]&&type==0)return &conf_store;
+	LOCK(store_lock);
 	if(p)do{
 		if(*p!='.')continue;
 		*p=0;
 		if(!(x=conf_get(cur,key))){
-			if(!create)EPRET(ENOENT);
-			x=conf_create(cur,key);
-			if(!x)return NULL;
+			if(!create){
+				UNLOCK(store_lock);
+				EPRET(ENOENT);
+			}
+			if(!(x=conf_create(cur,key))){
+				UNLOCK(store_lock);
+				return NULL;
+			}
 			x->type=TYPE_KEY;
 		}
 		cur=x,key=++p;
 	}while(*p++);
-	if(!key[0])EPRET(EINVAL);
+	if(!key[0]){
+		UNLOCK(store_lock);
+		EPRET(EINVAL);
+	}
 	if(!(x=conf_get(cur,key))){
-		if(!create)EPRET(ENOENT);
-		x=conf_create(cur,key);
-		if(!x)return NULL;
+		if(!create){
+			UNLOCK(store_lock);
+			EPRET(ENOENT);
+		}
+		if(!(x=conf_create(cur,key))){
+			UNLOCK(store_lock);
+			return NULL;
+		}
 		x->type=type;
 	}
+	UNLOCK(store_lock);
 	if(x->type==0)EPRET(EBADMSG);
 	if(type!=0&&type!=x->type)EPRET(ENOENT);
 	return x;
@@ -92,23 +119,29 @@ const char**conf_ls(const char*path){
 	struct conf*c=conf_lookup(path,false,0);
 	if(!c)return NULL;
 	if(c->type!=TYPE_KEY)EPRET(ENOTDIR);
+	LOCK(store_lock);
 	int i=list_count(c->keys),x=0;
 	if(i<0)i=0;
 	size_t s=sizeof(char*)*(i+1);
 	const char**r=malloc(s);
-	if(!r)EPRET(ENOMEM);
+	if(!r){
+		UNLOCK(store_lock);
+		EPRET(ENOMEM);
+	}
 	memset(r,0,s);
 	list*p=list_first(c->keys);
 	if(p)do{
 		LIST_DATA_DECLARE(d,p,struct conf*);
 		r[x++]=d->name;
 	}while((p=p->next));
+	UNLOCK(store_lock);
 	return r;
 }
 
 static int conf_del_obj(struct conf*c){
 	if(!c)return -1;
 	if(!c->parent)ERET(EINVAL);
+	LOCK(store_lock);
 	list*p;
 	if(c->type==TYPE_KEY){
 		list*x;
@@ -126,6 +159,7 @@ static int conf_del_obj(struct conf*c){
 		break;
 	}while((p=p->next));
 	free(c);
+	UNLOCK(store_lock);
 	return 0;
 }
 
@@ -138,8 +172,13 @@ int conf_del(const char*path){
 	int conf_set_##_func(const char*path,_type data){\
 		struct conf*c=conf_lookup(path,true,TYPE_##_tag);\
 		if(!c)return -errno;\
-		if(c->type!=TYPE_##_tag)ERET(EBADMSG);\
+		LOCK(store_lock);\
+		if(c->type!=TYPE_##_tag){\
+			UNLOCK(store_lock);\
+			ERET(EBADMSG);\
+		}\
 		VALUE_##_tag(c)=data;\
+		UNLOCK(store_lock);\
 		return 0;\
 	}\
 	_type conf_get_##_func(const char*path,_type def){\

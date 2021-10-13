f2207a1dd23bcfa4afd6a81720c98450c88af48b
BigfootACA <bigfoot@classfun.cn>
2021-10-14 01:33:32 +0800

    compatible: add errno.c

--- src/compatible/include/compatible.h
+++ src/compatible/include/compatible.h
@@ -1,6 +1,7 @@
 #ifndef _COMPATIBLE_H
 #define _COMPATIBLE_H
 #include<stddef.h>
+extern int efi_status_to_errno(EFI_STATUS st);
 extern char*comp_strdup(const char*s);
 extern char*comp_strndup(const char*s,size_t n);
 extern size_t comp_strnlen(const char*s,size_t n);

e7e18bc818296eed4727a5f44227c7e136c26acc
BigfootACA <bigfoot@classfun.cn>
2021-11-03 01:57:14 +0800

    cmdline: options.c: add cmdline option language

--- src/cmdline/console.c
+++ src/cmdline/console.c
@@ -8,8 +8,14 @@
 
 #define _GNU_SOURCE
 #include"confd.h"
+#include"language.h"
 
 int cmdline_console_shell(char*k __attribute__((unused)),char*v __attribute__((unused))){
 	confd_set_boolean("runtime.cmdline.console_shell",true);
+	return 0;
+}
+
+int cmdline_language(char*k __attribute__((unused)),char*v){
+	lang_set(v);
 	return 0;
 }

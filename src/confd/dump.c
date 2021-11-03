8b4edc1b3b3b4fac8174aee3e6a89f3650b98ded
BigfootACA <bigfoot@classfun.cn>
2021-11-03 09:49:14 +0800

    confd: dump.c: show config summary

--- src/confd/dump.c
+++ src/confd/dump.c
@@ -7,12 +7,20 @@
  */
 
 #define _GNU_SOURCE
+#include<stdio.h>
 #include<string.h>
 #include"str.h"
 #include"list.h"
 #include"logger.h"
 #include"confd_internal.h"
 #define TAG "confd"
+
+static const char*conf_summary(struct conf*key){
+	static char buf[256];
+	memset(buf,0,256);
+	snprintf(buf,255,"SAVE:%s",BOOL2STR(key->save));
+	return buf;
+}
 
 static int dump(struct conf*key,char*name){
 	char path[PATH_MAX]={0};
@@ -21,6 +29,7 @@ static int dump(struct conf*key,char*name){
 		else snprintf(path,PATH_MAX-1,"%s.%s",name,key->name);
 	}
 	if(key->type==TYPE_KEY){
+		tlog_debug("  %s %s (key)\n",conf_summary(key),*path?path:"[ROOT]");
 		list*p=list_first(key->keys);
 		if(!p)return 0;
 		do{dump(LIST_DATA(p,struct conf*),path);}while((p=p->next));
@@ -32,11 +41,11 @@ static int dump(struct conf*key,char*name){
 				strncpy(x,p,252);
 				len=strlen(p);
 			}else strcpy(x,"(null)");
-			tlog_debug("  %s = \"%s\"%s %d bytes (string)\n", path,x,len>252?"...":"",len);
+			tlog_debug("  %s %s = \"%s\"%s %d bytes (string)\n", conf_summary(key),path,x,len>252?"...":"",len);
 		}break;
-		case TYPE_INTEGER: tlog_debug("  %s = %lld (integer)\n",   path,(long long int)VALUE_INTEGER(key));break;
-		case TYPE_BOOLEAN: tlog_debug("  %s = %s (boolean)\n",    path,BOOL2STR(VALUE_BOOLEAN(key)));break;
-		default:           tlog_debug("  %s = (Unknown)\n",       path);break;
+		case TYPE_INTEGER: tlog_debug("  %s %s = %lld (integer)\n",  conf_summary(key),path,(long long int)VALUE_INTEGER(key));break;
+		case TYPE_BOOLEAN: tlog_debug("  %s %s = %s (boolean)\n",    conf_summary(key),path,BOOL2STR(VALUE_BOOLEAN(key)));break;
+		default:           tlog_debug("  %s %s = (Unknown)\n",       conf_summary(key),path);break;
 	}
 	return 0;
 }

ecc42f61940a9f8209e1e8f6f6a60f528c22cbfa
BigfootACA <bigfoot@classfun.cn>
2021-11-06 20:09:36 +0800

    gui: interface: core: guiapp.c: use gui_app struct to store global variables

--- src/gui/interface/core/guiapp.c
+++ src/gui/interface/core/guiapp.c
@@ -21,13 +21,17 @@
 #include"gui/msgbox.h"
 #include"gui/activity.h"
 #define TAG "guiapp"
-static lv_obj_t*screen,*tabview,*author;
-static list*apps=NULL;
-static int app_num=0,app_page=-1,cur_page=0;
+struct gui_app{
+	lv_obj_t*screen,*tabview,*author;
+	list*apps;
+	int app_num,app_page,cur_page;
+	lv_group_focus_cb_t old_cb;
+};
 struct app_info{
 	int x,y,page,num;
 	struct gui_register*reg;
 	lv_obj_t*tab,*app;
+	struct gui_app*data;
 };
 
 static int clean_button(void*b){
@@ -38,60 +42,64 @@ static int clean_button(void*b){
 	return 0;
 }
 
-static void clean_buttons(){
-	list_free_all(apps,clean_button);
-	if(tabview)lv_obj_del(tabview);
-	apps=NULL,tabview=NULL;
-	app_num=0,app_page=-1;
+static void clean_buttons(struct gui_app*ga){
+	list_free_all(ga->apps,clean_button);
+	if(ga->tabview)lv_obj_del(ga->tabview);
+	ga->apps=NULL,ga->tabview=NULL;
+	ga->app_num=0,ga->app_page=-1;
 }
 
 static void click_btn(lv_obj_t*obj,lv_event_t e){
 	if(!obj||e!=LV_EVENT_CLICKED)return;
-	if(!guiact_is_active_page(screen))return;
 	struct app_info*ai=lv_obj_get_user_data(obj);
+	struct gui_app*ga=ai->data;
+	if(!guiact_is_active_page(ga->screen))return;
 	if(guiact_start_activity(ai->reg,NULL)!=0)
 		msgbox_alert("This function does not implemented");
 }
 
 static void tabview_cb(lv_obj_t*obj,lv_event_t e){
 	if(!obj||e!=LV_EVENT_VALUE_CHANGED)return;
-	cur_page=lv_tabview_get_tab_act(tabview);
-	confd_set_integer("gui.guiapp.page",cur_page);
+	struct gui_app*ga=lv_obj_get_user_data(obj);
+	ga->cur_page=lv_tabview_get_tab_act(ga->tabview);
+	confd_set_integer("gui.guiapp.page",ga->cur_page);
 }
 
-static void add_page(){
-	app_page++,app_num=0;
+static void add_page(struct gui_app*ga){
+	ga->app_page++,ga->app_num=0;
 	char name[16]={0};
-	snprintf(name,15,"%d",app_page);
-	if(!tabview){
-		tabview=lv_tabview_create(screen,NULL);
-		lv_obj_set_pos(tabview,0,0);
-		lv_obj_set_size(tabview,gui_sw,gui_sh-lv_obj_get_height(author)-gui_font_size);
-		lv_obj_set_event_cb(tabview,tabview_cb);
-		lv_tabview_set_btns_pos(tabview,LV_TABVIEW_TAB_POS_NONE);
+	snprintf(name,15,"%d",ga->app_page);
+	if(!ga->tabview){
+		ga->tabview=lv_tabview_create(ga->screen,NULL);
+		lv_obj_set_pos(ga->tabview,0,0);
+		lv_obj_set_size(ga->tabview,gui_sw,gui_sh-lv_obj_get_height(ga->author)-gui_font_size);
+		lv_obj_set_user_data(ga->tabview,ga);
+		lv_obj_set_event_cb(ga->tabview,tabview_cb);
+		lv_tabview_set_btns_pos(ga->tabview,LV_TABVIEW_TAB_POS_NONE);
 	}
-	lv_obj_t*t=lv_tabview_add_tab(tabview,name);
+	lv_obj_t*t=lv_tabview_add_tab(ga->tabview,name);
 	lv_page_set_scrlbar_mode(t,LV_SCROLLBAR_MODE_HIDE);
 }
 
-static void add_button(struct gui_register*p){
+static void add_button(struct gui_app*ga,struct gui_register*p){
 	if(!p->show_app)return;
-	lv_obj_t*scr=lv_tabview_get_tab(tabview,app_page);
+	lv_obj_t*scr=lv_tabview_get_tab(ga->tabview,ga->app_page);
 	lv_coord_t pw=lv_page_get_scrl_width(scr);
 	lv_coord_t ph=lv_page_get_scrl_height(scr);
 	lv_coord_t xnum=pw/gui_dpi*2,ynum=ph/gui_dpi;
-	lv_coord_t a=app_num%xnum,b=app_num/xnum;
+	lv_coord_t a=ga->app_num%xnum,b=ga->app_num/xnum;
 	lv_coord_t w=(pw-gui_font_size-8)/xnum,h=(ph-gui_font_size-8)/(ynum+1);
 	lv_coord_t xx=(w*a)+(gui_font_size/2),yy=(h*b)+(gui_font_size/2);
 	if(yy+h>ph){
-		add_page();
-		add_button(p);
+		add_page(ga);
+		add_button(ga,p);
 		return;
 	}
 	struct app_info*ai=malloc(sizeof(struct app_info));
 	if(!ai)return;
 	memset(ai,0,sizeof(struct app_info));
-	ai->page=app_page,ai->tab=scr,ai->x=a,ai->y=b,ai->num=app_num,ai->reg=p;
+	ai->data=ga,ai->page=ga->app_page,ai->tab=scr;
+	ai->x=a,ai->y=b,ai->num=ga->app_num,ai->reg=p;
 	ai->app=lv_objmask_create(scr,NULL);
 	lv_obj_set_click(ai->app,true);
 	lv_obj_set_drag_parent(ai->app,true);
@@ -105,7 +113,7 @@ static void add_button(struct gui_register*p){
 	lv_obj_set_style_local_radius(ai->app,LV_OBJMASK_PART_MAIN,LV_STATE_DEFAULT,10);
 	lv_obj_set_user_data(ai->app,ai);
 	lv_obj_set_event_cb(ai->app,click_btn);
-	if(guiact_is_active_page(screen))lv_group_add_obj(gui_grp,ai->app);
+	if(guiact_is_active_page(ga->screen))lv_group_add_obj(gui_grp,ai->app);
 
 	int ix=w-gui_font_size,im=gui_font_size/2;
 	lv_obj_t*icon_w=lv_objmask_create(ai->app,NULL);
@@ -140,31 +148,33 @@ static void add_button(struct gui_register*p){
 	lv_obj_set_style_local_pad_top(txt,LV_LABEL_PART_MAIN,LV_STATE_DEFAULT,gui_dpi/100);
 	lv_obj_set_height(ai->app,lv_obj_get_y(txt)+lv_obj_get_height(txt)+gui_font_size);
 
-	cur_page=confd_get_integer("gui.guiapp.page",0);
-	list_obj_add_new(&apps,ai);
-	app_num++;
+	ga->cur_page=confd_get_integer("gui.guiapp.page",0);
+	list_obj_add_new(&ga->apps,ai);
+	ga->app_num++;
 }
 
-static void redraw_apps(){
-	clean_buttons();
-	add_page();
+static void redraw_apps(struct gui_app*ga){
+	clean_buttons(ga);
+	add_page(ga);
 	for(int i=0;guiact_register[i];i++)
-		add_button(guiact_register[i]);
-	if(cur_page>=lv_tabview_get_tab_count(tabview))cur_page=0;
-	lv_tabview_set_tab_act(tabview,cur_page,LV_ANIM_OFF);
+		add_button(ga,guiact_register[i]);
+	if(ga->cur_page>=lv_tabview_get_tab_count(ga->tabview))ga->cur_page=0;
+	lv_tabview_set_tab_act(ga->tabview,ga->cur_page,LV_ANIM_OFF);
 }
 
-static void do_reload(lv_task_t*t __attribute__((unused))){
-	redraw_apps();
+static void do_reload(lv_task_t*t){
+	redraw_apps(t->user_data);
 }
 
-static int guiapp_get_focus(struct gui_activity*d __attribute__((unused))){
-	lv_task_once(lv_task_create(do_reload,100,LV_TASK_PRIO_MID,NULL));
+static int guiapp_get_focus(struct gui_activity*d){
+	struct gui_app*ga=d->data;
+	lv_task_once(lv_task_create(do_reload,100,LV_TASK_PRIO_MID,ga));
 	return 0;
 }
 
-static int guiapp_lost_focus(struct gui_activity*d __attribute__((unused))){
-	list*app=list_first(apps);
+static int guiapp_lost_focus(struct gui_activity*d){
+	struct gui_app*ga=d->data;
+	list*app=list_first(ga->apps);
 	if(app)do{
 		LIST_DATA_DECLARE(ai,app,struct app_info*);
 		lv_group_remove_obj(ai->app);
@@ -172,23 +182,38 @@ static int guiapp_lost_focus(struct gui_activity*d __attribute__((unused))){
 	return 0;
 }
 
+static int guiapp_init(struct gui_activity*act){
+	struct gui_app*ga=malloc(sizeof(struct gui_app));
+	if(!ga)return -ENOMEM;
+	memset(ga,0,sizeof(struct gui_app));
+	act->data=ga;
+	return 0;
+}
+
+static int guiapp_exit(struct gui_activity*act){
+	if(act->data)free(act->data);
+	act->data=NULL;
+	return 0;
+}
+
 static int guiapp_draw(struct gui_activity*act){
-	screen=act->page;
+	struct gui_app*ga=act->data;
+	ga->screen=act->page;
 	static lv_style_t txt_style;
 	lv_style_init(&txt_style);
 	lv_style_set_text_font(&txt_style,LV_STATE_DEFAULT,gui_font_small);
 	lv_style_set_text_color(&txt_style,LV_STATE_DEFAULT,lv_color_make(200,200,200));
 
-	author=lv_label_create(act->page,NULL);
-	lv_label_set_text(author,"Author: BigfootACA");
-	lv_label_set_long_mode(author,LV_LABEL_LONG_BREAK);
-	lv_label_set_align(author,LV_LABEL_ALIGN_CENTER);
-	lv_obj_add_style(author,LV_LABEL_PART_MAIN,&txt_style);
-	lv_obj_set_width(author,gui_sw);
-	lv_obj_align(author,NULL,LV_ALIGN_IN_BOTTOM_MID,0,-gui_font_size);
+	ga->author=lv_label_create(act->page,NULL);
+	lv_label_set_text(ga->author,"Author: BigfootACA");
+	lv_label_set_long_mode(ga->author,LV_LABEL_LONG_BREAK);
+	lv_label_set_align(ga->author,LV_LABEL_ALIGN_CENTER);
+	lv_obj_add_style(ga->author,LV_LABEL_PART_MAIN,&txt_style);
+	lv_obj_set_width(ga->author,gui_sw);
+	lv_obj_align(ga->author,NULL,LV_ALIGN_IN_BOTTOM_MID,0,-gui_font_size);
 
-	list_free_all(apps,NULL);
-	apps=NULL,tabview=NULL,app_num=0,app_page=-1;
+	list_free_all(ga->apps,NULL);
+	ga->apps=NULL,ga->tabview=NULL,ga->app_num=0,ga->app_page=-1;
 	return 0;
 }
 
@@ -197,6 +222,8 @@ struct gui_register guireg_guiapp={
 	.title="GUI Application",
 	.icon="apps.png",
 	.show_app=false,
+	.init=guiapp_init,
+	.quiet_exit=guiapp_exit,
 	.get_focus=guiapp_get_focus,
 	.lost_focus=guiapp_lost_focus,
 	.draw=guiapp_draw,

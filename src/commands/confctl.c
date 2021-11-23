0fcdefcac4def849ea7f08b1a2cea161ccc7e838
BigfootACA <bigfoot@classfun.cn>
2021-11-23 09:22:37 +0800

    commands: confctl.c: add permission control support

--- src/commands/confctl.c
+++ src/commands/confctl.c
@@ -6,7 +6,9 @@
  *
  */
 
+#include<pwd.h>
 #include<errno.h>
+#include<fcntl.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<strings.h>
@@ -15,6 +17,7 @@
 #include"confd.h"
 #include"str.h"
 #include"getopt.h"
+#include"system.h"
 
 enum ctl_oper{
 	OPER_NONE,
@@ -25,17 +28,29 @@ enum ctl_oper{
 	OPER_SAVE,
 	OPER_LOAD,
 	OPER_SET_DEFAULT,
+	OPER_CHOWN,
+	OPER_CHGRP,
+	OPER_CHMOD,
+	OPER_GETOWN,
+	OPER_GETGRP,
+	OPER_GETMOD,
 };
 
 static int usage(int e){
 	return return_printf(
 		e,e==0?STDOUT_FILENO:STDERR_FILENO,
-		"Usage: confctl [OPTION] [KEY [VALUE]]\n"
+		"Usage: confctl [OPTION] [KEY [VALUE|OWNER|GROUP|MODE]]\n"
 		"Control init config daemon.\n\n"
 		"Options:\n"
 		"\t-s, --socket <SOCKET>  Use custom control socket (default is %s)\n"
 		"\t-d, --delete <KEY>     Delete item\n"
 		"\t-l, --list <KEY>       List items\n"
+		"\t-O, --chown            Change item owner\n"
+		"\t-G, --chgrp            Change item group\n"
+		"\t-M, --chmod            Change item mode\n"
+		"\t-o, --getown           Get item owner\n"
+		"\t-g, --getgrp           Get item group\n"
+		"\t-m, --getmod           Get item mode\n"
 		"\t-L, --load <PATH>      Load config from a file\n"
 		"\t-S, --save <PATH>      Save config to a file\n"
 		"\t-x, --path <PATH>      Set default config path\n"
@@ -113,6 +128,58 @@ static int do_get_set(char*key,char*value){
 	return value?confctl_do_set(key,value):confctl_do_get(key);
 }
 
+static int do_get_perm(enum ctl_oper op,char*key){
+	int r=0;
+	uid_t u=0;
+	gid_t g=0;
+	mode_t m=0;
+	char user[64]={0},group[64]={0};
+	switch(op){
+		case OPER_GETOWN:
+			r=confd_get_own(key,&u);
+			if(r!=0)fd_perror(STDERR_FILENO,_("get conf key '%s' owner failed"),key);
+			else printf("UID=%d\nUSER=%s\n",u,get_username(u,user,63));
+		break;
+		case OPER_GETGRP:
+			r=confd_get_grp(key,&g);
+			if(r!=0)fd_perror(STDERR_FILENO,_("get conf key '%s' group failed"),key);
+			else printf("GID=%d\nGROUP=%s\n",g,get_groupname(g,group,63));
+		break;
+		case OPER_GETMOD:
+			r=confd_get_mod(key,&m);
+			if(r!=0)fd_perror(STDERR_FILENO,_("get conf key '%s' mode failed"),key);
+			else printf("MODE=%04o\nPERM=%s\n",m,mode_string(m)+1);
+		break;
+		default:r=2;
+	}
+	return r;
+}
+
+static int do_set_perm(enum ctl_oper op,char*key,char*val){
+	int r;
+	char*end;
+	long l;
+	errno=0;
+	l=strtol(val,&end,op==OPER_CHMOD?8:10);
+	if((*end||val==end||errno!=0))return re_printf(2,"invalid argument: %s\n",val);
+	switch(op){
+		case OPER_CHOWN:
+			r=confd_set_own(key,(uid_t)l);
+			if(r!=0)fd_perror(STDERR_FILENO,_("set conf key '%s' owner failed"),key);
+		break;
+		case OPER_CHGRP:
+			r=confd_set_grp(key,(gid_t)l);
+			if(r!=0)fd_perror(STDERR_FILENO,_("set conf key '%s' group failed"),key);
+		break;
+		case OPER_CHMOD:
+			r=confd_set_mod(key,(mode_t)l);
+			if(r!=0)fd_perror(STDERR_FILENO,_("set conf key '%s' mode failed"),key);
+		break;
+		default:r=2;
+	}
+	return r;
+}
+
 int confctl_main(int argc,char**argv){
 	static const struct option lo[]={
 		{"help",    no_argument,       NULL,'h'},
@@ -124,12 +191,18 @@ int confctl_main(int argc,char**argv){
 		{"list",    required_argument, NULL,'l'},
 		{"delete",  required_argument, NULL,'d'},
 		{"socket",  required_argument, NULL,'s'},
+		{"chown",   no_argument,       NULL,'O'},
+		{"chgrp",   no_argument,       NULL,'G'},
+		{"chmod",   no_argument,       NULL,'M'},
+		{"getown",  no_argument,       NULL,'o'},
+		{"getgrp",  no_argument,       NULL,'g'},
+		{"getmod",  no_argument,       NULL,'m'},
 		{NULL,0,NULL,0}
 	};
 	char*socket=NULL,*key=NULL;
 	enum ctl_oper op=OPER_NONE;
 	int o;
-	while((o=b_getlopt(argc,argv,"hqDS:L:p:d:l:s:",lo,NULL))>0)switch(o){
+	while((o=b_getlopt(argc,argv,"hqDS:L:p:d:l:s:OGMogm",lo,NULL))>0)switch(o){
 		case 'h':return usage(0);
 		case 'q':
 			if(op!=OPER_NONE)goto conflict;
@@ -159,6 +232,31 @@ int confctl_main(int argc,char**argv){
 			if(op!=OPER_NONE)goto conflict;
 			op=OPER_SET_DEFAULT,key=b_optarg;
 		break;
+		case 'O':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_CHOWN;
+		break;
+		case 'G':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_CHGRP;
+		break;
+		case 'M':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_CHMOD;
+		break;
+		case 'o':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_GETOWN;
+		break;
+		case 'g':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_GETGRP;
+		break;
+
+		case 'm':
+			if(op!=OPER_NONE)goto conflict;
+			op=OPER_GETMOD;
+		break;
 		case 's':
 			if(socket)goto conflict;
 			socket=b_optarg;
@@ -167,10 +265,6 @@ int confctl_main(int argc,char**argv){
 	}
 	int ac=argc-b_optind;
 	char**av=argv+b_optind;
-	if(
-		(op==OPER_NONE&&ac<=0)||
-		(op!=OPER_NONE&&ac>0)
-	)return usage(1);
 	if(!socket)socket=DEFAULT_CONFD;
 	if(open_confd_socket(false,"confctl",socket)<0)return 2;
 	int r;
@@ -203,7 +297,22 @@ int confctl_main(int argc,char**argv){
 			r=confd_set_default_config(key);
 			if(errno>0)perror(_("set default config failed"));
 		break;
+		case OPER_CHOWN:
+		case OPER_CHGRP:
+		case OPER_CHMOD:
+			if(ac<2)return re_printf(2,"missing arguments\n");
+			if(ac>2)return re_printf(2,"too many arguments\n");
+			r=do_set_perm(op,av[0],av[1]);
+		break;
+		case OPER_GETOWN:
+		case OPER_GETGRP:
+		case OPER_GETMOD:
+			if(ac<1)return re_printf(2,"missing arguments\n");
+			if(ac>1)return re_printf(2,"too many arguments\n");
+			r=do_get_perm(op,av[0]);
+		break;
 		case OPER_NONE:{
+			if(ac<=0)return usage(1);
 			if(ac>2)return re_printf(2,"too many arguments\n");
 			r=do_get_set(av[0],ac==2?av[1]:NULL);
 		}break;

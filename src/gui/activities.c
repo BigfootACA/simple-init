51210df94594d8fdad9fca28ab9371cc95143faf
BigfootACA <bigfoot@classfun.cn>
2022-03-02 01:07:11 +0800

    gui: interface: apps: add uefi_dxe_load.c

--- src/gui/activities.c
+++ src/gui/activities.c
@@ -13,6 +13,7 @@ extern struct gui_register guireg_guipm_disk_select;
 extern struct gui_register guireg_guipm_partitions;
 extern struct gui_register guireg_picture_viewer;
 extern struct gui_register guireg_uefi_bootmenu;
+extern struct gui_register guireg_uefi_dxe_load;
 extern struct gui_register guireg_screen_test;
 extern struct gui_register guireg_boot_linux;
 extern struct gui_register guireg_uefi_start;
@@ -45,6 +46,7 @@ struct gui_register*guiact_register[]={
 	&guireg_linux_simple_mass_storage,
 	#endif
 	&guireg_uefi_bootmenu,
+	&guireg_uefi_dxe_load,
 	&guireg_uefi_start,
 	&guireg_uefi_shell,
 	&guireg_mouse_menu,
---
+++ src/gui/interface/apps/uefi_dxe_load.c
@@ -0,0 +1,97 @@
+/*
+ *
+ * Copyright (C) 2021 BigfootACA <bigfoot@classfun.cn>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#ifdef ENABLE_GUI
+#ifdef ENABLE_UEFI
+#include<stdlib.h>
+#include<Uefi.h>
+#include<Library/UefiLib.h>
+#include<Library/UefiBootServicesTableLib.h>
+#include<Protocol/LoadedImage.h>
+#include"gui.h"
+#include"logger.h"
+#include"language.h"
+#include"gui/tools.h"
+#include"gui/msgbox.h"
+#include"gui/activity.h"
+#define TAG "dxe-load"
+
+extern EFI_DEVICE_PATH_PROTOCOL*fs_get_device_path(const char*path);
+static void load_cb(lv_task_t*t){
+	char*full_path=t->user_data;
+	EFI_STATUS st;
+	EFI_HANDLE ih;
+	EFI_LOADED_IMAGE_PROTOCOL*li;
+	EFI_DEVICE_PATH_PROTOCOL*p=fs_get_device_path(full_path);
+	if(!p){
+		msgbox_alert("get device path failed");
+		tlog_warn("get device path failed");
+		return;
+	}
+	st=gBS->LoadImage(FALSE,gImageHandle,p,NULL,0,&ih);
+	if(EFI_ERROR(st)){
+		if(ih)gBS->UnloadImage(ih);
+		msgbox_alert("load image failed: %s",efi_status_to_string(st));
+		tlog_warn("load image failed: %s",efi_status_to_string(st));
+		return;
+	}
+	st=gBS->OpenProtocol(
+		ih,&gEfiLoadedImageProtocolGuid,
+		(VOID**)&li,gImageHandle,NULL,
+		EFI_OPEN_PROTOCOL_GET_PROTOCOL
+	);
+	if(EFI_ERROR(st)){
+		msgbox_alert("open protocol failed: %s",_(efi_status_to_string(st)));
+		tlog_warn("open protocol failed: %s",efi_status_to_string(st));
+		return;
+	}
+	if(
+		li->ImageCodeType!=EfiBootServicesCode&&
+		li->ImageCodeType!=EfiRuntimeServicesCode
+	){
+		if(ih)gBS->UnloadImage(ih);
+		msgbox_alert("not a UEFI Driver");
+		tlog_warn("not a UEFI Driver");
+		return;
+	}
+	st=gBS->StartImage(ih,NULL,NULL);
+	if(EFI_ERROR(st)){
+		if(ih)gBS->UnloadImage(ih);
+		msgbox_alert("start dxe failed: %s",_(efi_status_to_string(st)));
+		tlog_error("start dxe failed");
+	}
+}
+
+static bool confirm_click(uint16_t id,const char*text __attribute__((unused)),void*user_data){
+	if(id==0)lv_task_once(lv_task_create(load_cb,1,LV_TASK_PRIO_LOWEST,user_data));
+	return false;
+}
+
+static int uefi_dxe_load_draw(struct gui_activity*d){
+	if(!d)return -1;
+	static char full_path[PATH_MAX];
+	memset(full_path,0,sizeof(full_path));
+	strncpy(full_path,(char*)d->args,sizeof(full_path)-1);
+	msgbox_set_user_data(msgbox_create_yesno(
+		confirm_click,
+		"Load UEFI Driver (DXE) '%s'?",
+		full_path
+	),full_path);
+	return -10;
+}
+
+struct gui_register guireg_uefi_dxe_load={
+	.name="uefi-dxe-load",
+	.title="UEFI Driver (DXE) Load",
+	.icon="efi.svg",
+	.show_app=false,
+	.open_file=true,
+	.draw=uefi_dxe_load_draw,
+};
+#endif
+#endif

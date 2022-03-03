62fbf13eb12a177c53bf85032cb731f5a17ed48d
BigfootACA <bigfoot@classfun.cn>
2022-03-03 09:40:19 +0800

    lib: readable.c: rewrite make_readable_str_buf

--- src/lib/readable.c
+++ src/lib/readable.c
@@ -1,26 +1,26 @@
+/*
+ *
+ * Copyright (C) 2021 BigfootACA <bigfoot@classfun.cn>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
 #define _GNU_SOURCE
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include"defines.h"
+#include"str.h"
 
 const char*make_readable_str_buf(char*buf,size_t len,unsigned long long val,unsigned long block_size,unsigned long display){
-	static const char units[]="\0KMGTPEZY";
-	unsigned frac=0;
-	const char*u=units,*fmt="%llu";
+	int unit=0;
 	memset(buf,0,len);
 	if(val==0)return strncpy(buf,"0",len-1);
 	if(block_size>1)val*=block_size;
 	if(display)val+=display/2,val/=display;
-	else{
-		while((val>=1024))fmt="%llu.%u%c",u++,frac=(((unsigned)val%1024)*10+1024/2)/1024,val/=1024;
-		if(frac>=10)++val,frac=0;
-		if(block_size==0){
-			if(frac>=5)++val;
-			fmt="%llu%*c",frac=1;
-		}
-	}
-	snprintf(buf,len-1,fmt,val,frac,*u);
+	else while((val>=1024))val/=1024,unit++;
+	snprintf(buf,len-1,"%llu %s",val,size_units[unit]);
 	return buf;
 }
 

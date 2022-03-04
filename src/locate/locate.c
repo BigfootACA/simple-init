9aeb0a22092cfff82391939eb090da2ecd33b191
BigfootACA <bigfoot@classfun.cn>
2022-03-05 00:20:37 +0800

    locate: locate.c: add boot_locate_create_file

--- src/locate/locate.c
+++ src/locate/locate.c
@@ -226,7 +226,7 @@ static locate_dest*init_locate_ret(locate_ret*ret,const char*file){
 	return loc;
 }
 
-static bool locate_open_file(locate_dest*loc,locate_ret*ret){
+static bool locate_open_file(locate_dest*loc,locate_ret*ret,UINT64 mode){
 	EFI_STATUS st;
 	if(!ret->path[0]){
 		tlog_warn("no path specified");
@@ -244,10 +244,7 @@ static bool locate_open_file(locate_dest*loc,locate_ret*ret){
 		ret->path,ret->path16,
 		sizeof(ret->path16)/sizeof(CHAR16)
 	);
-	st=loc->root->Open(
-		loc->root,&ret->file,ret->path16,
-		EFI_FILE_MODE_READ,0
-	);
+	st=loc->root->Open(loc->root,&ret->file,ret->path16,mode,0);
 	if(EFI_ERROR(st)){
 		tlog_error(
 			"open file '%s' failed: %s",
@@ -286,11 +283,18 @@ static bool locate_open_block(locate_dest*loc,locate_ret*ret){
 bool boot_locate(locate_ret*ret,const char*file){
 	locate_dest*loc=init_locate_ret(ret,file);
 	if(loc)switch(ret->type){
-		case LOCATE_FILE:return locate_open_file(loc,ret);
+		case LOCATE_FILE:return locate_open_file(loc,ret,EFI_FILE_MODE_READ);
 		case LOCATE_BLOCK:return locate_open_block(loc,ret);
 		default:;
 	}
 	return false;
+}
+
+bool boot_locate_create_file(locate_ret*ret,const char*file){
+	locate_dest*loc=init_locate_ret(ret,file);
+	return loc&&ret->type==LOCATE_FILE&&locate_open_file(loc,ret,
+		EFI_FILE_MODE_READ|EFI_FILE_MODE_WRITE|EFI_FILE_MODE_CREATE
+	);
 }
 
 char*locate_find_name(char*buf,size_t len){

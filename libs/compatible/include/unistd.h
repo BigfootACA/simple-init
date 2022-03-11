55885fd40961d951a64bb0d2bac0e84ded71eabe
BigfootACA <bigfoot@classfun.cn>
2022-03-12 00:17:28 +0800

    libs: compatible: include: unistd.h: add close read write

--- libs/compatible/include/unistd.h
+++ libs/compatible/include/unistd.h
@@ -1,5 +1,11 @@
 #ifndef	_UNISTD_H
 #define _UNISTD_H
+#define __NEED_size_t
+#define __NEED_ssize_t
+#include <bits/alltypes.h>
 #define usleep comp_usleep
 int usleep(unsigned);
+int close(int);
+ssize_t read(int, void *, size_t);
+ssize_t write(int, const void *, size_t);
 #endif

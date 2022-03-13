0b7eb43d36322e951c6d69310387f67b2cf440d4
BigfootACA <bigfoot@classfun.cn>
2022-03-13 14:05:48 +0800

    libs: add mxml

---
+++ libs/mxml-uefi/config.h
@@ -0,0 +1,76 @@
+/*
+ * Xcode configuration file for Mini-XML, a small XML file parsing library.
+ *
+ * https://www.msweet.org/mxml
+ *
+ * Copyright © 2003-2020 by Michael R Sweet.
+ *
+ * Licensed under Apache License v2.0.  See the file "LICENSE" for more
+ * information.
+ */
+
+#define _GNU_SOURCE
+
+/*
+ * Include necessary headers...
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+
+/*
+ * Version number...
+ */
+
+#define MXML_VERSION "Mini-XML v3.3"
+
+
+/*
+ * Inline function support...
+ */
+
+#define inline
+
+
+/*
+ * Long long support...
+ */
+
+#define HAVE_LONG_LONG 1
+
+
+/*
+ * Do we have the *printf() functions?
+ */
+
+#define HAVE_SNPRINTF 1
+#define HAVE_VASPRINTF 1
+#define HAVE_VSNPRINTF 1
+
+
+/*
+ * Do we have the strXXX() functions?
+ */
+
+#define HAVE_STRDUP 1
+#define HAVE_STRLCAT 1
+#define HAVE_STRLCPY 1
+
+
+/*
+ * Do we have threading support?
+ */
+
+#undef HAVE_PTHREAD_H
+
+
+/*
+ * Define prototypes for string functions as needed...
+ */
+
+extern char	*_mxml_strdupf(const char *, ...);
+extern char	*_mxml_vstrdupf(const char *, va_list);
---
+++ libs/mxml.cmake
@@ -0,0 +1,34 @@
+include(CheckTypeSize)
+check_type_size("long long" HAVE_LONG_LONG)
+include(CheckFunctionExists)
+check_function_exists(snprintf HAVE_SNPRINTF)
+check_function_exists(vasprintf HAVE_VASPRINTF)
+check_function_exists(vsnprintf HAVE_VSNPRINTF)
+check_function_exists(strdup HAVE_STRDUP)
+check_function_exists(strlcat HAVE_STRLCAT)
+check_function_exists(strlcpy HAVE_STRLCPY)
+set(MXML_VERSION "Mini-XML v3.3")
+configure_file(libs/mxml.config.h.in mxml/config.h)
+set(MXML_HEADERS
+	libs/mxml/mxml-private.h
+	${CMAKE_CURRENT_BINARY_DIR}/mxml/config.h
+)
+set(MXML_SOURCES
+	libs/mxml/mxml-attr.c
+	libs/mxml/mxml-entity.c
+	libs/mxml/mxml-file.c
+	libs/mxml/mxml-get.c
+	libs/mxml/mxml-index.c
+	libs/mxml/mxml-node.c
+	libs/mxml/mxml-private.c
+	libs/mxml/mxml-search.c
+	libs/mxml/mxml-set.c
+	libs/mxml/mxml-string.c
+)
+add_library(mxml ${MXML_SOURCES} ${MXML_HEADERS})
+target_include_directories(mxml PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/mxml)
+target_compile_options(mxml PRIVATE
+	-Wno-sign-compare
+	-Wno-implicit-fallthrough
+)
+include_directories(libs/mxml)

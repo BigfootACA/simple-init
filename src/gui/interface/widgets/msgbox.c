616e3332c642328a8d1f82d6fb87d92a21582856
BigfootACA <bigfoot@classfun.cn>
2022-03-19 12:40:54 +0800

    gui: interface: widgets: msgbox.c: add resize

--- src/gui/interface/widgets/msgbox.c
+++ src/gui/interface/widgets/msgbox.c
@@ -10,7 +10,6 @@
 #include<stdlib.h>
 #include"gui.h"
 #include"logger.h"
-#include"gui/tools.h"
 #include"gui/msgbox.h"
 #include"gui/activity.h"
 
@@ -19,7 +18,6 @@ struct msgbox{
 	const char**buttons;
 	msgbox_callback callback;
 	uint16_t btn_cnt;
-	lv_obj_t*mask;
 	lv_obj_t*box;
 	lv_obj_t*label;
 	lv_obj_t*btn[64];
@@ -59,51 +57,28 @@ static int msgbox_clean(struct gui_activity*act){
 	return 0;
 }
 
-static int msgbox_draw(struct gui_activity*act){
+static int msgbox_resize(struct gui_activity*act){
 	lv_coord_t box_h=0;
 	lv_coord_t max_w=gui_dpi*4,cur_w=gui_sw/4*3,xw=MIN(max_w,cur_w);
 	lv_coord_t max_h=gui_dpi*6,cur_h=gui_sh/3*2,xh=MIN(max_h,cur_h);
 	struct msgbox*box=act->args;
-	if(!box->buttons)box->buttons=null_btn;
-	for(box->btn_cnt=0;*box->buttons[box->btn_cnt];box->btn_cnt++);
-	if(box->btn_cnt>64)return 3;
-	box->act=act;
-
-	box->mask=lv_create_opa_mask(act->page);
-	box->box=lv_page_create(box->mask,NULL);
-	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_DEFAULT,0);
-	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_PRESSED,0);
-	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_FOCUSED,0);
+	static bool style_init=false;
+	static lv_style_t style;
+	if(!style_init)lv_style_init(&style);
 	lv_obj_set_width(box->box,xw);
-
-	box->label=lv_label_create(box->box,NULL);
-	lv_label_set_align(box->label,LV_LABEL_ALIGN_CENTER);
-	lv_label_set_long_mode(box->label,LV_LABEL_LONG_BREAK);
 	lv_obj_set_width(box->label,lv_page_get_scrl_width(box->box));
-	lv_label_set_text(box->label,box->text);
 	box_h+=lv_obj_get_height(box->label);
-
 	if(box->btn_cnt>0){
 		lv_coord_t
 			btn_m=gui_font_size/2,
 			btn_w=lv_page_get_scrl_width(box->box)-(gui_dpi/16),
 			btn_h=gui_font_size+(gui_dpi/8);
 		box_h+=btn_m;
-		struct msgbox_btn*d=malloc(sizeof(struct msgbox_btn)*box->btn_cnt);
-		if(!d){
-			free(box);
-			return 4;
-		}
-		box->btn_data_p=d;
+		lv_style_set_margin_bottom(&style,LV_STATE_DEFAULT,btn_m);
+		lv_style_set_radius(&style,LV_STATE_DEFAULT,gui_dpi/15);
 		for(uint16_t i=0;i<box->btn_cnt;i++){
-			d[i].box=box,d[i].id=i,d[i].text=box->buttons[i];
-			box->btn[i]=lv_btn_create(box->box,NULL);
-			lv_label_set_text(lv_label_create(box->btn[i],NULL),_(box->buttons[i]));
-			lv_obj_set_style_local_margin_bottom(box->btn[i],LV_BTN_PART_MAIN,LV_STATE_DEFAULT,btn_m);
-			lv_obj_set_style_local_radius(box->btn[i],LV_BTN_PART_MAIN,LV_STATE_DEFAULT,gui_dpi/15);
+			lv_obj_add_style(box->btn[i],LV_BTN_PART_MAIN,&style);
 			lv_obj_set_size(box->btn[i],btn_w-btn_m,btn_h);
-			lv_obj_set_user_data(box->btn[i],&d[i]);
-			lv_obj_set_event_cb(box->btn[i],msg_click);
 		}
 		if(box->btn_cnt<=5){
 			btn_w/=box->btn_cnt,box_h+=btn_m;
@@ -120,7 +95,41 @@ static int msgbox_draw(struct gui_activity*act){
 	box_h+=gui_dpi/3;
 	lv_obj_set_height(box->box,MIN(box_h,xh));
 	lv_obj_align(box->box,NULL,LV_ALIGN_CENTER,0,0);
+	return 0;
+}
 
+static int msgbox_draw(struct gui_activity*act){
+	struct msgbox*box=act->args;
+	if(!box->buttons)box->buttons=null_btn;
+	for(box->btn_cnt=0;*box->buttons[box->btn_cnt];box->btn_cnt++);
+	if(box->btn_cnt>64)return 3;
+	box->act=act;
+
+	box->box=lv_page_create(act->page,NULL);
+	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_DEFAULT,0);
+	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_PRESSED,0);
+	lv_obj_set_style_local_border_width(box->box,LV_PAGE_PART_BG,LV_STATE_FOCUSED,0);
+
+	box->label=lv_label_create(box->box,NULL);
+	lv_label_set_align(box->label,LV_LABEL_ALIGN_CENTER);
+	lv_label_set_long_mode(box->label,LV_LABEL_LONG_BREAK);
+	lv_label_set_text(box->label,box->text);
+
+	if(box->btn_cnt>0){
+		struct msgbox_btn*d=malloc(sizeof(struct msgbox_btn)*box->btn_cnt);
+		if(!d){
+			free(box);
+			return 4;
+		}
+		box->btn_data_p=d;
+		for(uint16_t i=0;i<box->btn_cnt;i++){
+			d[i].box=box,d[i].id=i,d[i].text=box->buttons[i];
+			box->btn[i]=lv_btn_create(box->box,NULL);
+			lv_label_set_text(lv_label_create(box->btn[i],NULL),_(box->buttons[i]));
+			lv_obj_set_user_data(box->btn[i],&d[i]);
+			lv_obj_set_event_cb(box->btn[i],msg_click);
+		}
+	}
 	return 0;
 }
 
@@ -146,6 +155,7 @@ struct gui_register guireg_msgbox={
 	.show_app=false,
 	.quiet_exit=msgbox_clean,
 	.draw=msgbox_draw,
+	.resize=msgbox_resize,
 	.get_focus=msgbox_get_focus,
 	.lost_focus=msgbox_lost_focus,
 	.back=true,

6f7142535e91962e29323ba671290f05a7871c91
BigfootACA <bigfoot@classfun.cn>
2022-03-21 22:29:44 +0800

    gui: drivers: uefi_touch.c: add touch hotplug support

--- src/gui/drivers/uefi_touch.c
+++ src/gui/drivers/uefi_touch.c
@@ -15,13 +15,17 @@
 #include<stddef.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<Library/UefiLib.h>
 #include<Library/UefiBootServicesTableLib.h>
 #include<Protocol/AbsolutePointer.h>
 #define TAG "uefitouch"
 #include"gui.h"
+#include"list.h"
 #include"logger.h"
 #include"defines.h"
 #include"gui/guidrv.h"
+static void*event_reg=NULL;
+static list*touchs;
 struct input_data{
 	bool lp,oldp;
 	INT64 rx,lx,oldx;
@@ -29,10 +33,21 @@ struct input_data{
 	EFI_ABSOLUTE_POINTER_PROTOCOL*touch;
 	lv_indev_drv_t drv;
 	lv_indev_t*dev;
+	void*pd;
 };
 static bool touch_read(lv_indev_drv_t*indev_drv,lv_indev_data_t*data){
 	EFI_ABSOLUTE_POINTER_STATE p;
 	struct input_data*d=indev_drv->user_data;
+	if(!d)return false;
+	if(!d->pd)d->pd=d->touch->GetState;
+	else if(d->touch->GetState!=d->pd){
+		tlog_warn("GetState changed, disable touch device");
+		indev_drv->user_data=NULL;
+		list_obj_del_data(&touchs,d,NULL);
+		memset(d,0,sizeof(struct input_data));
+		free(d);
+		return false;
+	}
 	if(!EFI_ERROR(d->touch->GetState(d->touch,&p))){
 		d->lx=(p.CurrentX*gui_w/d->rx);
 		d->ly=(p.CurrentY*gui_h/d->ry);
@@ -49,6 +64,36 @@ static bool touch_read(lv_indev_drv_t*indev_drv,lv_indev_data_t*data){
 	data->state=d->lp?LV_INDEV_STATE_PR:LV_INDEV_STATE_REL;
 	return false;
 }
+static bool proto_cmp(list*f,void*data){
+	LIST_DATA_DECLARE(d,f,struct input_data*);
+	return d->touch==(EFI_ABSOLUTE_POINTER_PROTOCOL*)data;
+}
+static int _touch_register(EFI_ABSOLUTE_POINTER_PROTOCOL*touch){
+	struct input_data*data=NULL;
+	if(list_search_one(touchs,proto_cmp,touch))return 0;
+	if(!(data=malloc(sizeof(struct input_data))))return -1;
+	memset(data,0,sizeof(struct input_data));
+	data->touch=touch;
+	lv_indev_drv_init(&data->drv);
+	data->drv.type=LV_INDEV_TYPE_POINTER;
+	data->drv.read_cb=touch_read;
+	data->drv.user_data=data;
+	data->dev=lv_indev_drv_register(&data->drv);
+	data->rx=data->touch->Mode->AbsoluteMaxX;
+	data->ry=data->touch->Mode->AbsoluteMaxY;
+	tlog_debug("found new uefi absolute %p",data->touch);
+	list_obj_add_new(&touchs,data);
+	return 0;
+}
+STATIC VOID touch_event(IN EFI_EVENT ev,IN VOID*ctx){
+	EFI_ABSOLUTE_POINTER_PROTOCOL*touch=NULL;
+	EFI_STATUS st=gBS->LocateProtocol(
+		&gEfiAbsolutePointerProtocolGuid,
+		event_reg,(VOID**)&touch
+	);
+	tlog_notice("receive touch device hot-plug event");
+	if(!EFI_ERROR(st)&&touch)_touch_register(touch);
+}
 int touch_register(){
 	bool found=false;
 	UINTN cnt=0;
@@ -58,27 +103,24 @@ int touch_register(){
 		&gEfiAbsolutePointerProtocolGuid,
 		NULL,&cnt,&hands
 	);
-	if(EFI_ERROR(st))return trlog_warn(-1,"locate absolute failed: %s",efi_status_to_string(st));
+	if(EFI_ERROR(st))return trlog_warn(-1,
+		"locate touch failed: %s",
+		efi_status_to_string(st)
+	);
 	for(UINTN i=0;i<cnt;i++){
-		struct input_data*data=malloc(sizeof(struct input_data));
-		if(!data)break;
-		memset(data,0,sizeof(struct input_data));
+		EFI_ABSOLUTE_POINTER_PROTOCOL*touch=NULL;
 		if(EFI_ERROR(gBS->HandleProtocol(
 			hands[i],
 			&gEfiAbsolutePointerProtocolGuid,
-			(VOID**)&data->touch
-		)))continue;
-		lv_indev_drv_init(&data->drv);
-		data->drv.type=LV_INDEV_TYPE_POINTER;
-		data->drv.read_cb=touch_read;
-		data->drv.user_data=data;
-		data->dev=lv_indev_drv_register(&data->drv);
-		data->rx=data->touch->Mode->AbsoluteMaxX;
-		data->ry=data->touch->Mode->AbsoluteMaxY;
-		tlog_debug("found uefi absolute %p",data->touch);
-		found=true;
+			(VOID**)&touch
+		))||!touch)continue;
+		if(_touch_register(touch)==0)found=true;
 	}
-	return found?0:trlog_warn(-1,"no uefi absolute found");
+	EfiCreateProtocolNotifyEvent(
+		&gEfiAbsolutePointerProtocolGuid,
+		TPL_CALLBACK,touch_event,NULL,&event_reg
+	);
+	return found?0:trlog_warn(-1,"no uefi touch found");
 }
 #endif
 #endif

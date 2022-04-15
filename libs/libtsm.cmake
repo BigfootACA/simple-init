c630cd245e1c0b3b38a19f2b504581c483769260
BigfootACA <bigfoot@classfun.cn>
2022-04-15 11:38:23 +0800

    libs: add libtsm

---
+++ libs/libtsm.cmake
@@ -0,0 +1,22 @@
+include_directories(
+	libs/libtsm/src/tsm
+	libs/libtsm/src/shared
+	libs/libtsm/external
+)
+add_library(libtsm STATIC
+	libs/libtsm/src/shared/shl-htable.c
+	libs/libtsm/src/shared/shl-pty.c
+	libs/libtsm/src/shared/shl-ring.c
+	libs/libtsm/src/tsm/tsm-vte.c
+	libs/libtsm/src/tsm/tsm-render.c
+	libs/libtsm/src/tsm/tsm-vte-charsets.c
+	libs/libtsm/src/tsm/tsm-selection.c
+	libs/libtsm/src/tsm/tsm-unicode.c
+	libs/libtsm/src/tsm/tsm-screen.c
+	libs/libtsm/external/wcwidth/wcwidth.c
+)
+target_compile_definitions(libtsm PRIVATE _GNU_SOURCE)
+target_compile_options(libtsm PRIVATE
+	-Wno-sign-compare
+	-Wno-old-style-declaration
+)

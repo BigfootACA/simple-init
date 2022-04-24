12bcd516b9caf100cabecdb076b4c1d74b798725
BigfootACA <bigfoot@classfun.cn>
2022-04-24 11:45:22 +0800

    gui: interface: term: add baudrates.c

---
+++ src/gui/interface/term/serial.h
@@ -0,0 +1,33 @@
+/*
+ *
+ * Copyright (C) 2021 BigfootACA <bigfoot@classfun.cn>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#ifndef _SERIAL_H
+#define _SERIAL_H
+
+#ifdef ENABLE_UEFI
+#include<Protocol/SerialIo.h>
+#include<Library/UefiBootServicesTableLib.h>
+#endif
+
+struct serial_port_cfg{
+	#ifdef ENABLE_UEFI
+	EFI_SERIAL_IO_PROTOCOL*proto;
+	#else
+	char port[256+sizeof(void*)];
+	#endif
+	unsigned int baudrate;
+};
+
+struct baudrate{
+	unsigned int speed;
+	unsigned int number;
+	char name[24];
+};
+
+extern struct baudrate serial_baudrates[];
+#endif

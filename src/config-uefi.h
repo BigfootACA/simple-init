5199d8f2a0e8619d3e05b54b7bc41b8f708c5153
BigfootACA <bigfoot@classfun.cn>
2022-05-16 15:41:33 +0800

    src: add config-uefi.h

---
+++ src/config-uefi.h
@@ -0,0 +1,15 @@
+#define ENABLE_UEFI         1
+#define ENABLE_GUI          1
+#define ENABLE_FREETYPE2    1
+#define ENABLE_LODEPNG      1
+#define ENABLE_NANOSVG      1
+#define ENABLE_JSONC        1
+#define ENABLE_MXML         1
+#define ENABLE_STB          1
+#define ENABLE_LUA          1
+#define ENABLE_FDT          1
+#define ENABLE_LIBTSM       1
+#define FT2_BUILD_LIBRARY   1
+#define FT_CONFIG_OPTIONS_H "libs/freetype-uefi/ftoption.h"
+#define LV_CONF_PATH        lv_conf.h
+#define LODEPNG_NO_COMPILE_DISK

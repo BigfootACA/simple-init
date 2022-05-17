d2822be373408fca5558d3035b7abf738da50d4d
BigfootACA <bigfoot@classfun.cn>
2022-05-17 18:56:36 +0800

    src: config-uefi.h: add zlib

--- src/config-uefi.h
+++ src/config-uefi.h
@@ -8,6 +8,7 @@
 #define ENABLE_STB          1
 #define ENABLE_LUA          1
 #define ENABLE_FDT          1
+#define ENABLE_ZLIB         1
 #define ENABLE_LIBTSM       1
 #define FT2_BUILD_LIBRARY   1
 #define FT_CONFIG_OPTIONS_H "libs/freetype-uefi/ftoption.h"

5dba12ac0216e97561f5f0a48b8fc2410c5c1b89
BigfootACA <bigfoot@classfun.cn>
2022-05-22 12:40:22 +0800

    gui: interface: apps: acpi.c: fix focus

--- src/gui/interface/apps/acpi.c
+++ src/gui/interface/apps/acpi.c
@@ -6,6 +6,7 @@
  *
  */
 
+#include "list.h"
 #ifdef ENABLE_GUI
 #include<stdlib.h>
 #include<Library/UefiLib.h>
@@ -32,28 +33,33 @@
 #include"gui/filepicker.h"
 #define TAG "acpi"
 
-static lv_obj_t*lst=NULL,*last=NULL,*title=NULL;
-static lv_obj_t*info,*btn_load,*btn_refresh,*btn_save;
+struct acpi_mgr{
+	list*acpis;
+	struct acpi_info*selected;
+	lv_obj_t*lst,*last,*title;
+	lv_obj_t*info,*btn_load,*btn_refresh,*btn_save;
+};
+
 struct acpi_info{
+	struct acpi_mgr*am;
 	bool enable;
 	lv_obj_t*btn,*chk;
 	lv_obj_t*name,*info;
 	EFI_ACPI_DESCRIPTION_HEADER*table;
 	CHAR8 type[8],oem[8];
 };
-static list*acpis=NULL;
-static struct acpi_info*selected=NULL;
 
 static void item_check(lv_obj_t*obj,lv_event_t e){
 	if(e!=LV_EVENT_VALUE_CHANGED)return;
+	struct acpi_info*ai=lv_obj_get_user_data(obj);
 	lv_checkbox_set_checked(obj,true);
-	if(selected&&obj!=selected->chk){
-		lv_checkbox_set_checked(selected->chk,false);
-		lv_obj_set_checked(selected->btn,false);
+	if(ai->am->selected&&obj!=ai->am->selected->chk){
+		lv_checkbox_set_checked(ai->am->selected->chk,false);
+		lv_obj_set_checked(ai->am->selected->btn,false);
 	}
-	selected=lv_obj_get_user_data(obj);
-	lv_obj_set_checked(selected->btn,true);
-	lv_obj_set_enabled(btn_save,true);
+	ai->am->selected=lv_obj_get_user_data(obj);
+	lv_obj_set_checked(ai->am->selected->btn,true);
+	lv_obj_set_enabled(ai->am->btn_save,true);
 }
 
 static void free_acpi_info(struct acpi_info*m){
@@ -71,49 +77,50 @@ static int item_free(void*d){
 static int item_free_del(void*d){
 	struct acpi_info*mi=d;
 	lv_obj_del(mi->btn);
+	lv_group_remove_obj(mi->chk);
 	free_acpi_info(mi);
 	return 0;
 }
 
-static void acpi_clear(){
-	selected=NULL;
-	lv_obj_set_enabled(btn_save,false);
-	if(info)lv_obj_del(info);
-	list_free_all(acpis,item_free_del);
-	info=NULL,last=NULL,acpis=NULL;
+static void acpi_clear(struct acpi_mgr*am){
+	am->selected=NULL;
+	lv_obj_set_enabled(am->btn_save,false);
+	if(am->info)lv_obj_del(am->info);
+	list_free_all(am->acpis,item_free_del);
+	am->info=NULL,am->last=NULL,am->acpis=NULL;
 }
 
-void acpi_set_info(char*text){
-	acpi_clear();
-	info=lv_label_create(lst,NULL);
-	lv_label_set_long_mode(info,LV_LABEL_LONG_BREAK);
-	lv_obj_set_size(info,lv_page_get_scrl_width(lst),gui_sh/16);
-	lv_label_set_align(info,LV_LABEL_ALIGN_CENTER);
-	lv_label_set_text(info,text);
+void acpi_set_info(struct acpi_mgr*am,char*text){
+	acpi_clear(am);
+	am->info=lv_label_create(am->lst,NULL);
+	lv_label_set_long_mode(am->info,LV_LABEL_LONG_BREAK);
+	lv_obj_set_size(am->info,lv_page_get_scrl_width(am->lst),gui_sh/16);
+	lv_label_set_align(am->info,LV_LABEL_ALIGN_CENTER);
+	lv_label_set_text(am->info,text);
 }
 
-static bool acpi_add_item(EFI_ACPI_DESCRIPTION_HEADER*table){
-	lv_coord_t bw=lv_page_get_scrl_width(lst);
+static bool acpi_add_item(struct acpi_mgr*am,EFI_ACPI_DESCRIPTION_HEADER*table){
+	lv_coord_t bw=lv_page_get_scrl_width(am->lst);
 	lv_coord_t bm=gui_dpi/20;
 	if(!table)return false;
 	struct acpi_info*k=malloc(sizeof(struct acpi_info));
 	if(!k)return false;
 	memset(k,0,sizeof(struct acpi_info));
-	k->table=table;
+	k->table=table,k->am=am;
 	CopyMem(k->type,&table->Signature,sizeof(table->Signature));
 	CopyMem(k->oem,&table->OemId,sizeof(table->OemId));
 
 	// button
-	k->btn=lv_btn_create(lst,NULL);
+	k->btn=lv_btn_create(am->lst,NULL);
 	lv_obj_set_size(k->btn,bw,gui_dpi/2);
 	lv_obj_align(
-		k->btn,last,
-		last?LV_ALIGN_OUT_BOTTOM_MID:LV_ALIGN_IN_TOP_MID,
-		0,last?gui_dpi/8:0
+		k->btn,am->last,
+		am->last?LV_ALIGN_OUT_BOTTOM_MID:LV_ALIGN_IN_TOP_MID,
+		0,am->last?gui_dpi/8:0
 	);
 	lv_style_set_btn_item(k->btn);
 	lv_obj_set_click(k->btn,false);
-	last=k->btn;
+	am->last=k->btn;
 
 	lv_obj_t*line=lv_line_create(k->btn,NULL);
 	lv_obj_set_width(line,bw);
@@ -143,12 +150,13 @@ static bool acpi_add_item(EFI_ACPI_DESCRIPTION_HEADER*table){
 		(long long)(UINTN)table,table->Length,table->OemId
 	);
 
+	list_obj_add_new(&am->acpis,k);
 	return true;
 }
 
-static void acpi_reload(){
-	acpi_clear();
+static void acpi_reload(struct acpi_mgr*am){
 	bool found=false;
+	acpi_clear(am);
 	for(UINTN i=0;i<gST->NumberOfTableEntries;i++){
 		EFI_CONFIGURATION_TABLE*t=&gST->ConfigurationTable[i];
 		EFI_ACPI_6_3_ROOT_SYSTEM_DESCRIPTION_POINTER*sdt=t->VendorTable;
@@ -163,22 +171,24 @@ static void acpi_reload(){
 		UINTN cnt=(xsdt->Length-sizeof(EFI_ACPI_SDT_HEADER))/sizeof(UINT64);
 		for(UINTN x=0;x<cnt;x++,ptr++) {
 			EFI_ACPI_DESCRIPTION_HEADER*e=(VOID*)(UINTN)*ptr;
-			if(acpi_add_item(e))found=true;
+			if(acpi_add_item(am,e))found=true;
 			if(e->Signature!=EFI_ACPI_6_3_FIXED_ACPI_DESCRIPTION_TABLE_SIGNATURE)continue;
 			EFI_ACPI_5_1_FIXED_ACPI_DESCRIPTION_TABLE*fadt=(VOID*)e;
 			if(acpi_add_item(
+				am,
 				(VOID*)((e->Revision>=3&&fadt->XDsdt)?
 				(UINTN)fadt->XDsdt:
 				(UINTN)fadt->Dsdt
 			)))found=true;
 			if(acpi_add_item(
+				am,
 				(VOID*)((e->Revision>=3&&fadt->XFirmwareCtrl)?
 				(UINTN)fadt->XFirmwareCtrl:
 				(UINTN)fadt->FirmwareCtrl
 			)))found=true;
 		}
 	}
-	if(!found)acpi_set_info(_("No any ACPI tables found"));
+	if(!found)acpi_set_info(am,_("No any ACPI tables found"));
 }
 
 static void load_aml(const char*path){
@@ -312,53 +322,65 @@ static bool save_cb(bool ok,const char**path,uint16_t cnt,void*user_data){
 }
 
 static void load_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED||obj!=btn_load)return;
+	struct acpi_mgr*am=lv_obj_get_user_data(obj);
+	if(e!=LV_EVENT_CLICKED||obj!=am->btn_load)return;
 	tlog_debug("request load");
 	struct filepicker*fp=filepicker_create(load_cb,"Select aml file to load");
 	filepicker_set_max_item(fp,1);
 }
 
 static void save_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED||obj!=btn_save||!selected)return;
+	struct acpi_mgr*am=lv_obj_get_user_data(obj);
+	if(e!=LV_EVENT_CLICKED||obj!=am->btn_save||!am->selected)return;
 	tlog_debug("request save");
 	struct filepicker*fp=filepicker_create(save_cb,"Select aml file to save");
 	filepicker_set_max_item(fp,1);
-	filepicker_set_user_data(fp,selected);
+	filepicker_set_user_data(fp,am->selected);
 }
 
 static void refresh_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED||obj!=btn_refresh)return;
+	struct acpi_mgr*am=lv_obj_get_user_data(obj);
+	if(e!=LV_EVENT_CLICKED||obj!=am->btn_refresh)return;
 	tlog_debug("request refresh");
-	acpi_reload();
+	acpi_reload(am);
 }
 
-static int do_cleanup(struct gui_activity*d __attribute__((unused))){
-	list_free_all(acpis,item_free);
-	info=NULL,last=NULL,acpis=NULL;
+static int do_cleanup(struct gui_activity*d){
+	struct acpi_mgr*am=d->data;
+	if(!am)return 0;
+	list_free_all(am->acpis,item_free);
+	am->info=NULL,am->last=NULL,am->acpis=NULL;
+	free(am);
+	d->data=NULL;
 	return 0;
 }
 
 static int do_load(struct gui_activity*d){
-	acpi_reload();
+	struct acpi_mgr*am=d->data;
+	if(am)acpi_reload(am);
 	return 0;
 }
 
-static int acpi_get_focus(struct gui_activity*d __attribute__((unused))){
-	lv_group_add_obj(gui_grp,btn_load);
-	lv_group_add_obj(gui_grp,btn_refresh);
-	lv_group_add_obj(gui_grp,btn_save);
+static int acpi_get_focus(struct gui_activity*d){
+	struct acpi_mgr*am=d->data;
+	if(!am)return 0;
+	lv_group_add_obj(gui_grp,am->btn_load);
+	lv_group_add_obj(gui_grp,am->btn_refresh);
+	lv_group_add_obj(gui_grp,am->btn_save);
 	return 0;
 }
 
-static int acpi_lost_focus(struct gui_activity*d __attribute__((unused))){
-	list*o=list_first(acpis);
+static int acpi_lost_focus(struct gui_activity*d){
+	struct acpi_mgr*am=d->data;
+	if(!am)return 0;
+	list*o=list_first(am->acpis);
 	if(o)do{
 		LIST_DATA_DECLARE(item,o,struct acpi_info*);
 		lv_group_remove_obj(item->chk);
 	}while((o=o->next));
-	lv_group_remove_obj(btn_load);
-	lv_group_remove_obj(btn_refresh);
-	lv_group_remove_obj(btn_save);
+	lv_group_remove_obj(am->btn_load);
+	lv_group_remove_obj(am->btn_refresh);
+	lv_group_remove_obj(am->btn_save);
 	return 0;
 }
 
@@ -366,58 +388,72 @@ static int acpi_resize(struct gui_activity*act){
 	lv_coord_t btm=gui_dpi/10;
 	lv_coord_t btw=act->w/3-btm;
 	lv_coord_t bth=gui_font_size+gui_dpi/10;
+	struct acpi_mgr*am=act->data;
 
-	lv_obj_set_pos(title,0,gui_font_size);
-	lv_obj_set_width(title,act->w);
+	lv_obj_set_pos(am->title,0,gui_font_size);
+	lv_obj_set_width(am->title,act->w);
 
-	lv_obj_align(lst,title,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
-	lv_obj_set_x(lst,gui_font_size/2);
-	lv_obj_set_size(lst,act->w-gui_font_size,act->h-lv_obj_get_y(lst)-bth-btm*2);
+	lv_obj_align(am->lst,am->title,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
+	lv_obj_set_x(am->lst,gui_font_size/2);
+	lv_obj_set_size(am->lst,act->w-gui_font_size,act->h-lv_obj_get_y(am->lst)-bth-btm*2);
 
-	lv_obj_set_size(btn_load,btw,bth);
-	lv_obj_align(btn_load,lst,LV_ALIGN_OUT_BOTTOM_LEFT,0,btm);
+	lv_obj_set_size(am->btn_load,btw,bth);
+	lv_obj_align(am->btn_load,am->lst,LV_ALIGN_OUT_BOTTOM_LEFT,0,btm);
 
-	lv_obj_set_size(btn_refresh,btw,bth);
-	lv_obj_align(btn_refresh,lst,LV_ALIGN_OUT_BOTTOM_MID,0,btm);
+	lv_obj_set_size(am->btn_refresh,btw,bth);
+	lv_obj_align(am->btn_refresh,am->lst,LV_ALIGN_OUT_BOTTOM_MID,0,btm);
 
-	lv_obj_set_size(btn_save,btw,bth);
-	lv_obj_align(btn_save,lst,LV_ALIGN_OUT_BOTTOM_RIGHT,0,btm);
+	lv_obj_set_size(am->btn_save,btw,bth);
+	lv_obj_align(am->btn_save,am->lst,LV_ALIGN_OUT_BOTTOM_RIGHT,0,btm);
 
-	acpi_reload();
+	acpi_reload(am);
+	return 0;
+}
+
+static int acpi_init(struct gui_activity*act){
+	struct acpi_mgr*am;
+	if(!(am=malloc(sizeof(struct acpi_mgr))))ERET(ENOMEM);
+	memset(am,0,sizeof(struct acpi_mgr));
+	act->data=am;
 	return 0;
 }
 
 static int acpi_draw(struct gui_activity*act){
+	struct acpi_mgr*am=act->data;
+	if(!am)return -1;
 
 	// function title
-	title=lv_label_create(act->page,NULL);
-	lv_label_set_long_mode(title,LV_LABEL_LONG_BREAK);
-	lv_label_set_align(title,LV_LABEL_ALIGN_CENTER);
-	lv_label_set_text(title,_("ACPI Manager"));
+	am->title=lv_label_create(act->page,NULL);
+	lv_label_set_long_mode(am->title,LV_LABEL_LONG_BREAK);
+	lv_label_set_align(am->title,LV_LABEL_ALIGN_CENTER);
+	lv_label_set_text(am->title,_("ACPI Manager"));
 
 	// options list
-	lst=lv_page_create(act->page,NULL);
-	lv_obj_set_style_local_border_width(lst,LV_PAGE_PART_BG,LV_STATE_DEFAULT,0);
-	lv_obj_set_style_local_border_width(lst,LV_PAGE_PART_BG,LV_STATE_FOCUSED,0);
-	lv_obj_set_style_local_border_width(lst,LV_PAGE_PART_BG,LV_STATE_PRESSED,0);
+	am->lst=lv_page_create(act->page,NULL);
+	lv_obj_set_style_local_border_width(am->lst,LV_PAGE_PART_BG,LV_STATE_DEFAULT,0);
+	lv_obj_set_style_local_border_width(am->lst,LV_PAGE_PART_BG,LV_STATE_FOCUSED,0);
+	lv_obj_set_style_local_border_width(am->lst,LV_PAGE_PART_BG,LV_STATE_PRESSED,0);
 
 	// load button
-	btn_load=lv_btn_create(act->page,NULL);
-	lv_obj_set_event_cb(btn_load,load_click);
-	lv_style_set_action_button(btn_load,true);
-	lv_label_set_text(lv_label_create(btn_load,NULL),_("Load"));
+	am->btn_load=lv_btn_create(act->page,NULL);
+	lv_obj_set_user_data(am->btn_load,am);
+	lv_obj_set_event_cb(am->btn_load,load_click);
+	lv_style_set_action_button(am->btn_load,true);
+	lv_label_set_text(lv_label_create(am->btn_load,NULL),_("Load"));
 
 	// refresh button
-	btn_refresh=lv_btn_create(act->page,NULL);
-	lv_obj_set_event_cb(btn_refresh,refresh_click);
-	lv_style_set_action_button(btn_refresh,true);
-	lv_label_set_text(lv_label_create(btn_refresh,NULL),_("Refresh"));
+	am->btn_refresh=lv_btn_create(act->page,NULL);
+	lv_obj_set_user_data(am->btn_refresh,am);
+	lv_obj_set_event_cb(am->btn_refresh,refresh_click);
+	lv_style_set_action_button(am->btn_refresh,true);
+	lv_label_set_text(lv_label_create(am->btn_refresh,NULL),_("Refresh"));
 
 	// save button
-	btn_save=lv_btn_create(act->page,NULL);
-	lv_obj_set_event_cb(btn_save,save_click);
-	lv_style_set_action_button(btn_save,true);
-	lv_label_set_text(lv_label_create(btn_save,NULL),_("Save"));
+	am->btn_save=lv_btn_create(act->page,NULL);
+	lv_obj_set_user_data(am->btn_save,am);
+	lv_obj_set_event_cb(am->btn_save,save_click);
+	lv_style_set_action_button(am->btn_save,true);
+	lv_label_set_text(lv_label_create(am->btn_save,NULL),_("Save"));
 
 	return 0;
 }
@@ -463,6 +499,7 @@ struct gui_register guireg_acpi_manager={
 		NULL
 	},
 	.show_app=true,
+	.init=acpi_init,
 	.draw=acpi_draw,
 	.resize=acpi_resize,
 	.quiet_exit=do_cleanup,

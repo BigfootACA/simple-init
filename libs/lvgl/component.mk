8ba48b5b3610b0c3f4fd6dab33a5e1b9ea7a6414
BigfootACA <bigfoot@classfun.cn>
2022-06-14 04:33:04 +0800

    libs: lvgl: upgrade to v8.2

--- libs/lvgl/component.mk
+++ libs/lvgl/component.mk
@@ -1,23 +1,34 @@
 # ESP-IDF component file for make based commands
 
-COMPONENT_SRCDIRS := .
-COMPONENT_SRCDIRS += src
-COMPONENT_SRCDIRS += src/lv_core
-COMPONENT_SRCDIRS += src/lv_draw
-COMPONENT_SRCDIRS += src/lv_font
-COMPONENT_SRCDIRS += src/lv_gpu
-COMPONENT_SRCDIRS += src/lv_hal
-COMPONENT_SRCDIRS += src/lv_misc
-COMPONENT_SRCDIRS += src/lv_themes
-COMPONENT_SRCDIRS += src/lv_widgets
+COMPONENT_SRCDIRS := . \
+                  src \
+                  src/core \
+                  src/draw \
+                  src/extra \
+                  src/font \
+                  src/gpu \
+                  src/hal \
+                  src/misc \
+                  src/widgets \
+                  src/extra/layouts \
+                  src/extra/layouts/flex \
+                  src/extra/layouts/grid \
+                  src/extra/themes \
+                  src/extra/themes/basic \
+                  src/extra/themes/default \
+                  src/extra/widgets/calendar \
+                  src/extra/widgets/colorwheel \
+                  src/extra/widgets \
+                  src/extra/widgets/imgbtn \
+                  src/extra/widgets/keyboard \
+                  src/extra/widgets/led \
+                  src/extra/widgets/list \
+                  src/extra/widgets/msgbox \
+                  src/extra/widgets/spinbox \
+                  src/extra/widgets/spinner \
+                  src/extra/widgets/tabview \
+                  src/extra/widgets/tileview \
+                  src/extra/widgets/win
 
-COMPONENT_ADD_INCLUDEDIRS := .
-COMPONENT_ADD_INCLUDEDIRS += src
-COMPONENT_ADD_INCLUDEDIRS += src/lv_core
-COMPONENT_ADD_INCLUDEDIRS += src/lv_draw
-COMPONENT_ADD_INCLUDEDIRS += src/lv_font
-COMPONENT_ADD_INCLUDEDIRS += src/lv_gpu
-COMPONENT_ADD_INCLUDEDIRS += src/lv_hal
-COMPONENT_ADD_INCLUDEDIRS += src/lv_misc
-COMPONENT_ADD_INCLUDEDIRS += src/lv_themes
-COMPONENT_ADD_INCLUDEDIRS += src/lv_widgets
+
+COMPONENT_ADD_INCLUDEDIRS := $(COMPONENT_SRCDIRS) .

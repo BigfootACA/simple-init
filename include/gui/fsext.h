ea3e64d35898a7221c89ab24a6abdca58986d47a
BigfootACA <bigfoot@classfun.cn>
2022-06-26 00:00:48 +0800

    gui: filesystem: fsext.c: upgrade and add removed methods

--- include/gui/fsext.h
+++ include/gui/fsext.h
@@ -21,17 +21,23 @@ enum item_type{
 };
 struct fsext{
 	void*user_data;
-	lv_res_t(*get_type_cb)(struct _lv_fs_drv_t*,const char*,enum item_type*);
-	lv_res_t(*get_volume_label)(struct _lv_fs_drv_t*,char*,size_t);
-	lv_res_t(*mkdir)(struct _lv_fs_drv_t*,const char*);
-	lv_res_t(*creat)(struct _lv_fs_drv_t*,const char*);
-	bool(*is_dir_cb)(struct _lv_fs_drv_t*,const char*);
+	lv_res_t(*get_type_cb)(lv_fs_drv_t*,const char*,enum item_type*);
+	lv_res_t(*get_volume_label)(lv_fs_drv_t*,char*,size_t);
+	lv_res_t(*mkdir)(lv_fs_drv_t*,const char*);
+	lv_res_t(*size)(lv_fs_drv_t*,void*,uint32_t*);
+	lv_res_t(*rename)(lv_fs_drv_t*,const char*,const char*);
+	lv_res_t(*remove)(lv_fs_drv_t*,const char*);
+	lv_res_t(*creat)(lv_fs_drv_t*,const char*);
+	bool(*is_dir_cb)(lv_fs_drv_t*,const char*);
 };
 extern bool fsext_is_multi;
 extern lv_res_t lv_fs_get_type(enum item_type*type,const char*path);
 extern lv_res_t lv_fs_get_volume_label(lv_fs_drv_t*drv,char*label,size_t len);
 extern lv_res_t lv_fs_mkdir(const char*name);
 extern lv_res_t lv_fs_creat(const char*name);
+extern lv_fs_res_t lv_fs_size(lv_fs_file_t*file_p,uint32_t*size);
+extern lv_fs_res_t lv_fs_rename(const char*oldname,const char*newname);
+extern lv_fs_res_t lv_fs_remove(const char*path);
 extern bool lv_fs_is_dir(const char*path);
 extern const char*lv_fs_res_to_string(lv_fs_res_t res);
 extern const char*lv_fs_res_to_i18n_string(lv_fs_res_t res);
--- src/gui/filesystem/fsext.c
+++ src/gui/filesystem/fsext.c
@@ -63,6 +63,39 @@ lv_res_t lv_fs_creat(const char*path){
 	return fse->creat(drv,lv_fs_get_real_path(path));
 }
 
+lv_fs_res_t lv_fs_size(lv_fs_file_t*file_p,uint32_t*size){
+	if(!size)return LV_FS_RES_INV_PARAM;
+	if(!file_p->drv)return LV_FS_RES_INV_PARAM;
+	struct fsext*fse=file_p->drv->user_data;
+	if(!fse->size)return LV_FS_RES_NOT_IMP;
+	return fse->size(file_p->drv,file_p->file_d,size);
+}
+
+lv_fs_res_t lv_fs_rename(const char*oldname,const char*newname){
+	if(!oldname||!newname)return LV_FS_RES_INV_PARAM;
+	char letter=oldname[0];
+	lv_fs_drv_t*drv=lv_fs_get_drv(letter);
+	if(!drv)return LV_FS_RES_NOT_EX;
+	if(drv->ready_cb&&!drv->ready_cb(drv))return LV_FS_RES_HW_ERR;
+	struct fsext*fse=drv->user_data;
+	if(!fse->rename)return LV_FS_RES_NOT_IMP;
+	return fse->rename(drv,
+		lv_fs_get_real_path(oldname),
+		lv_fs_get_real_path(newname)
+	);
+}
+
+lv_fs_res_t lv_fs_remove(const char*path){
+	if(!path)return LV_FS_RES_INV_PARAM;
+	char letter=path[0];
+	lv_fs_drv_t*drv= lv_fs_get_drv(letter);
+	if(drv == NULL) return LV_FS_RES_NOT_EX;
+	if(drv->ready_cb&&!drv->ready_cb(drv))return LV_FS_RES_HW_ERR;
+	struct fsext*fse=drv->user_data;
+	if(!fse->remove)return LV_FS_RES_NOT_IMP;
+	return fse->remove(drv,lv_fs_get_real_path(path));
+}
+
 bool lv_fs_is_dir(const char*path){
 	if(!path)return LV_FS_RES_INV_PARAM;
 	char letter=path[0];

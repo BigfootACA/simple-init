27ab49c8048b6eb3bad64d0e10b9d789f5b9daa0
BigfootACA <bigfoot@classfun.cn>
2022-07-04 23:48:16 +0800

    gui: interface: core: upgrade bootmenu.c

--- src/gui/interface/core/bootmenu.c
+++ src/gui/interface/core/bootmenu.c
@@ -25,11 +25,11 @@ struct bootmenu;
 struct bootmenu_item;
 
 struct bootmenu{
-	lv_coord_t bm,bw,bh,bi,si;
+	lv_coord_t si,bw;
 	lv_label_long_mode_t lm;
-	lv_obj_t*scr,*last_btn;
+	lv_obj_t*scr;
 	lv_obj_t*bg,*page,*btn,*title,*time;
-	lv_task_t*auto_boot;
+	lv_timer_t*auto_boot;
 	list*items;
 	bool do_auto_boot;
 	int timeout;
@@ -42,7 +42,9 @@ struct bootmenu_item{
 	struct bootmenu*bm;
 	bool parent;
 	boot_config cfg;
-	lv_obj_t*btn,*img,*title;
+	lv_obj_t*btn;
+	lv_obj_t*w_img,*img;
+	lv_obj_t*title,*chk;
 };
 
 static bool no_autoboot=false;
@@ -59,12 +61,12 @@ static int bootmenu_item_free(void*d){
 static int bootmenu_clean(struct bootmenu*bm){
 	lv_obj_set_enabled(bm->btn,false);
 	list_free_all(bm->items,bootmenu_item_free);
-	bm->last_btn=NULL,bm->selected=NULL,bm->items=NULL;
+	bm->selected=NULL,bm->items=NULL;
 	return 0;
 }
 
 #ifdef ENABLE_UEFI
-static void enter_simple_init(lv_task_t*t __attribute__((unused))){
+static void enter_simple_init(lv_timer_t*t __attribute__((unused))){
 	gui_screen_init();
 	gui_draw();
 }
@@ -107,7 +109,7 @@ static bool bootmenu_chdir(struct bootmenu_item*bi){
 	return true;
 }
 
-static void bootmenu_boot(lv_task_t*tsk){
+static void bootmenu_boot(lv_timer_t*tsk){
 	struct bootmenu*bm;
 	struct bootmenu_item*bi;
 	if(!(bm=tsk->user_data)||!(bi=bm->selected))return;
@@ -117,10 +119,9 @@ static void bootmenu_boot(lv_task_t*tsk){
 	confd_set_save("boot.current",false);
 	#ifdef ENABLE_UEFI
 	if(bi->cfg.mode==BOOT_SIMPLE_INIT){
-		lv_task_once(lv_task_create(
-			enter_simple_init,100,
-			LV_TASK_PRIO_LOWEST,NULL
-		));
+		lv_timer_set_repeat_count(lv_timer_create(
+			enter_simple_init,100,NULL
+		),1);
 		return;
 	}
 	memset(
@@ -140,29 +141,33 @@ static void bootmenu_boot(lv_task_t*tsk){
 
 static void bootmenu_remove_auto_boot(struct bootmenu*bm){
 	if(!bm->auto_boot)return;
-	lv_task_del(bm->auto_boot);
+	lv_timer_del(bm->auto_boot);
 	lv_obj_set_hidden(bm->time,true);
 	bm->auto_boot=NULL;
 }
 
-static void bootmenu_click(lv_obj_t*obj,lv_event_t e){
-	struct bootmenu*bm=lv_obj_get_user_data(obj);
-	if(!bm||!bm->selected||bm->btn!=obj)return;
-	bootmenu_remove_auto_boot(bm);
-	if(e!=LV_EVENT_CLICKED)return;
-	lv_task_once(lv_task_create(
-		bootmenu_boot,10,
-		LV_TASK_PRIO_HIGHEST,bm
-	));
+static void bootmenu_remove_auto_boot_cb(lv_event_t*e){
+	bootmenu_remove_auto_boot(e->user_data);
 }
 
-static void bootmenu_item_click(lv_obj_t*obj,lv_event_t e){
-	struct bootmenu_item*bi=lv_obj_get_user_data(obj);
-	if(!bi||(bi->img!=obj&&bi->btn!=obj))return;
-	bootmenu_remove_auto_boot(bi->bm);
-	if(e==LV_EVENT_RELEASED&&!bi->parent)
-		lv_group_focus_obj(bi->bm->btn);
-	if(e!=LV_EVENT_CLICKED)return;
+static void bootmenu_click(lv_event_t*e){
+	lv_timer_set_repeat_count(lv_timer_create(
+		bootmenu_boot,10,e->user_data
+	),1);
+}
+
+static void bootmenu_item_released(lv_event_t*e){
+	struct bootmenu_item*bi=e->user_data;
+	if(!bi->parent)lv_group_focus_obj(bi->bm->btn);
+}
+
+static void bootmenu_item_focused(lv_event_t*e){
+	struct bootmenu_item*bi=e->user_data;
+	lv_obj_scroll_to_view(bi->btn,LV_ANIM_ON);
+}
+
+static void bootmenu_item_click(lv_event_t*e){
+	struct bootmenu_item*bi=e->user_data;
 	if(bi->parent){
 		memset(
 			bi->bm->folder,0,
@@ -187,22 +192,23 @@ static void bootmenu_item_click(lv_obj_t*obj,lv_event_t e){
 		if(item&&item->btn){
 			lv_obj_set_checked(item->btn,false);
 			lv_obj_clear_state(item->img,LV_STATE_FOCUSED);
+			lv_obj_add_flag(item->chk,LV_OBJ_FLAG_HIDDEN);
 		}
 	}while((o=o->next));
 	lv_obj_set_enabled(bi->bm->btn,true);
 	lv_obj_set_checked(bi->btn,true);
+	lv_obj_clear_flag(bi->chk,LV_OBJ_FLAG_HIDDEN);
 	bi->bm->selected=bi;
 }
 
-static void bootmenu_auto_boot_cb(lv_task_t*tsk);
+static void bootmenu_auto_boot_cb(lv_timer_t*tsk);
 static void bootmenu_auto_boot(struct bootmenu*bm){
 	if(bm->timeout<=0){
 		bootmenu_remove_auto_boot(bm);
-		bootmenu_click(bm->btn,LV_EVENT_CLICKED);
+		bootmenu_click(&(lv_event_t){.user_data=bm});
 	}else if(!bm->auto_boot){
-		bm->auto_boot=lv_task_create(
-			bootmenu_auto_boot_cb,
-			1000,LV_TASK_PRIO_MID,bm
+		bm->auto_boot=lv_timer_create(
+			bootmenu_auto_boot_cb,1000,bm
 		);
 	}else{
 		lv_obj_set_hidden(bm->time,false);
@@ -212,30 +218,29 @@ static void bootmenu_auto_boot(struct bootmenu*bm){
 			tout?tout:_("Auto boot in %d seconds"),
 			bm->timeout
 		);
-		if(gui_sh>gui_sw)lv_obj_align(
-			bm->time,bm->page,
-			LV_ALIGN_OUT_BOTTOM_MID,
-			0,gui_font_size/2
-		);
-		else lv_obj_set_pos(
-			bm->time,(lv_obj_get_x(bm->page)-
-			lv_obj_get_width(bm->time))/2,
-			(gui_h-lv_obj_get_height(bm->time))/2
-		);
 		if(tout)free(tout);
 		bm->timeout--;
 		lv_disp_trig_activity(NULL);
 	}
 }
 
-static void bootmenu_auto_boot_cb(lv_task_t*tsk){
+static void bootmenu_auto_boot_cb(lv_timer_t*tsk){
 	bootmenu_auto_boot(tsk->user_data);
 }
 
 static void bootmenu_add(struct bootmenu*bm,char*c){
-	struct bootmenu_item*bi=malloc(sizeof(struct bootmenu_item));
-	if(!bi)return;
+	struct bootmenu_item*bi;
+	static lv_coord_t grid_row[]={
+		LV_GRID_FR(1),
+		LV_GRID_TEMPLATE_LAST
+	},grid_col[]={
+		0,LV_GRID_FR(1),0,
+		LV_GRID_TEMPLATE_LAST
+	};
+	if(!(bi=malloc(sizeof(struct bootmenu_item))))return;
 	memset(bi,0,sizeof(struct bootmenu_item));
+	if(grid_col[0]!=bm->si)grid_col[0]=bm->si;
+	if(grid_col[2]!=bm->si)grid_col[2]=bm->si;
 	if(c){
 		boot_config*cfg=boot_get_config(c);
 		if(!cfg)goto fail;
@@ -252,54 +257,61 @@ static void bootmenu_add(struct bootmenu*bm,char*c){
 		bi->parent=true;
 	}
 	bi->bm=bm;
-	bi->btn=lv_btn_create(bm->page,NULL);
-	lv_obj_set_size(bi->btn,bm->bw,bm->bh);
-	lv_obj_set_user_data(bi->btn,bi);
-	lv_obj_set_event_cb(bi->btn,bootmenu_item_click);
-	lv_style_set_btn_item(bi->btn);
-	lv_obj_align(
-		bi->btn,bm->last_btn,bm->last_btn?
-		LV_ALIGN_OUT_BOTTOM_LEFT:
-		LV_ALIGN_IN_TOP_MID,
-		0,bm->bm/8+(bm->last_btn?gui_dpi/20:0)
-	);
-	bm->last_btn=bi->btn;
+	bi->btn=lv_obj_create(bm->page);
+	lv_obj_set_size(bi->btn,bm->bw,LV_SIZE_CONTENT);
+	lv_obj_set_scroll_dir(bi->btn,LV_DIR_NONE);
+	lv_obj_set_grid_dsc_array(bi->btn,grid_col,grid_row);
+	lv_obj_add_event_cb(bi->btn,bootmenu_item_click,LV_EVENT_CLICKED,bi);
+	lv_obj_add_event_cb(bi->btn,bootmenu_item_released,LV_EVENT_RELEASED,bi);
+	lv_obj_add_event_cb(bi->btn,bootmenu_item_focused,LV_EVENT_FOCUSED,bi);
+	lv_obj_set_style_outline_width(bi->btn,gui_dpi/100,LV_STATE_FOCUSED);
+	lv_obj_set_style_outline_color(bi->btn,lv_theme_get_color_primary(bi->btn),LV_STATE_FOCUSED);
+	lv_group_add_obj(gui_grp,bi->btn);
 	if(list_obj_add_new(&bm->items,bi)!=0){
 		telog_error("cannot add item list");
 		abort();
 	}
 
-	// line for button text
-	lv_obj_t*line=lv_line_create(bi->btn,NULL);
-	lv_obj_set_width(line,bm->bw);
-
 	// boot item image
-	bi->img=lv_img_create(line,NULL);
-	lv_obj_set_size(bi->img,bm->si,bm->si);
-	lv_obj_align(bi->img,bi->btn,LV_ALIGN_IN_LEFT_MID,gui_font_size/2,0);
-	lv_obj_set_drag_parent(bi->img,true);
-	lv_obj_set_click(bi->img,true);
-	lv_obj_set_user_data(bi->img,bi);
-	lv_obj_set_event_cb(bi->img,bootmenu_item_click);
-	lv_obj_set_style_local_outline_width(bi->img,LV_IMG_PART_MAIN,LV_STATE_FOCUSED,gui_dpi/100);
-	lv_obj_set_style_local_outline_color(bi->img,LV_IMG_PART_MAIN,LV_STATE_FOCUSED,lv_theme_get_color_primary());
-	lv_obj_set_style_local_radius(bi->img,LV_IMG_PART_MAIN,LV_STATE_FOCUSED,gui_dpi/50);
-	lv_img_ext_t*ext=lv_obj_get_ext_attr(bi->img);
+	bi->w_img=lv_obj_create(bi->btn);
+	lv_obj_set_size(bi->w_img,bm->si,bm->si);
+	lv_obj_clear_flag(bi->w_img,LV_OBJ_FLAG_CLICKABLE);
+	lv_obj_clear_flag(bi->w_img,LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_set_style_border_width(bi->w_img,0,0);
+	lv_obj_set_style_bg_opa(bi->w_img,LV_OPA_0,0);
+	lv_obj_set_grid_cell(
+		bi->w_img,
+		LV_GRID_ALIGN_STRETCH,0,1,
+		LV_GRID_ALIGN_STRETCH,0,1
+	);
+	bi->img=lv_img_create(bi->w_img);
+	lv_obj_clear_flag(bi->img,LV_OBJ_FLAG_CLICKABLE);
+	lv_img_t*ext=(lv_img_t*)bi->img;
+	lv_img_set_size_mode(bi->img,LV_IMG_SIZE_MODE_REAL);
 	lv_img_set_src(bi->img,c?bi->cfg.icon:"back.svg");
-	if((ext->w<=0||ext->h<=0))
-		lv_img_set_src(bi->img,"apps.svg");
+	if(ext->w<=0||ext->h<=0)lv_img_set_src(bi->img,"apps.svg");
 	lv_img_fill_image(bi->img,bm->si,bm->si);
-	lv_group_add_obj(gui_grp,bi->img);
+	lv_obj_center(bi->img);
 
-	bi->title=lv_label_create(line,NULL);
+	bi->title=lv_label_create(bi->btn);
+	lv_obj_clear_flag(bi->title,LV_OBJ_FLAG_CLICKABLE);
 	lv_label_set_long_mode(bi->title,bm->lm);
 	lv_label_set_text(bi->title,_(c?bi->cfg.desc:"Go back"));
-	lv_obj_set_width(bi->title,bm->bw-bm->si-(gui_font_size*2));
-	lv_obj_set_user_data(bi->title,bi);
-	lv_obj_align(
-		bi->title,NULL,
-		LV_ALIGN_IN_LEFT_MID,
-		gui_font_size+bm->si,0
+	lv_obj_set_align(bi->title,LV_ALIGN_LEFT_MID);
+	lv_obj_set_grid_cell(
+		bi->title,
+		LV_GRID_ALIGN_STRETCH,1,1,
+		LV_GRID_ALIGN_CENTER,0,1
+	);
+
+	bi->chk=lv_img_create(bi->btn);
+	lv_obj_add_flag(bi->chk,LV_OBJ_FLAG_HIDDEN);
+	lv_img_set_src(bi->chk,LV_SYMBOL_OK);
+	lv_img_set_size_mode(bi->chk,LV_IMG_SIZE_MODE_REAL);
+	lv_obj_set_grid_cell(
+		bi->chk,
+		LV_GRID_ALIGN_END,2,1,
+		LV_GRID_ALIGN_CENTER,0,1
 	);
 
 	if(c)switch(bi->cfg.mode){
@@ -315,12 +327,17 @@ static void bootmenu_add(struct bootmenu*bm,char*c){
 			if(!bm->do_auto_boot)break;
 			if(strcmp(bm->def,bi->cfg.ident)!=0)break;
 			lv_obj_set_enabled(bm->btn,true);
-			lv_obj_set_checked(bi->btn,true);
-			lv_group_focus_obj(bi->img);
+			lv_obj_clear_flag(bi->chk,LV_OBJ_FLAG_HIDDEN);
+			lv_obj_scroll_to_view(bi->btn,LV_ANIM_OFF);
+			lv_group_focus_obj(bi->btn);
 			bi->bm->selected=bi;
 		break;
 	}
-	lv_group_add_obj(gui_grp,bi->img);
+	lv_obj_add_event_cb(
+		bi->btn,
+		bootmenu_remove_auto_boot_cb,
+		LV_EVENT_FOCUSED,bm
+	);
 	return;
 	fail:
 	free(bi);
@@ -330,11 +347,9 @@ static int bootmenu_load(struct bootmenu*bm){
 	if(bm->items)bm->do_auto_boot=false;
 	lv_group_remove_obj(bm->btn);
 	bootmenu_clean(bm);
-	bm->bm=gui_font_size;
-	bm->bw=lv_page_get_scrl_width(bm->page)-bm->bm;
-	bm->bh=gui_font_size*3,bm->si=bm->bh-gui_font_size;
+	bm->si=gui_font_size*2;
 	bm->lm=confd_get_boolean("gui.text_scroll",true)?
-		LV_LABEL_LONG_SROLL_CIRC:
+		LV_LABEL_LONG_SCROLL_CIRCULAR:
 		LV_LABEL_LONG_DOT;
 	char**cs=confd_ls("boot.configs");
 	if(cs){
@@ -347,15 +362,14 @@ static int bootmenu_load(struct bootmenu*bm){
 		if(cs)free(cs);
 		if(bm->def)free(bm->def);
 		bm->def=NULL;
-		if(bm->selected&&bm->timeout>=0)bootmenu_auto_boot(bm);
+		if(bm->selected&&bm->timeout>=0)
+			bootmenu_auto_boot(bm);
 	}
 	lv_group_add_obj(gui_grp,bm->btn);
 	return 0;
 }
 
 static int bootmenu_draw(){
-	lv_coord_t bth=gui_font_size/2*5,btm=gui_dpi/5;
-	lv_coord_t pw=MIN((lv_coord_t)gui_w-(gui_dpi/4),(lv_coord_t)(gui_h/5*4));
 	struct bootmenu*bm=malloc(sizeof(struct bootmenu));
 	if(!bm)abort();
 	memset(bm,0,sizeof(struct bootmenu));
@@ -377,102 +391,76 @@ static int bootmenu_draw(){
 	}
 	bm->do_auto_boot=true;
 
-	bm->scr=lv_obj_create(lv_scr_act(),NULL);
+	bm->scr=lv_obj_create(lv_scr_act());
 	lv_obj_set_pos(bm->scr,0,0);
 	lv_obj_set_size(bm->scr,gui_w,gui_h);
-	lv_theme_apply(bm->scr,LV_THEME_SCR);
+	lv_obj_set_scroll_dir(bm->scr,LV_DIR_NONE);
+	lv_obj_set_style_bg_opa(bm->scr,LV_OPA_0,0);
+	lv_obj_set_style_radius(bm->scr,0,0);
+	lv_obj_set_style_border_width(bm->scr,0,0);
 
-	bm->bg=lv_img_create(bm->scr,NULL);
+	bm->bg=lv_img_create(bm->scr);
 	lv_obj_set_size(bm->bg,gui_sw,gui_sh);
 	lv_obj_set_pos(bm->bg,0,0);
 	lv_obj_set_hidden(bm->bg,true);
 	if(confd_get_boolean("gui.show_background",true)){
 		lv_img_set_src(bm->bg,bm->bg_path);
 		lv_img_fill_image(bm->bg,gui_sw,gui_sh);
-		lv_img_ext_t*x=lv_obj_get_ext_attr(bm->bg);
+		lv_img_t*x=(lv_img_t*)bm->bg;
 		if(x->w>0&&x->h>0)lv_obj_set_hidden(bm->bg,false);
 	}
 
 	char*title=confd_get_string("boot.title",NULL);
-	bm->title=lv_label_create(bm->scr,NULL);
+	bm->title=lv_label_create(bm->scr);
 	lv_label_set_text(bm->title,title?title:_("Boot Menu"));
-	lv_obj_set_style_local_bg_color(bm->title,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_COLOR_GRAY);
-	lv_obj_set_style_local_bg_opa(bm->title,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_OPA_50);
-	lv_obj_set_style_local_pad_ver(bm->title,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/4);
-	lv_obj_set_style_local_pad_hor(bm->title,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/2);
-	lv_obj_set_style_local_radius(bm->title,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/4);
+	lv_obj_set_style_bg_opa(bm->title,LV_OPA_0,0);
+	lv_obj_set_style_pad_ver(bm->title,gui_font_size/4,0);
+	lv_obj_set_style_pad_hor(bm->title,gui_font_size/2,0);
+	lv_obj_set_style_radius(bm->title,gui_font_size/4,0);
+	lv_obj_set_style_text_align(bm->title,LV_TEXT_ALIGN_CENTER,0);
 	if(title)free(title);
 
-	bm->page=lv_page_create(bm->scr,NULL);
-	lv_obj_set_style_local_border_width(bm->page,LV_PAGE_PART_BG,LV_STATE_DEFAULT,0);
-	lv_obj_set_style_local_border_width(bm->page,LV_PAGE_PART_BG,LV_STATE_FOCUSED,0);
-	lv_obj_set_style_local_border_width(bm->page,LV_PAGE_PART_BG,LV_STATE_PRESSED,0);
-	lv_obj_set_style_local_bg_color(bm->page,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_COLOR_GRAY);
-	lv_obj_set_style_local_bg_opa(bm->page,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_OPA_50);
+	bm->page=lv_obj_create(bm->scr);
+	lv_obj_set_scroll_dir(bm->page,LV_DIR_VER);
+	lv_obj_set_style_border_width(bm->page,0,0);
+	lv_obj_set_style_bg_opa(bm->page,LV_OPA_0,LV_STATE_DEFAULT);
+	lv_obj_add_event_cb(bm->page,bootmenu_remove_auto_boot_cb,LV_EVENT_PRESSING,bm);
 
-	bm->time=lv_label_create(bm->scr,NULL);
-	lv_obj_set_style_local_text_font(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_small);
-	lv_obj_set_style_local_bg_color(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_COLOR_GRAY);
-	lv_obj_set_style_local_bg_opa(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,LV_OPA_50);
-	lv_obj_set_style_local_pad_ver(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/4);
-	lv_obj_set_style_local_pad_hor(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/2);
-	lv_obj_set_style_local_radius(bm->time,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size/4);
+	bm->time=lv_label_create(bm->scr);
+	lv_obj_set_style_text_font(bm->time,gui_font_small,LV_STATE_DEFAULT);
 	lv_obj_set_hidden(bm->time,true);
 
-	bm->btn=lv_btn_create(bm->scr,NULL);
-	lv_label_set_text(lv_label_create(bm->btn,NULL),_("Boot"));
-	lv_obj_set_checked(bm->btn,true);
+	bm->btn=lv_btn_create(bm->scr);
+	lv_obj_t*lbl=lv_label_create(bm->btn);
+	lv_label_set_text(lbl,_("Boot"));
 	lv_obj_set_enabled(bm->btn,false);
-	lv_obj_set_state(bm->btn,LV_STATE_CHECKED);
 	lv_obj_set_user_data(bm->btn,bm);
-	lv_obj_set_event_cb(bm->btn,bootmenu_click);
-	lv_obj_set_style_local_radius(
-		bm->btn,
-		LV_BTN_PART_MAIN,
-		LV_STATE_DEFAULT,
-		gui_font_size/2
-	);
+	lv_obj_add_event_cb(bm->btn,bootmenu_click,LV_EVENT_CLICKED,bm);
 
-	lv_obj_set_width(bm->page,pw);
-	if(gui_w>gui_h){
-		lv_coord_t lw=gui_w-pw-gui_font_size;
-		lv_obj_set_size(bm->btn,gui_h/3,bth);
-		lv_obj_set_pos(
-			bm->title,
-			(lw-lv_obj_get_width(bm->title))/2,
-			gui_h/4-lv_obj_get_height(bm->title)
-		);
-		lv_obj_set_pos(
-			bm->btn,
-			(lw-lv_obj_get_width(bm->btn))/2,
-			gui_h/2+bth
-		);
-		lv_obj_align(
-			bm->page,NULL,
-			LV_ALIGN_IN_TOP_RIGHT,
-			-(gui_font_size/2),
-			gui_font_size/2
-		);
-		lv_obj_set_height(bm->page,gui_h-gui_font_size);
-	}else{
-		lv_obj_align(bm->title,NULL,LV_ALIGN_IN_TOP_MID,0,gui_font_size);
-		lv_obj_align(
-			bm->page,NULL,
-			LV_ALIGN_IN_TOP_MID,
-			0,lv_obj_get_y(bm->title)+
-			lv_obj_get_height(bm->title)+
-			gui_font_size
-		);
-		lv_obj_set_size(bm->btn,gui_w/3,bth);
-		lv_obj_align(bm->btn,0,LV_ALIGN_IN_BOTTOM_MID,0,-btm);
-		lv_obj_set_height(
-			bm->page,
-			lv_obj_get_y(bm->btn)-
-			lv_obj_get_y(bm->page)-
-			lv_obj_get_height(bm->time)-
-			gui_font_size
-		);
-	}
+	lv_obj_set_flex_flow(bm->scr,LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_flex_align(
+		bm->scr,
+		LV_FLEX_ALIGN_CENTER,
+		LV_FLEX_ALIGN_CENTER,
+		LV_FLEX_ALIGN_CENTER
+	);
+	lv_obj_set_style_max_width(bm->btn,gui_dpi*2,0);
+	lv_obj_set_width(bm->page,lv_pct(100));
+	lv_obj_set_width(bm->btn,lv_pct(100));
+	lv_obj_set_flex_grow(bm->page,1);
+	lv_obj_center(bm->btn);
+	lv_obj_center(lbl);
+
+	lv_obj_set_flex_flow(bm->page,LV_FLEX_FLOW_ROW_WRAP);
+	lv_obj_update_layout(bm->page);
+	bm->bw=(
+		lv_obj_get_width(bm->page)-
+		lv_obj_get_style_pad_right(bm->page,0)-
+		lv_obj_get_style_pad_column(bm->page,0)-
+		lv_obj_get_style_pad_left(bm->page,0)
+	)/lv_coord_border(confd_get_integer(
+		"gui.bootmenu.column",gui_w>gui_h?2:1
+	),255,1);
 	bootmenu_load(bm);
 	return 0;
 }

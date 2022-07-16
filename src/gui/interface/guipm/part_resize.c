bc0a58e68107626210f06f2156c7a9ecad01aec0
BigfootACA <bigfoot@classfun.cn>
2022-07-16 19:27:27 +0800

    gui: interface: guipm: upgrade part_resize.c

--- src/gui/interface/guipm/part_resize.c
+++ src/gui/interface/guipm/part_resize.c
@@ -7,7 +7,6 @@
  */
 
 #ifdef ENABLE_GUI
-#ifdef ENABLE_FDISK
 #define _GNU_SOURCE
 #include<stddef.h>
 #include<stdlib.h>
@@ -18,7 +17,6 @@
 #include"array.h"
 #include"logger.h"
 #include"gui/activity.h"
-#include"gui/tools.h"
 #define TAG "guipm"
 
 static void reload_info(struct part_resize_info*pi){
@@ -39,6 +37,7 @@ static void reload_info(struct part_resize_info*pi){
 static int guipm_part_get_focus(struct gui_activity*d){
 	struct part_resize_info*pi=d->data;
 	if(!pi)return 0;
+	pi->size.on_get_focus(&pi->size);
 	lv_group_add_obj(gui_grp,pi->ok);
 	lv_group_add_obj(gui_grp,pi->cancel);
 	return 0;
@@ -47,15 +46,14 @@ static int guipm_part_get_focus(struct gui_activity*d){
 static int guipm_part_lost_focus(struct gui_activity*d){
 	struct part_resize_info*pi=d->data;
 	if(!pi)return 0;
+	pi->size.on_lost_focus(&pi->size);
 	lv_group_remove_obj(pi->ok);
 	lv_group_remove_obj(pi->cancel);
 	return 0;
 }
 
-static void ok_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct part_resize_info*pi=lv_obj_get_user_data(obj);
-	if(!pi||obj!=pi->ok)return;
+static void ok_cb(lv_event_t*e){
+	struct part_resize_info*pi=e->user_data;
 	if(pi->size.sec!=pi->part->size_sec){
 		size_t pn=fdisk_partition_get_partno(pi->part->part);
 		struct fdisk_partition*np=fdisk_new_partition();
@@ -74,68 +72,106 @@ static void ok_cb(lv_obj_t*obj,lv_event_t e){
 			fdisk_unref_partition(np);
 			return;
 		}
-		tlog_debug("change partition %zu size to %zu sectors",pn+1,pi->size.sec);
+		tlog_debug(
+			"change partition %zu size to %zu sectors",
+			pn+1,pi->size.sec
+		);
 		fdisk_unref_partition(np);
+		pi->act->data_changed=true;
 	}
 	guiact_do_back();
 }
 
-static void cancel_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct part_resize_info*pi=lv_obj_get_user_data(obj);
-	if(!pi||obj!=pi->cancel)return;
+static void cancel_cb(lv_event_t*e __attribute__((unused))){
 	guiact_do_back();
 }
 
-static int guipm_draw_resize_partition(struct gui_activity*act){
+static int guipm_part_init(struct gui_activity*act){
+	struct part_resize_info*pi;
 	struct part_partition_info*info=act->args;
 	if(!info||info->free||!info->di)return -EINVAL;
-	struct part_resize_info*pi=malloc(sizeof(struct part_resize_info));
-	if(!pi)return -ENOMEM;
+	if(!(pi=malloc(sizeof(struct part_resize_info))))return -ENOMEM;
 	memset(pi,0,sizeof(struct part_resize_info));
-	pi->part=info,act->data=pi;
+	pi->part=info,act->data=pi,pi->act=act;
+	return 0;
+}
 
-	pi->box=lv_page_create(act->page,NULL);
-	lv_obj_set_style_local_pad_all(pi->box,LV_PAGE_PART_BG,LV_STATE_DEFAULT,gui_font_size);
-	lv_obj_set_width(pi->box,gui_sw/8*7);
-	lv_obj_set_click(pi->box,false);
-	lv_coord_t h=0;
-	lv_coord_t w=lv_page_get_scrl_width(pi->box);
+static int guipm_part_exit(struct gui_activity*act){
+	if(act->data)free(act->data);
+	act->data=NULL;
+	return 0;
+}
+
+static int guipm_draw_resize_partition(struct gui_activity*act){
+	struct part_resize_info*pi=act->data;
+	static lv_coord_t grid_col[]={
+		LV_GRID_FR(1),
+		LV_GRID_FR(1),
+		LV_GRID_TEMPLATE_LAST
+	},grid_row[]={
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	};
+
+	pi->box=lv_obj_create(act->page);
+	lv_obj_set_style_pad_all(pi->box,gui_font_size,0);
+	lv_obj_set_style_pad_row(pi->box,gui_font_size,0);
+	lv_obj_set_style_max_width(pi->box,lv_pct(80),0);
+	lv_obj_set_style_max_height(pi->box,lv_pct(80),0);
+	lv_obj_set_style_min_width(pi->box,gui_dpi*2,0);
+	lv_obj_set_style_min_height(pi->box,gui_font_size*2,0);
+	lv_obj_set_grid_dsc_array(pi->box,grid_col,grid_row);
+	lv_obj_set_height(pi->box,LV_SIZE_CONTENT);
+	lv_obj_center(pi->box);
 
 	// Title
-	lv_obj_t*title=lv_label_create(pi->box,NULL);
+	lv_obj_t*title=lv_label_create(pi->box);
 	lv_label_set_text(title,_("Resize Partition"));
-	lv_label_set_long_mode(title,LV_LABEL_LONG_BREAK);
-	lv_obj_set_width(title,w);
-	lv_obj_set_y(title,h);
-	lv_label_set_align(title,LV_LABEL_ALIGN_CENTER);
-	h+=lv_obj_get_height(title);
+	lv_label_set_long_mode(title,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(title,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_grid_cell(
+		title,
+		LV_GRID_ALIGN_CENTER,0,2,
+		LV_GRID_ALIGN_CENTER,0,1
+	);
 
-	guipm_init_size_block(&h,w,pi->box,pi,pi->part->di->lsec_size,&pi->size,"Size:");
+	guipm_init_size_block(
+		pi->box,pi,
+		pi->part->di->lsec_size,
+		&pi->size,
+		"Size:"
+	);
+	lv_obj_set_grid_cell(
+		pi->size.box,
+		LV_GRID_ALIGN_STRETCH,0,2,
+		LV_GRID_ALIGN_STRETCH,1,1
+	);
 
 	// OK Button
-	h+=gui_font_size;
-	pi->ok=lv_btn_create(pi->box,NULL);
-	lv_style_set_action_button(pi->ok,true);
-	lv_obj_set_size(pi->ok,w/2-gui_font_size,gui_font_size*2);
-	lv_obj_align(pi->ok,NULL,LV_ALIGN_IN_TOP_LEFT,(gui_font_size/2),h);
-	lv_obj_set_user_data(pi->ok,pi);
-	lv_obj_set_event_cb(pi->ok,ok_cb);
-	lv_label_set_text(lv_label_create(pi->ok,NULL),_("OK"));
+	pi->ok=lv_btn_create(pi->box);
+	lv_obj_add_event_cb(pi->ok,ok_cb,LV_EVENT_CLICKED,pi);
+	lv_obj_t*lbl_ok=lv_label_create(pi->ok);
+	lv_label_set_text(lbl_ok,_("OK"));
+	lv_obj_center(lbl_ok);
+	lv_obj_set_grid_cell(
+		pi->ok,
+		LV_GRID_ALIGN_STRETCH,0,1,
+		LV_GRID_ALIGN_STRETCH,2,1
+	);
 
 	// Cancel Button
-	pi->cancel=lv_btn_create(pi->box,NULL);
-	lv_style_set_action_button(pi->cancel,true);
-	lv_obj_set_size(pi->cancel,w/2-gui_font_size,gui_font_size*2);
-	lv_obj_align(pi->cancel,NULL,LV_ALIGN_IN_TOP_RIGHT,-(gui_font_size/2),h);
-	lv_obj_set_user_data(pi->cancel,pi);
-	lv_obj_set_event_cb(pi->cancel,cancel_cb);
-	lv_label_set_text(lv_label_create(pi->cancel,NULL),_("Cancel"));
-	h+=lv_obj_get_height(pi->cancel);
-
-	h+=gui_font_size*3;
-	lv_obj_set_height(pi->box,MIN(h,(lv_coord_t)gui_sh/6*5));
-	lv_obj_align(pi->box,NULL,LV_ALIGN_CENTER,0,0);
+	pi->cancel=lv_btn_create(pi->box);
+	lv_obj_add_event_cb(pi->cancel,cancel_cb,LV_EVENT_CLICKED,NULL);
+	lv_obj_t*lbl_cancel=lv_label_create(pi->cancel);
+	lv_label_set_text(lbl_cancel,_("Cancel"));
+	lv_obj_center(lbl_cancel);
+	lv_obj_set_grid_cell(
+		pi->cancel,
+		LV_GRID_ALIGN_STRETCH,1,1,
+		LV_GRID_ALIGN_STRETCH,2,1
+	);
 
 	reload_info(pi);
 	return 0;
@@ -147,11 +183,12 @@ struct gui_register guireg_guipm_resize_partition={
 	.icon="guipm.svg",
 	.show_app=false,
 	.open_file=false,
+	.init=guipm_part_init,
 	.get_focus=guipm_part_get_focus,
 	.lost_focus=guipm_part_lost_focus,
 	.draw=guipm_draw_resize_partition,
+	.quiet_exit=guipm_part_exit,
 	.back=true,
 	.mask=true,
 };
-#endif
 #endif

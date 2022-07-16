978eb10bb85abccf7e32ef04c1f4eb024a810893
BigfootACA <bigfoot@classfun.cn>
2022-07-16 09:55:36 +0800

    gui: interface: guipm: disk_operation.c: expand code

--- src/gui/interface/guipm/disk_operation.c
+++ src/gui/interface/guipm/disk_operation.c
@@ -7,7 +7,6 @@
  */
 
 #ifdef ENABLE_GUI
-#ifdef ENABLE_FDISK
 #define _GNU_SOURCE
 #include<stdbool.h>
 #include<libfdisk/libfdisk.h>
@@ -17,7 +16,33 @@
 #include"gui/activity.h"
 #define TAG "guipm"
 
-static bool mk_label_cb(uint16_t id,const char*btn __attribute__((unused)),void*user_data){
+enum disk_operation{
+	DISK_OPER_CANCEL,
+	DISK_OPER_MOUNT,
+	DISK_OPER_CREATE_LABEL,
+	DISK_OPER_ERASE,
+	DISK_OPER_SAVE_IMAGE,
+	DISK_OPER_RESTORE_IMAGE,
+	DISK_OPER_USB_MASS,
+	DISK_OPER_LAST,
+};
+
+static const char*disk_operations[]={
+	[DISK_OPER_CANCEL]        = "Cancel",
+	[DISK_OPER_MOUNT]         = "Mount the whole disk",
+	[DISK_OPER_CREATE_LABEL]  = "Create new disk label",
+	[DISK_OPER_ERASE]         = "Erase the disk",
+	[DISK_OPER_SAVE_IMAGE]    = "Save disk image",
+	[DISK_OPER_RESTORE_IMAGE] = "Restore disk image",
+	[DISK_OPER_USB_MASS]      = "USB Gadget Mass Storage",
+	[DISK_OPER_LAST]          = ""
+};
+
+static bool mk_label_cb(
+	uint16_t id,
+	const char*btn __attribute__((unused)),
+	void*user_data
+){
 	struct fdisk_context*ctx=user_data;
 	char*type=NULL;
 	switch(id){
@@ -29,7 +54,10 @@ static bool mk_label_cb(uint16_t id,const char*btn __attribute__((unused)),void*
 		case 5:type="sun";break;
 		default:msgbox_alert("Unknown disk label");break;
 	}
-	tlog_debug("make new disk label %s on %s",type,fdisk_get_devname(ctx));
+	tlog_debug(
+		"make new disk label %s on %s",
+		type,fdisk_get_devname(ctx)
+	);
 	if((errno=fdisk_create_disklabel(ctx,type))!=0){
 		if(errno<0)errno=-(errno);
 		telog_warn("create disk label failed");
@@ -38,7 +66,11 @@ static bool mk_label_cb(uint16_t id,const char*btn __attribute__((unused)),void*
 	return false;
 }
 
-static bool ask_label_cb(uint16_t id,const char*btn __attribute__((unused)),void*user_data){
+static bool ask_label_cb(
+	uint16_t id,
+	const char*btn __attribute__((unused)),
+	void*user_data
+){
 	static const char*btns[]={
 		"Cancel",
 		"GPT (EFI Guid)",
@@ -48,11 +80,17 @@ static bool ask_label_cb(uint16_t id,const char*btn __attribute__((unused)),void
 		"SUN",
 		""
 	};
-	if(id==0)msgbox_set_user_data(msgbox_create_custom(mk_label_cb,btns,"Select disk label"),user_data);
+	if(id==0)msgbox_set_user_data(msgbox_create_custom(
+		mk_label_cb,btns,"Select disk label"
+	),user_data);
 	return false;
 }
 
-static bool add_mass_cb(uint16_t id,const char*btn __attribute__((unused)),void*user_data){
+static bool add_mass_cb(
+	uint16_t id,
+	const char*btn __attribute__((unused)),
+	void*user_data
+){
 	struct fdisk_context*ctx=user_data;
 	if(id==0){
 		if(!guipm_save_label(ctx))return false;
@@ -64,7 +102,11 @@ static bool add_mass_cb(uint16_t id,const char*btn __attribute__((unused)),void*
 	return false;
 }
 
-static bool add_mount_cb(uint16_t id,const char*btn __attribute__((unused)),void*user_data){
+static bool add_mount_cb(
+	uint16_t id,
+	const char*btn __attribute__((unused)),
+	void*user_data
+){
 	struct fdisk_context*ctx=user_data;
 	if(id==0){
 		if(!guipm_save_label(ctx))return false;
@@ -76,15 +118,19 @@ static bool add_mount_cb(uint16_t id,const char*btn __attribute__((unused)),void
 	return false;
 }
 
-static bool disk_menu_cb(uint16_t id,const char*btn __attribute__((unused)),void*user_data){
+static bool disk_menu_cb(
+	uint16_t id,
+	const char*btn __attribute__((unused)),
+	void*user_data
+){
 	struct fdisk_context*ctx=user_data;
 	bool ro=fdisk_is_readonly(ctx);
 	switch(id){
-		case 0:break;
-		case 1:
+		case DISK_OPER_CANCEL:break;
+		case DISK_OPER_MOUNT:
 			guipm_ask_save_label(ctx,add_mount_cb,user_data);
 		break;
-		case 2:
+		case DISK_OPER_CREATE_LABEL:
 			if(ro)goto readonly;
 			msgbox_set_user_data(msgbox_create_yesno(
 				ask_label_cb,
@@ -93,7 +139,7 @@ static bool disk_menu_cb(uint16_t id,const char*btn __attribute__((unused)),void
 				"IN THE DISK WILL BE LOST. Are you sure you want to continue?"
 			),user_data);
 		break;
-		case 6:
+		case DISK_OPER_USB_MASS:
 			guipm_ask_save_label(ctx,add_mass_cb,user_data);
 		break;
 		default:msgbox_alert("This function does not implemented");break;
@@ -104,17 +150,9 @@ static bool disk_menu_cb(uint16_t id,const char*btn __attribute__((unused)),void
 }
 
 void guipm_disk_operation_menu(struct fdisk_context*ctx){
-	static const char*btns[]={
-		"Cancel",
-		"Mount the whole disk",
-		"Create new disk label",
-		"Erase the disk",
-		"Save disk image",
-		"Restore disk image",
-		"USB Gadget Mass Storage",
-		""
-	};
-	msgbox_set_user_data(msgbox_create_custom(disk_menu_cb,btns,"Select operation"),ctx);
+	msgbox_set_user_data(msgbox_create_custom(
+		disk_menu_cb,disk_operations,
+		"Select operation"
+	),ctx);
 }
-#endif
 #endif

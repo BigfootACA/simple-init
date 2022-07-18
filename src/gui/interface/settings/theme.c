b8cb4bac1bd322fe98223a8a27ce3ebc05ca04ea
BigfootACA <bigfoot@classfun.cn>
2022-07-18 17:53:34 +0800

    gui: interface: settings: upgrade theme.c

--- src/gui/interface/settings/theme.c
+++ src/gui/interface/settings/theme.c
@@ -12,7 +12,6 @@
 #include"logger.h"
 #include"defines.h"
 #include"gui/tools.h"
-#include"gui/sysbar.h"
 #include"gui/activity.h"
 #include"gui/filepicker.h"
 #define TAG "theme"
@@ -22,74 +21,42 @@ struct theme_menu{
 	lv_obj_t*title,*txt_bg,*btn_bg,*chk_dark;
 	lv_obj_t*btn_ok,*btn_restart;
 	lv_obj_t*lbl_tips,*lbl_tip_icon;
+	lv_obj_t*input_box,*tip_line,*btns;
 };
 
-static void dark_switch(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_VALUE_CHANGED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->chk_dark)return;
-	bool val=lv_checkbox_is_checked(obj);
+static void dark_switch(lv_event_t*e){
+	struct theme_menu*tm=e->user_data;
+	bool val=lv_obj_is_checked(e->target);
 	if(val==gui_dark)return;
-	lv_obj_set_x(tm->lbl_tip_icon,gui_font_size);
-	lv_obj_set_x(tm->lbl_tips,gui_font_size+lv_obj_get_width(tm->lbl_tip_icon));
+	lv_obj_clear_flag(tm->tip_line,LV_OBJ_FLAG_HIDDEN);
 	gui_dark=val;
 	confd_set_boolean("gui.dark",gui_dark);
 }
 
-static void text_scroll(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_VALUE_CHANGED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->chk_scroll)return;
-	confd_set_boolean("gui.text_scroll",lv_checkbox_is_checked(obj));
+static void text_scroll(lv_event_t*e){
+	confd_set_boolean("gui.text_scroll",lv_obj_is_checked(e->target));
 }
 
 static void do_save(struct theme_menu*tm){
-	confd_set_boolean("gui.show_background",lv_checkbox_is_checked(tm->chk_bg));
+	confd_set_boolean("gui.show_background",lv_obj_is_checked(tm->chk_bg));
 	confd_set_string("gui.background",(char*)lv_textarea_get_text(tm->txt_bg));
 }
 
-static bool select_cb(bool ok,const char**path,uint16_t cnt,void*user_data __attribute__((unused))){
-	struct theme_menu*tm=user_data;
-	if(!ok||!tm)return false;
-	if(!path||!path[0]||path[1]||cnt!=1)return true;
-	lv_textarea_set_text(tm->txt_bg,path[0]+2);
-	return false;
-}
-
-static void bg_btn(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->btn_bg)return;
-	filepicker_set_max_item(filepicker_create(select_cb,"Select background"),1);
-}
-
-static void bg_text(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->txt_bg)return;
-	sysbar_focus_input(obj);
-	sysbar_keyboard_open();
-}
-
-static void ok_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->btn_ok)return;
+static void ok_click(lv_event_t*e){
+	struct theme_menu*tm=e->user_data;
 	do_save(tm);
 	guiact_do_back();
 }
 
-static void do_restart(lv_task_t*t __attribute__((unused))){
+static void do_restart(lv_timer_t*t __attribute__((unused))){
 	gui_screen_init();
 	gui_draw();
 }
 
-static void restart_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct theme_menu*tm=lv_obj_get_user_data(obj);
-	if(obj!=tm->btn_restart)return;
+static void restart_click(lv_event_t*e){
+	struct theme_menu*tm=e->user_data;
 	do_save(tm);
-	lv_task_once(lv_task_create(do_restart,100,LV_TASK_PRIO_LOWEST,NULL));
+	lv_timer_set_repeat_count(lv_timer_create(do_restart,100,NULL),1);
 }
 
 static int theme_menu_init(struct gui_activity*act){
@@ -106,202 +73,54 @@ static int theme_menu_clean(struct gui_activity*act){
 	return 0;
 }
 
-static int theme_menu_resize(struct gui_activity*act){
-	struct theme_menu*tm=act->data;
-	int bts=gui_font_size+(gui_dpi/8);
-	if(!tm)return -1;
-	lv_obj_set_width(tm->box,gui_sw/6*5);
-	lv_obj_set_width(
-		tm->title,
-		lv_obj_get_width(tm->box)
-	);
-	lv_obj_align(
-		tm->title,NULL,
-		LV_ALIGN_IN_TOP_MID,
-		0,gui_font_size
-	);
-	lv_obj_set_width(
-		tm->chk_bg,
-		lv_obj_get_width(tm->box)-
-		gui_font_size*2
-	);
-	lv_obj_align(
-		tm->chk_bg,
-		tm->title,
-		LV_ALIGN_OUT_BOTTOM_LEFT,
-		gui_font_size,gui_font_size
-	);
-	lv_obj_set_width(
-		tm->txt_bg,
-		lv_obj_get_width(tm->box)-
-		gui_font_size*2
-	);
-	lv_obj_align(
-		tm->txt_bg,
-		tm->chk_bg,
-		LV_ALIGN_OUT_BOTTOM_LEFT,
-		0,gui_font_size
-	);
-	lv_obj_set_size(
-		tm->btn_bg,
-		gui_font_size*3,
-		lv_obj_get_height(tm->txt_bg)
-	);
-	lv_obj_set_width(
-		tm->txt_bg,
-		lv_obj_get_width(tm->box)-
-		lv_obj_get_width(tm->btn_bg)-
-		gui_font_size*2
-	);
-	lv_obj_align(
-		tm->btn_bg,
-		tm->txt_bg,
-		LV_ALIGN_OUT_RIGHT_MID,
-		gui_font_size/4,0
-	);
-	lv_obj_set_width(
-		tm->chk_scroll,
-		lv_obj_get_width(tm->box)-
-		gui_font_size*2
-	);
-	lv_obj_align(
-		tm->chk_scroll,
-		tm->txt_bg,
-		LV_ALIGN_OUT_BOTTOM_MID,
-		0,gui_font_size
-	);
-	lv_obj_set_x(
-		tm->chk_scroll,
-		(lv_obj_get_width(tm->box)-
-		lv_obj_get_width(tm->chk_scroll))/2
-	);
-	lv_obj_set_width(
-		tm->chk_dark,
-		lv_obj_get_width(tm->box)-
-		gui_font_size*2
-	);
-	lv_obj_align(
-		tm->chk_dark,
-		tm->chk_scroll,
-		LV_ALIGN_OUT_BOTTOM_MID,
-		0,gui_font_size
-	);
-	lv_obj_align(
-		tm->lbl_tip_icon,
-		tm->chk_dark,
-		LV_ALIGN_OUT_BOTTOM_LEFT,
-		0,gui_font_size
-	);
-	lv_obj_set_x(tm->lbl_tip_icon,gui_font_size);
-	lv_obj_set_width(
-		tm->lbl_tips,
-		lv_obj_get_width(tm->box)-
-		gui_font_size*2-
-		lv_obj_get_width(tm->lbl_tip_icon)
-	);
-	lv_obj_align(
-		tm->lbl_tips,
-		tm->lbl_tip_icon,
-		LV_ALIGN_OUT_RIGHT_MID,
-		gui_font_size,
-		gui_font_size/4
-	);
-	lv_obj_set_size(
-		tm->btn_ok,
-		lv_obj_get_width(tm->box)/2-
-		gui_font_size/3*4,bts
-	);
-	lv_obj_align(
-		tm->btn_ok,
-		tm->lbl_tips,
-		LV_ALIGN_OUT_BOTTOM_LEFT,
-		0,gui_font_size
-	);
-	lv_obj_set_x(tm->btn_ok,gui_font_size);
-	lv_obj_set_size(
-		tm->btn_restart,
-		lv_obj_get_width(tm->box)/2-
-		gui_font_size/3*4,bts
-	);
-	lv_obj_align(
-		tm->btn_restart,
-		tm->lbl_tips,
-		LV_ALIGN_OUT_BOTTOM_RIGHT,
-		0,gui_font_size
-	);
-	lv_obj_set_x(
-		tm->btn_restart,
-		lv_obj_get_width(tm->box)-
-		lv_obj_get_width(tm->btn_restart)-
-		gui_font_size
-	);
-	lv_obj_set_x(
-		tm->lbl_tips,
-		lv_obj_get_width(tm->lbl_tips)*2
-	);
-	lv_obj_set_x(
-		tm->lbl_tip_icon,
-		lv_obj_get_width(tm->lbl_tips)*2
-	);
-	lv_obj_set_height(tm->box,
-		lv_obj_get_y(tm->chk_dark)+
-		lv_obj_get_height(tm->lbl_tips)+
-		lv_obj_get_height(tm->btn_ok)+
-		(gui_font_size*3)+
-		bts
-	);
-	lv_obj_align(tm->box,NULL,LV_ALIGN_CENTER,0,0);
-	return 0;
-}
-
 static int theme_menu_draw(struct gui_activity*act){
+	static lv_coord_t grid_col[]={
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	},grid_col_f[]={
+		LV_GRID_FR(1),
+		LV_GRID_FR(1),
+		LV_GRID_TEMPLATE_LAST
+	},grid_row[]={
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	};
 	struct theme_menu*tm=act->data;
 	if(!tm)return -1;
 
-	tm->box=lv_obj_create(act->page,NULL);
-	lv_obj_set_click(tm->box,false);
-	lv_obj_set_style_local_pad_all(
-		tm->box,
-		LV_PAGE_PART_BG,
-		LV_STATE_DEFAULT,
-		gui_font_size
-	);
+	tm->box=lv_obj_create(act->page);
+	lv_obj_set_flex_flow(tm->box,LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_style_pad_all(tm->box,gui_font_size/2,0);
+	lv_obj_set_style_pad_row(tm->box,gui_font_size/2,0);
+	lv_obj_set_style_max_width(tm->box,lv_pct(80),0);
+	lv_obj_set_style_max_height(tm->box,lv_pct(80),0);
+	lv_obj_set_style_min_width(tm->box,gui_dpi*2,0);
+	lv_obj_set_style_min_height(tm->box,gui_font_size*2,0);
+	lv_obj_set_height(tm->box,LV_SIZE_CONTENT);
+	lv_obj_center(tm->box);
 
-	tm->title=lv_label_create(tm->box,NULL);
-	lv_label_set_long_mode(tm->title,LV_LABEL_LONG_BREAK);
-	lv_label_set_align(tm->title,LV_LABEL_ALIGN_CENTER);
+	tm->title=lv_label_create(tm->box);
 	lv_label_set_text(tm->title,_("Select theme"));
+	lv_label_set_long_mode(tm->title,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(tm->title,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_width(tm->title,lv_pct(100));
 
-	tm->chk_bg=lv_checkbox_create(tm->box,NULL);
-	lv_checkbox_set_checked(
+	tm->input_box=lv_draw_input(
+		tm->box,_("Background"),
+		&tm->chk_bg,NULL,
+		&tm->txt_bg,&tm->btn_bg
+	);
+	lv_obj_set_checked(
 		tm->chk_bg,
 		confd_get_boolean("gui.show_background",true)
 	);
-	lv_checkbox_set_text(tm->chk_bg,_("Background"));
-
-	tm->txt_bg=lv_textarea_create(tm->box,NULL);
 	char*bg=confd_get_string("gui.background",NULL);
 	lv_textarea_set_text(tm->txt_bg,bg?bg:"");
-	lv_textarea_set_one_line(tm->txt_bg,true);
-	lv_textarea_set_cursor_hidden(tm->txt_bg,true);
-	lv_obj_set_user_data(tm->txt_bg,tm);
-	lv_obj_set_event_cb(tm->txt_bg,bg_text);
 	if(bg)free(bg);
 
-	tm->btn_bg=lv_btn_create(tm->box,NULL);
-	lv_style_set_action_button(tm->btn_bg,true);
-	lv_obj_set_user_data(tm->btn_bg,tm);
-	lv_obj_set_event_cb(tm->btn_bg,bg_btn);
-	lv_obj_set_style_local_radius(
-		tm->btn_bg,
-		LV_BTN_PART_MAIN,
-		LV_STATE_DEFAULT,
-		gui_font_size/2
-	);
-	lv_label_set_text(lv_label_create(tm->btn_bg,NULL),"...");
-
-	tm->chk_scroll=lv_checkbox_create(tm->box,NULL);
-	lv_checkbox_set_checked(
+	tm->chk_scroll=lv_checkbox_create(tm->box);
+	lv_obj_set_checked(
 		tm->chk_scroll,
 		confd_get_boolean("gui.text_scroll",true)
 	);
@@ -309,44 +128,54 @@ static int theme_menu_draw(struct gui_activity*act){
 		tm->chk_scroll,
 		_("Scroll text when overflow")
 	);
-	lv_obj_set_user_data(tm->chk_scroll,tm);
-	lv_obj_set_event_cb(tm->chk_scroll,text_scroll);
+	lv_obj_add_event_cb(tm->chk_scroll,text_scroll,LV_EVENT_VALUE_CHANGED,tm);
 
-	tm->chk_dark=lv_checkbox_create(tm->box,NULL);
-	lv_checkbox_set_checked(tm->chk_dark,gui_dark);
+	tm->chk_dark=lv_checkbox_create(tm->box);
+	lv_obj_set_checked(tm->chk_dark,gui_dark);
 	lv_checkbox_set_text(tm->chk_dark,_("Dark Mode"));
-	lv_obj_set_user_data(tm->chk_dark,tm);
-	lv_obj_set_event_cb(tm->chk_dark,dark_switch);
+	lv_obj_add_event_cb(tm->chk_dark,dark_switch,LV_EVENT_VALUE_CHANGED,tm);
 
-	tm->lbl_tip_icon=lv_label_create(tm->box,NULL);
-	lv_label_set_align(tm->lbl_tip_icon,LV_LABEL_ALIGN_CENTER);
+	tm->tip_line=lv_obj_create(tm->box);
+	lv_obj_add_flag(tm->tip_line,LV_OBJ_FLAG_HIDDEN);
+	lv_obj_set_style_border_width(tm->tip_line,0,0);
+	lv_obj_set_style_pad_all(tm->tip_line,0,0);
+	lv_obj_set_grid_dsc_array(tm->tip_line,grid_col,grid_row);
+	lv_obj_set_size(tm->tip_line,lv_pct(100),LV_SIZE_CONTENT);
+
+	tm->lbl_tip_icon=lv_label_create(tm->tip_line);
+	lv_obj_set_style_text_align(tm->lbl_tip_icon,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_grid_cell(tm->lbl_tip_icon,LV_GRID_ALIGN_CENTER,0,1,LV_GRID_ALIGN_CENTER,0,1);
 	lv_label_set_text(tm->lbl_tip_icon,LV_SYMBOL_WARNING);
 
-	tm->lbl_tips=lv_label_create(tm->box,NULL);
-	lv_label_set_long_mode(tm->lbl_tips,LV_LABEL_LONG_BREAK);
-	lv_label_set_align(tm->lbl_tips,LV_LABEL_ALIGN_LEFT);
-	lv_obj_set_small_text_font(tm->lbl_tips,LV_LABEL_PART_MAIN);
+	tm->lbl_tips=lv_label_create(tm->tip_line);
+	lv_label_set_long_mode(tm->lbl_tips,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(tm->lbl_tips,LV_TEXT_ALIGN_LEFT,0);
+	lv_obj_set_small_text_font(tm->lbl_tips,LV_PART_MAIN);
+	lv_obj_set_grid_cell(tm->lbl_tips,LV_GRID_ALIGN_START,1,1,LV_GRID_ALIGN_CENTER,0,1);
 	lv_label_set_text(
 		tm->lbl_tips,
 		_("Need to restart GUI when changing theme")
 	);
 
-	tm->btn_ok=lv_btn_create(tm->box,NULL);
-	lv_obj_set_user_data(tm->btn_ok,tm);
-	lv_obj_set_event_cb(tm->btn_ok,ok_click);
-	lv_style_set_action_button(tm->btn_ok,true);
-	lv_label_set_text(
-		lv_label_create(tm->btn_ok,NULL),
-		_("OK")
-	);
-	tm->btn_restart=lv_btn_create(tm->box,NULL);
-	lv_obj_set_user_data(tm->btn_restart,tm);
-	lv_obj_set_event_cb(tm->btn_restart,restart_click);
-	lv_style_set_action_button(tm->btn_restart,true);
-	lv_label_set_text(
-		lv_label_create(tm->btn_restart,NULL),
-		_("Restart")
-	);
+	tm->btns=lv_obj_create(tm->box);
+	lv_obj_set_style_border_width(tm->btns,0,0);
+	lv_obj_set_style_pad_all(tm->btns,gui_font_size/4,0);
+	lv_obj_set_grid_dsc_array(tm->btns,grid_col_f,grid_row);
+	lv_obj_set_size(tm->btns,lv_pct(100),LV_SIZE_CONTENT);
+
+	tm->btn_ok=lv_btn_create(tm->btns);
+	lv_obj_add_event_cb(tm->btn_ok,ok_click,LV_EVENT_CLICKED,tm);
+	lv_obj_set_grid_cell(tm->btn_ok,LV_GRID_ALIGN_STRETCH,0,1,LV_GRID_ALIGN_STRETCH,0,1);
+	lv_obj_t*lbl_ok=lv_label_create(tm->btn_ok);
+	lv_label_set_text(lbl_ok,_("OK"));
+	lv_obj_center(lbl_ok);
+
+	tm->btn_restart=lv_btn_create(tm->btns);
+	lv_obj_add_event_cb(tm->btn_restart,restart_click,LV_EVENT_CLICKED,tm);
+	lv_obj_set_grid_cell(tm->btn_restart,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_STRETCH,0,1);
+	lv_obj_t*lbl_restart=lv_label_create(tm->btn_restart);
+	lv_label_set_text(lbl_restart,_("Restart"));
+	lv_obj_center(lbl_restart);
 	return 0;
 }
 
@@ -384,7 +213,6 @@ struct gui_register guireg_theme_menu={
 	.show_app=true,
 	.init=theme_menu_init,
 	.draw=theme_menu_draw,
-	.resize=theme_menu_resize,
 	.lost_focus=theme_lost_focus,
 	.get_focus=theme_get_focus,
 	.quiet_exit=theme_menu_clean,

9dc029e42980b6f19dce790638cd45c1127d452b
BigfootACA <bigfoot@classfun.cn>
2022-07-18 17:52:45 +0800

    gui: interface: gadget: upgrade add_mass.c

--- src/gui/interface/gadget/add_mass.c
+++ src/gui/interface/gadget/add_mass.c
@@ -16,7 +16,6 @@
 #include"gui.h"
 #include"confd.h"
 #include"gui/tools.h"
-#include"gui/sysbar.h"
 #include"gui/msgbox.h"
 #include"gui/activity.h"
 #include"gui/filepicker.h"
@@ -65,8 +64,8 @@ static void set_disk_file(struct gadget_add_mass*am,const char*path){
 	if(!ext)return;
 	ext++;
 	bool is_cdrom=strcasecmp(ext,"iso")==0;
-	lv_checkbox_set_checked(am->ro,is_cdrom);
-	lv_checkbox_set_checked(am->cdrom,is_cdrom);
+	lv_obj_set_checked(am->ro,is_cdrom);
+	lv_obj_set_checked(am->cdrom,is_cdrom);
 }
 
 static bool select_cb(
@@ -82,10 +81,8 @@ static bool select_cb(
 	return false;
 }
 
-static void sel_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct gadget_add_mass*am=lv_obj_get_user_data(obj);
-	if(!am||obj!=am->btn_sel)return;
+static void sel_cb(lv_event_t*e){
+	struct gadget_add_mass*am=e->user_data;
 	struct filepicker*fp=filepicker_create(
 		select_cb,
 		"Open disk file"
@@ -104,10 +101,8 @@ static bool restart_cb(
 	return false;
 }
 
-static void ok_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct gadget_add_mass*am=lv_obj_get_user_data(obj);
-	if(!am||obj!=am->ok)return;
+static void ok_cb(lv_event_t*e){
+	struct gadget_add_mass*am=e->user_data;
 	static char*base="gadget.func";
 	struct stat st;
 	int cnt=confd_count(base);
@@ -143,15 +138,15 @@ static void ok_cb(lv_obj_t*obj,lv_event_t e){
 	);
 	confd_set_boolean_array(
 		base,cnt,"ro",
-		lv_checkbox_is_checked(am->ro)
+		lv_obj_is_checked(am->ro)
 	);
 	confd_set_boolean_array(
 		base,cnt,"cdrom",
-		lv_checkbox_is_checked(am->cdrom)
+		lv_obj_is_checked(am->cdrom)
 	);
 	confd_set_boolean_array(
 		base,cnt,"removable",
-		lv_checkbox_is_checked(am->removable)
+		lv_obj_is_checked(am->removable)
 	);
 	mass_id++;
 	guiact_do_back();
@@ -162,10 +157,7 @@ static void ok_cb(lv_obj_t*obj,lv_event_t e){
 	);
 }
 
-static void cancel_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct gadget_add_mass*am=lv_obj_get_user_data(obj);
-	if(!am||obj!=am->cancel)return;
+static void cancel_cb(lv_event_t*e __attribute__((unused))){
 	guiact_do_back();
 }
 
@@ -188,190 +180,175 @@ static int add_mass_cleanup(struct gui_activity*act){
 	return 0;
 }
 
-static void chk_cb(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
-	struct gadget_add_mass*am=lv_obj_get_user_data(obj);
+static void chk_cb(lv_event_t*e){
+	struct gadget_add_mass*am=e->user_data;
 	if(am)am->check_changed=true;
 }
 
-static int add_mass_resize(struct gui_activity*act){
-	lv_coord_t w=0,h=0;
-	struct gadget_add_mass*am=act->data;
-	if(!am)return 0;
-	lv_obj_set_style_local_pad_all(
-		am->box,
-		LV_PAGE_PART_BG,
-		LV_STATE_DEFAULT,
-		gui_font_size
-	);
-	lv_obj_set_width(am->box,act->w/8*7);
-	w=lv_page_get_scrl_width(am->box);
-	lv_obj_set_width(am->title,w);
-	lv_obj_set_y(am->title,h);
-	h+=lv_obj_get_height(am->title)+gui_font_size;
-	lv_obj_set_y(am->lbl_path,h);
-	lv_obj_align_x(
-		am->txt_path,am->lbl_path,
-		LV_ALIGN_OUT_RIGHT_MID,
-		gui_font_size/2
-	);
-	lv_obj_set_y(am->txt_path,h);
-	lv_obj_align(
-		am->lbl_path,am->txt_path,
-		LV_ALIGN_OUT_LEFT_MID,
-		-gui_font_size/2,0
-	);
-	lv_obj_set_style_local_radius(
-		am->btn_sel,
-		LV_BTN_PART_MAIN,
-		LV_STATE_DEFAULT,
-		gui_font_size/2
-	);
-	lv_obj_set_size(
-		am->btn_sel,
-		gui_font_size*3,
-		lv_obj_get_height(am->txt_path)
-	);
-	lv_obj_set_width(
-		am->txt_path,
-		w-lv_obj_get_width(am->btn_sel)-
-		lv_obj_get_width(am->lbl_path)-
-		gui_font_size
-	);
-	lv_obj_align(
-		am->btn_sel,am->txt_path,
-		LV_ALIGN_OUT_RIGHT_MID,
-		gui_font_size/4,0
-	);
-	h+=lv_obj_get_height(am->btn_sel)+gui_font_size;
-	lv_obj_set_y(am->lbl_id,h);
-	lv_obj_set_width(
-		am->txt_id,
-		w-lv_obj_get_width(am->lbl_id)-
-		gui_font_size
-	);
-	lv_obj_align_x(
-		am->txt_id,am->lbl_id,
-		LV_ALIGN_OUT_RIGHT_MID,
-		gui_font_size/2
-	);
-	lv_obj_set_y(am->txt_id,h);
-	lv_obj_align(
-		am->lbl_id,am->txt_id,
-		LV_ALIGN_OUT_LEFT_MID,
-		-gui_font_size/2,0
-	);
-	h+=lv_obj_get_height(am->txt_id)+gui_font_size;
-	lv_obj_set_y(am->cdrom,h);
-	h+=lv_obj_get_height(am->cdrom)+gui_font_size;
-	lv_obj_set_y(am->removable,h);
-	h+=lv_obj_get_height(am->removable)+gui_font_size;
-	lv_obj_set_y(am->ro,h);
-	h+=lv_obj_get_height(am->ro)+gui_font_size;
-	lv_obj_set_size(
-		am->ok,
-		w/2-gui_font_size,
-		gui_font_size*2
-	);
-	lv_obj_align(
-		am->ok,NULL,
-		LV_ALIGN_IN_TOP_LEFT,
-		(gui_font_size/2),h
-	);
-	lv_obj_set_size(
-		am->cancel,
-		w/2-gui_font_size,
-		gui_font_size*2
-	);
-	lv_obj_align(
-		am->cancel,NULL,
-		LV_ALIGN_IN_TOP_RIGHT,
-		-(gui_font_size/2),h
-	);
-	h+=lv_obj_get_height(am->cancel)+(gui_font_size*3);
-	lv_obj_set_height(am->box,MIN(h,(lv_coord_t)act->h/6*5));
-	lv_obj_align(am->box,NULL,LV_ALIGN_CENTER,0,0);
-	return 0;
-}
-
 static int draw_add_mass(struct gui_activity*act){
+	static lv_coord_t grid_col[]={
+		LV_GRID_CONTENT,
+		LV_GRID_FR(1),
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	},grid_row[]={
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	};
 	struct gadget_add_mass*am=act->data;
 
-	am->box=lv_page_create(act->page,NULL);
-	lv_obj_set_click(am->box,false);
+	am->box=lv_obj_create(act->page);
+	lv_obj_set_style_max_width(am->box,lv_pct(80),0);
+	lv_obj_set_style_max_height(am->box,lv_pct(80),0);
+	lv_obj_set_style_min_width(am->box,gui_dpi*2,0);
+	lv_obj_set_style_min_height(am->box,gui_dpi,0);
+	lv_obj_set_height(am->box,LV_SIZE_CONTENT);
+	lv_obj_set_flex_flow(am->box,LV_FLEX_FLOW_COLUMN);
+	lv_obj_center(am->box);
 
 	// Title
-	am->title=lv_label_create(am->box,NULL);
+	am->title=lv_label_create(am->box);
 	lv_label_set_text(am->title,_("Add Mass Storage into USB Gadget"));
-	lv_label_set_long_mode(am->title,LV_LABEL_LONG_BREAK);
-	lv_label_set_align(am->title,LV_LABEL_ALIGN_CENTER);
+	lv_label_set_long_mode(am->title,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(am->title,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_width(am->title,lv_pct(100));
+
+	lv_obj_t*fields=lv_obj_create(am->box);
+	lv_obj_set_style_radius(fields,0,0);
+	lv_obj_set_scroll_dir(fields,LV_DIR_NONE);
+	lv_obj_set_style_border_width(fields,0,0);
+	lv_obj_set_style_bg_opa(fields,LV_OPA_0,0);
+	lv_obj_set_style_pad_all(fields,gui_dpi/50,0);
+	lv_obj_set_grid_dsc_array(fields,grid_col,grid_row);
+	lv_obj_clear_flag(fields,LV_OBJ_FLAG_SCROLLABLE|LV_OBJ_FLAG_CLICKABLE);
+	lv_obj_set_style_pad_row(fields,gui_font_size/2,0);
+	lv_obj_set_style_pad_column(fields,gui_font_size/2,0);
+	lv_obj_set_size(fields,lv_pct(100),LV_SIZE_CONTENT);
+	lv_obj_center(fields);
 
 	// File select
-	am->lbl_path=lv_label_create(am->box,NULL);
+	am->lbl_path=lv_label_create(fields);
 	lv_label_set_text(am->lbl_path,_("Path:"));
+	lv_obj_set_grid_cell(
+		am->lbl_path,
+		LV_GRID_ALIGN_START,0,1,
+		LV_GRID_ALIGN_CENTER,0,1
+	);
 
-	am->txt_path=lv_textarea_create(am->box,NULL);
+	am->txt_path=lv_textarea_create(fields);
 	lv_textarea_set_text(am->txt_path,"");
 	lv_textarea_set_one_line(am->txt_path,true);
-	lv_textarea_set_cursor_hidden(am->txt_path,true);
 	lv_obj_set_user_data(am->txt_path,am);
-	lv_obj_set_event_cb(am->txt_path,lv_input_cb);
+	lv_obj_add_event_cb(am->txt_path,lv_input_cb,LV_EVENT_CLICKED,NULL);
+	lv_obj_set_grid_cell(
+		am->txt_path,
+		LV_GRID_ALIGN_STRETCH,1,1,
+		LV_GRID_ALIGN_STRETCH,0,1
+	);
 
-	am->btn_sel=lv_btn_create(am->box,NULL);
-	lv_style_set_action_button(am->btn_sel,true);
-	lv_obj_set_user_data(am->btn_sel,am);
-	lv_obj_set_event_cb(am->btn_sel,sel_cb);
-	lv_label_set_text(lv_label_create(am->btn_sel,NULL),"...");
+	am->btn_sel=lv_btn_create(fields);
+	lv_obj_set_enabled(am->btn_sel,true);
+	lv_obj_add_event_cb(am->btn_sel,sel_cb,LV_EVENT_CLICKED,am);
+	lv_obj_t*lbl_sel=lv_label_create(am->btn_sel);
+	lv_label_set_text(lbl_sel,"...");
+	lv_obj_center(lbl_sel);
+	lv_obj_set_grid_cell(
+		am->btn_sel,
+		LV_GRID_ALIGN_STRETCH,2,1,
+		LV_GRID_ALIGN_STRETCH,0,1
+	);
 
 	// Function Name
-	am->lbl_id=lv_label_create(am->box,NULL);
+	am->lbl_id=lv_label_create(fields);
 	lv_label_set_text(am->lbl_id,_("Name:"));
+	lv_obj_set_grid_cell(
+		am->lbl_id,
+		LV_GRID_ALIGN_START,0,1,
+		LV_GRID_ALIGN_CENTER,1,1
+	);
 
 	char buf[32];
 	snprintf(buf,31,"%d",mass_id);
-	am->txt_id=lv_textarea_create(am->box,NULL);
+	am->txt_id=lv_textarea_create(fields);
 	lv_textarea_set_text(am->txt_id,buf);
 	lv_textarea_set_one_line(am->txt_id,true);
-	lv_textarea_set_cursor_hidden(am->txt_id,true);
 	lv_textarea_set_accepted_chars(am->txt_id,VALID);
-	lv_obj_set_user_data(am->txt_id,am);
-	lv_obj_set_event_cb(am->txt_id,lv_input_cb);
+	lv_obj_add_event_cb(am->txt_id,lv_input_cb,LV_EVENT_CLICKED,am);
+	lv_obj_set_grid_cell(
+		am->txt_id,
+		LV_GRID_ALIGN_STRETCH,1,2,
+		LV_GRID_ALIGN_STRETCH,1,1
+	);
 
 	// Is CDROM
-	am->cdrom=lv_checkbox_create(am->box,NULL);
-	lv_style_set_focus_checkbox(am->cdrom);
+	am->cdrom=lv_checkbox_create(fields);
 	lv_checkbox_set_text(am->cdrom,_("CDROM"));
-	lv_obj_set_user_data(am->cdrom,am);
-	lv_obj_set_event_cb(am->cdrom,chk_cb);
+	lv_obj_add_event_cb(am->cdrom,chk_cb,LV_EVENT_VALUE_CHANGED,am);
+	lv_obj_set_grid_cell(
+		am->cdrom,
+		LV_GRID_ALIGN_START,0,3,
+		LV_GRID_ALIGN_CENTER,2,1
+	);
 
 	// Is Removable
-	am->removable=lv_checkbox_create(am->box,NULL);
-	lv_style_set_focus_checkbox(am->removable);
+	am->removable=lv_checkbox_create(fields);
 	lv_checkbox_set_text(am->removable,_("Removable"));
-	lv_checkbox_set_checked(am->removable,true);
+	lv_obj_set_checked(am->removable,true);
 	lv_obj_set_user_data(am->removable,am);
-	lv_obj_set_event_cb(am->removable,chk_cb);
+	lv_obj_add_event_cb(am->removable,chk_cb,LV_EVENT_VALUE_CHANGED,am);
+	lv_obj_set_grid_cell(
+		am->removable,
+		LV_GRID_ALIGN_START,0,3,
+		LV_GRID_ALIGN_CENTER,3,1
+	);
 
 	// Is Read-Only
-	am->ro=lv_checkbox_create(am->box,NULL);
-	lv_style_set_focus_checkbox(am->ro);
+	am->ro=lv_checkbox_create(fields);
 	lv_checkbox_set_text(am->ro,_("Read Only"));
 	lv_obj_set_user_data(am->ro,am);
-	lv_obj_set_event_cb(am->ro,chk_cb);
+	lv_obj_add_event_cb(am->ro,chk_cb,LV_EVENT_VALUE_CHANGED,am);
+	lv_obj_set_grid_cell(
+		am->ro,
+		LV_GRID_ALIGN_START,0,3,
+		LV_GRID_ALIGN_CENTER,4,1
+	);
+
+	lv_obj_t*btns=lv_obj_create(am->box);
+	lv_obj_set_style_radius(btns,0,0);
+	lv_obj_set_scroll_dir(btns,LV_DIR_NONE);
+	lv_obj_set_style_border_width(btns,0,0);
+	lv_obj_set_style_bg_opa(btns,LV_OPA_0,0);
+	lv_obj_set_style_pad_all(btns,gui_dpi/50,0);
+	lv_obj_set_flex_flow(btns,LV_FLEX_FLOW_ROW);
+	lv_obj_clear_flag(btns,LV_OBJ_FLAG_SCROLLABLE|LV_OBJ_FLAG_CLICKABLE);
+	lv_obj_set_style_pad_row(btns,gui_font_size/2,0);
+	lv_obj_set_style_pad_column(btns,gui_font_size/2,0);
+	lv_obj_set_size(btns,lv_pct(100),LV_SIZE_CONTENT);
+	lv_obj_center(btns);
 
 	// OK Button
-	am->ok=lv_btn_create(am->box,NULL);
-	lv_style_set_action_button(am->ok,true);
+	am->ok=lv_btn_create(btns);
+	lv_obj_set_enabled(am->ok,true);
 	lv_obj_set_user_data(am->ok,am);
-	lv_obj_set_event_cb(am->ok,ok_cb);
-	lv_label_set_text(lv_label_create(am->ok,NULL),_("OK"));
+	lv_obj_add_event_cb(am->ok,ok_cb,LV_EVENT_CLICKED,am);
+	lv_obj_set_flex_grow(am->ok,1);
+	lv_obj_t*lbl_ok=lv_label_create(am->ok);
+	lv_label_set_text(lbl_ok,_("OK"));
+	lv_obj_center(lbl_ok);
 
 	// Cancel Button
-	am->cancel=lv_btn_create(am->box,NULL);
-	lv_style_set_action_button(am->cancel,true);
+	am->cancel=lv_btn_create(btns);
+	lv_obj_set_enabled(am->cancel,true);
 	lv_obj_set_user_data(am->cancel,am);
-	lv_obj_set_event_cb(am->cancel,cancel_cb);
-	lv_label_set_text(lv_label_create(am->cancel,NULL),_("Cancel"));
+	lv_obj_add_event_cb(am->cancel,cancel_cb,LV_EVENT_CLICKED,am);
+	lv_obj_set_flex_grow(am->cancel,1);
+	lv_obj_t*lbl_cancel=lv_label_create(am->cancel);
+	lv_label_set_text(lbl_cancel,_("Cancel"));
+	lv_obj_center(lbl_cancel);
 
 	if(act->args)set_disk_file(am,act->args);
 	return 0;
@@ -392,7 +369,6 @@ struct gui_register guireg_add_mass={
 		NULL
 	},
 	.init=add_mass_init,
-	.resize=add_mass_resize,
 	.quiet_exit=add_mass_cleanup,
 	.get_focus=add_mass_get_focus,
 	.lost_focus=add_mass_lost_focus,

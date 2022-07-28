51c51497ccb5f6ba8a2bab999f132467697f72fd
BigfootACA <bigfoot@classfun.cn>
2022-07-28 17:11:36 +0800

    gui: interface: settings: upgrade mouse.c

--- src/gui/interface/settings/mouse.c
+++ src/gui/interface/settings/mouse.c
@@ -20,19 +20,17 @@
 extern int gui_mouse_scale;
 static lv_obj_t*box,*value,*slider,*arr_left,*arr_right;
 
-static void mouse_scale_change(lv_obj_t*obj,lv_event_t e){
-	if(obj!=slider||e!=LV_EVENT_VALUE_CHANGED)return;
+static void mouse_scale_change(lv_event_t*e){
 	gui_mouse_scale=lv_slider_get_value(slider);
 	if(gui_mouse_scale<=0)gui_mouse_scale=1;
 	lv_label_set_text_fmt(value,"%d",gui_mouse_scale);
 	confd_set_integer("gui.mouse_scale",gui_mouse_scale);
 }
 
-static void mouse_scale_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED)return;
+static void mouse_scale_click(lv_event_t*e){
 	gui_mouse_scale=lv_slider_get_value(slider);
-	if(obj==arr_left)gui_mouse_scale--;
-	else if(obj==arr_right)gui_mouse_scale++;
+	if(e->target==arr_left)gui_mouse_scale--;
+	else if(e->target==arr_right)gui_mouse_scale++;
 	else return;
 	if(gui_mouse_scale<=0)gui_mouse_scale=1;
 	if(gui_mouse_scale>=64)gui_mouse_scale=64;
@@ -42,56 +40,66 @@ static void mouse_scale_click(lv_obj_t*obj,lv_event_t e){
 }
 
 static int mouse_menu_draw(struct gui_activity*act){
-	int bts=gui_font_size+(gui_dpi/8);
+	static lv_coord_t grid_col[]={
+		LV_GRID_CONTENT,
+		LV_GRID_FR(1),
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	},grid_row[]={
+		LV_GRID_FR(1),
+		LV_GRID_TEMPLATE_LAST
+	};
 
-	static lv_style_t bs;
-	lv_style_init(&bs);
-	lv_style_set_pad_all(&bs,LV_STATE_DEFAULT,gui_font_size);
+	box=lv_obj_create(act->page);
+	lv_obj_set_flex_flow(box,LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_style_pad_all(box,gui_font_size/2,0);
+	lv_obj_set_style_pad_row(box,gui_font_size/2,0);
+	lv_obj_set_style_max_width(box,lv_pct(80),0);
+	lv_obj_set_style_max_height(box,lv_pct(80),0);
+	lv_obj_set_style_min_width(box,gui_dpi*2,0);
+	lv_obj_set_style_min_height(box,gui_font_size*2,0);
+	lv_obj_set_height(box,LV_SIZE_CONTENT);
+	lv_obj_center(box);
 
-	box=lv_obj_create(act->page,NULL);
-	lv_obj_add_style(box,LV_PAGE_PART_BG,&bs);
-	lv_obj_set_click(box,false);
-	lv_obj_set_width(box,gui_sw/6*5);
-
-	lv_obj_t*txt=lv_label_create(box,NULL);
-	lv_label_set_long_mode(txt,LV_LABEL_LONG_BREAK);
-	lv_obj_set_width(txt,lv_obj_get_width(box));
-	lv_label_set_align(txt,LV_LABEL_ALIGN_CENTER);
-	lv_obj_align(txt,NULL,LV_ALIGN_IN_TOP_MID,0,gui_font_size);
+	lv_obj_t*txt=lv_label_create(box);
 	lv_label_set_text(txt,_("Change mouse scale level"));
+	lv_label_set_long_mode(txt,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(txt,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_width(txt,lv_pct(100));
 
-	value=lv_label_create(box,NULL);
+	value=lv_label_create(box);
 	lv_label_set_text_fmt(value,"%d",gui_mouse_scale);
-	lv_obj_align(value,txt,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
+	lv_obj_set_style_text_align(value,LV_TEXT_ALIGN_CENTER,0);
+	lv_obj_set_width(value,lv_pct(100));
 
-	slider=lv_slider_create(box,NULL);
+	lv_obj_t*fields=lv_obj_create(box);
+	lv_obj_set_style_radius(fields,0,0);
+	lv_obj_set_style_border_width(fields,0,0);
+	lv_obj_set_style_bg_opa(fields,LV_OPA_0,0);
+	lv_obj_set_style_pad_column(fields,gui_font_size,0);
+	lv_obj_set_size(fields,lv_pct(100),LV_SIZE_CONTENT);
+	lv_obj_set_grid_dsc_array(fields,grid_col,grid_row);
+
+	arr_left=lv_btn_create(fields);
+	lv_obj_add_event_cb(arr_left,mouse_scale_click,LV_EVENT_CLICKED,NULL);
+	lv_obj_set_grid_cell(arr_left,LV_GRID_ALIGN_START,0,1,LV_GRID_ALIGN_STRETCH,0,1);
+	lv_obj_t*lbl_arr_left=lv_label_create(arr_left);
+	lv_label_set_text(lbl_arr_left,LV_SYMBOL_LEFT);
+	lv_obj_center(lbl_arr_left);
+
+	slider=lv_slider_create(fields);
 	lv_slider_set_range(slider,1,64);
 	lv_slider_set_value(slider,gui_mouse_scale,LV_ANIM_OFF);
-	lv_obj_set_event_cb(slider,mouse_scale_change);
-	lv_obj_set_width(slider,lv_obj_get_width(box)-gui_font_size*4);
-	lv_obj_align(slider,value,LV_ALIGN_OUT_BOTTOM_MID,0,gui_font_size);
+	lv_obj_add_event_cb(slider,mouse_scale_change,LV_EVENT_VALUE_CHANGED,NULL);
+	lv_obj_set_grid_cell(slider,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_CENTER,0,1);
 
-	arr_left=lv_btn_create(box,NULL);
-	lv_obj_set_size(arr_left,bts,bts);
-	lv_obj_set_event_cb(arr_left,mouse_scale_click);
-	lv_obj_align(arr_left,slider,LV_ALIGN_OUT_BOTTOM_LEFT,0,gui_font_size);
-	lv_style_set_action_button(arr_left,true);
-	lv_label_set_text(lv_label_create(arr_left,NULL),LV_SYMBOL_LEFT);
+	arr_right=lv_btn_create(fields);
+	lv_obj_add_event_cb(arr_right,mouse_scale_click,LV_EVENT_CLICKED,NULL);
+	lv_obj_set_grid_cell(arr_right,LV_GRID_ALIGN_END,2,1,LV_GRID_ALIGN_STRETCH,0,1);
+	lv_obj_t*lbl_arr_right=lv_label_create(arr_right);
+	lv_label_set_text(lbl_arr_right,LV_SYMBOL_RIGHT);
+	lv_obj_center(lbl_arr_right);
 
-	arr_right=lv_btn_create(box,NULL);
-	lv_obj_set_size(arr_right,bts,bts);
-	lv_obj_set_event_cb(arr_right,mouse_scale_click);
-	lv_obj_align(arr_right,slider,LV_ALIGN_OUT_BOTTOM_RIGHT,0,gui_font_size);
-	lv_style_set_action_button(arr_right,true);
-	lv_label_set_text(lv_label_create(arr_right,NULL),LV_SYMBOL_RIGHT);
-
-	lv_obj_set_height(box,
-		lv_obj_get_y(slider)+
-		lv_obj_get_height(slider)+
-		(gui_font_size*2)+
-		bts
-	);
-	lv_obj_align(box,NULL,LV_ALIGN_CENTER,0,0);
 	return 0;
 }
 

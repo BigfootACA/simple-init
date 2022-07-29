f8ef5305fe2fafa0dae4f055cdf8eecce46dd8dc
BigfootACA <bigfoot@classfun.cn>
2022-07-29 18:12:05 +0800

    gui: interface: apps: upgrade uefi_bootmenu.c

--- src/gui/interface/apps/uefi_bootmenu.c
+++ src/gui/interface/apps/uefi_bootmenu.c
@@ -18,30 +18,26 @@
 #include"gui/tools.h"
 #include"gui/activity.h"
 #define TAG "bootmenu"
-static lv_obj_t*lst=NULL,*last=NULL;
+static lv_obj_t*lst=NULL;
 static lv_obj_t*options_info,*btn_ok,*btn_refresh;
 static EFI_BOOT_MANAGER_LOAD_OPTION*cur=NULL;
 static struct option_info{
 	bool enable;
-	lv_obj_t*btn,*chk;
+	lv_obj_t*btn,*lbl;
 	char name[512];
 	EFI_BOOT_MANAGER_LOAD_OPTION*option;
 }options[256]={0};
 static struct option_info*selected=NULL;
 
-static void option_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_VALUE_CHANGED)return;
-	lv_checkbox_set_checked(obj,true);
+static void option_click(lv_event_t*e){
+	lv_obj_set_checked(e->target,true);
 	if(selected){
-		if(obj==selected->chk)return;
-		else{
-			lv_checkbox_set_checked(selected->chk,false);
-			lv_obj_set_checked(selected->btn,false);
-		}
+		if(e->target==selected->btn)return;
+		else lv_obj_set_checked(selected->btn,false);
 	}
 	selected=NULL;
 	for(int i=0;i<256&&!selected;i++)
-		if(options[i].enable&&options[i].chk==obj)
+		if(options[i].enable&&options[i].btn==e->target)
 			selected=&options[i];
 	if(!selected)return;
 	lv_obj_set_checked(selected->btn,true);
@@ -54,7 +50,7 @@ static void bootmenu_option_clear(){
 	selected=NULL;
 	lv_obj_set_enabled(btn_ok,false);
 	if(options_info)lv_obj_del(options_info);
-	options_info=NULL,last=NULL;
+	options_info=NULL;
 	for(int i=0;i<256;i++){
 		if(!options[i].enable)continue;
 		lv_obj_del(options[i].btn);
@@ -64,41 +60,30 @@ static void bootmenu_option_clear(){
 
 static void bootmenu_set_options_info(char*text){
 	bootmenu_option_clear();
-	options_info=lv_label_create(lst,NULL);
-	lv_label_set_long_mode(options_info,LV_LABEL_LONG_BREAK);
-	lv_obj_set_size(options_info,lv_page_get_scrl_width(lst),gui_sh/16);
-	lv_label_set_align(options_info,LV_LABEL_ALIGN_CENTER);
+	options_info=lv_label_create(lst);
+	lv_label_set_long_mode(options_info,LV_LABEL_LONG_WRAP);
+	lv_obj_set_width(options_info,lv_pct(100));
+	lv_obj_set_style_text_align(options_info,LV_TEXT_ALIGN_CENTER,0);
 	lv_label_set_text(options_info,text);
 }
 
 static void options_add_item(struct option_info*k){
-	lv_coord_t bw;
-	bw=lv_page_get_scrl_width(lst);
-
-	// option select button
-	k->btn=lv_btn_create(lst,NULL);
-	lv_obj_set_size(k->btn,bw,gui_dpi/3);
-	lv_obj_align(
-		k->btn,last,
-		last?LV_ALIGN_OUT_BOTTOM_MID:LV_ALIGN_IN_TOP_MID,
-		0,last?gui_dpi/8:0
-	);
+	// button
+	k->btn=lv_btn_create(lst);
+	lv_obj_set_size(k->btn,lv_pct(100),LV_SIZE_CONTENT);
+	lv_obj_set_style_min_height(k->btn,gui_font_size,0);
 	lv_style_set_btn_item(k->btn);
-	lv_obj_set_click(k->btn,false);
-	last=k->btn;
+	lv_obj_add_event_cb(k->btn,option_click,LV_EVENT_CLICKED,k);
+	lv_group_add_obj(gui_grp,k->btn);
+	lv_obj_update_layout(k->btn);
 
-	// option name and checkbox
-	k->chk=lv_checkbox_create(k->btn,NULL);
-	lv_checkbox_ext_t*e=lv_obj_get_ext_attr(k->chk);
-	lv_label_set_long_mode(e->label,confd_get_boolean("gui.text_scroll",true)?
-		LV_LABEL_LONG_SROLL_CIRC:
+	// option name
+	k->lbl=lv_label_create(k->btn);
+	lv_label_set_long_mode(k->lbl,confd_get_boolean("gui.text_scroll",true)?
+		LV_LABEL_LONG_SCROLL_CIRCULAR:
 		LV_LABEL_LONG_DOT
 	);
-	lv_obj_set_width(e->label,bw-gui_dpi/5*2);
-	lv_checkbox_set_text(k->chk,k->name);
-	lv_obj_set_event_cb(k->chk,option_click);
-	lv_style_set_focus_checkbox(k->chk);
-	lv_group_add_obj(gui_grp,k->chk);
+	lv_label_set_text(k->lbl,k->name);
 }
 
 static void bootmenu_option_reload(){
@@ -125,8 +110,8 @@ static void bootmenu_option_reload(){
 	tlog_info("found %llu options",(unsigned long long)i);
 }
 
-static void refresh_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED||obj!=btn_refresh)return;
+static void refresh_click(lv_event_t*e){
+	if(e->target!=btn_refresh)return;
 	tlog_debug("request refresh");
 	bootmenu_option_reload();
 }
@@ -137,8 +122,8 @@ static int after_exit(void*d __attribute__((unused))){
 	return 0;
 }
 
-static void ok_click(lv_obj_t*obj,lv_event_t e){
-	if(e!=LV_EVENT_CLICKED||obj!=btn_ok||!selected)return;
+static void ok_click(lv_event_t*e){
+	if(e->target!=btn_ok||!selected)return;
 	tlog_notice("shutdown guiapp and start option %s",selected->name);
 	cur=selected->option;
 	gui_run_and_exit(after_exit);
@@ -163,7 +148,7 @@ static int bootmenu_option_get_focus(struct gui_activity*d __attribute__((unused
 static int bootmenu_option_lost_focus(struct gui_activity*d __attribute__((unused))){
 	for(int i=0;i<256;i++){
 		if(!options[i].enable)continue;
-		lv_group_remove_obj(options[i].chk);
+		lv_group_remove_obj(options[i].btn);
 	}
 	lv_group_remove_obj(btn_ok);
 	lv_group_remove_obj(btn_refresh);
@@ -171,51 +156,63 @@ static int bootmenu_option_lost_focus(struct gui_activity*d __attribute__((unuse
 }
 
 static int uefi_bootmenu_draw(struct gui_activity*act){
-
-	lv_coord_t btm=gui_dpi/10;
-	lv_coord_t btw=gui_sw/2-btm*2;
-	lv_coord_t bth=gui_font_size+gui_dpi/10;
+	static lv_coord_t grid_col[]={
+		LV_GRID_FR(1),
+		LV_GRID_FR(1),
+		LV_GRID_TEMPLATE_LAST
+	},grid_row[]={
+		LV_GRID_CONTENT,
+		LV_GRID_FR(1),
+		LV_GRID_CONTENT,
+		LV_GRID_TEMPLATE_LAST
+	};
+	lv_obj_set_style_pad_all(act->page,gui_font_size/2,0);
+	lv_obj_set_grid_dsc_array(act->page,grid_col,grid_row);
 
 	// function title
-	lv_obj_t*title=lv_label_create(act->page,NULL);
-	lv_label_set_long_mode(title,LV_LABEL_LONG_BREAK);
-	lv_obj_set_y(title,gui_sh/16);
-	lv_obj_set_size(title,gui_sw,gui_sh/16);
-	lv_label_set_align(title,LV_LABEL_ALIGN_CENTER);
+	lv_obj_t*title=lv_label_create(act->page);
+	lv_label_set_long_mode(title,LV_LABEL_LONG_WRAP);
+	lv_obj_set_style_text_align(title,LV_TEXT_ALIGN_CENTER,0);
 	lv_label_set_text(title,_("UEFI Boot Menu"));
+	lv_obj_set_grid_cell(
+		title,
+		LV_GRID_ALIGN_CENTER,0,2,
+		LV_GRID_ALIGN_CENTER,0,1
+	);
 
 	// options list
-	static lv_style_t lst_style;
-	lv_style_init(&lst_style);
-	lv_style_set_border_width(&lst_style,LV_STATE_DEFAULT,0);
-	lv_style_set_border_width(&lst_style,LV_STATE_FOCUSED,0);
-	lv_style_set_border_width(&lst_style,LV_STATE_PRESSED,0);
-	lst=lv_page_create(act->page,NULL);
-	lv_obj_add_style(lst,LV_PAGE_PART_BG,&lst_style);
-	lv_obj_set_pos(lst,gui_dpi/20,gui_sh/8);
-	lv_obj_set_size(lst,gui_sw-gui_dpi/10,gui_sh-lv_obj_get_y(lst)-bth-btm*2);
-
-	// button style
-	static lv_style_t btn_style;
-	lv_style_init(&btn_style);
-	lv_style_set_radius(&btn_style,LV_STATE_DEFAULT,2);
-	lv_style_set_outline_width(&btn_style,LV_STATE_PRESSED,0);
+	lst=lv_obj_create(act->page);
+	lv_obj_set_flex_flow(lst,LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_width(lst,lv_pct(100));
+	lv_obj_set_grid_cell(
+		lst,
+		LV_GRID_ALIGN_STRETCH,0,2,
+		LV_GRID_ALIGN_STRETCH,1,1
+	);
 
 	// ok button
-	btn_ok=lv_btn_create(act->page,NULL);
-	lv_obj_set_size(btn_ok,btw,bth);
-	lv_obj_align(btn_ok,NULL,LV_ALIGN_IN_BOTTOM_LEFT,btm,-btm);
-	lv_obj_set_event_cb(btn_ok,ok_click);
-	lv_style_set_action_button(btn_ok,false);
-	lv_label_set_text(lv_label_create(btn_ok,NULL),_("OK"));
+	btn_ok=lv_btn_create(act->page);
+	lv_obj_add_event_cb(btn_ok,ok_click,LV_EVENT_CLICKED,NULL);
+	lv_obj_t*lbl_ok=lv_label_create(btn_ok);
+	lv_label_set_text(lbl_ok,_("OK"));
+	lv_obj_center(lbl_ok);
+	lv_obj_set_grid_cell(
+		btn_ok,
+		LV_GRID_ALIGN_STRETCH,0,1,
+		LV_GRID_ALIGN_CENTER,2,1
+	);
 
 	// refresh button
-	btn_refresh=lv_btn_create(act->page,NULL);
-	lv_obj_set_size(btn_refresh,btw,bth);
-	lv_obj_align(btn_refresh,NULL,LV_ALIGN_IN_BOTTOM_RIGHT,-btm,-btm);
-	lv_obj_set_event_cb(btn_refresh,refresh_click);
-	lv_style_set_action_button(btn_refresh,true);
-	lv_label_set_text(lv_label_create(btn_refresh,NULL),_("Refresh"));
+	btn_refresh=lv_btn_create(act->page);
+	lv_obj_add_event_cb(btn_refresh,refresh_click,LV_EVENT_CLICKED,NULL);
+	lv_obj_t*lbl_refresh=lv_label_create(btn_refresh);
+	lv_label_set_text(lbl_refresh,_("Refresh"));
+	lv_obj_center(lbl_refresh);
+	lv_obj_set_grid_cell(
+		btn_refresh,
+		LV_GRID_ALIGN_STRETCH,1,1,
+		LV_GRID_ALIGN_CENTER,2,1
+	);
 
 	return 0;
 }

9ca9de8b26ccf8f45a609c8870a44d868b923015
BigfootACA <bigfoot@classfun.cn>
2022-08-08 11:41:00 +0800

    gui: interface: apps: ramdisk.c: simplify gui draw code

--- src/gui/interface/apps/ramdisk.c
+++ src/gui/interface/apps/ramdisk.c
@@ -38,9 +38,7 @@
 struct add_ramdisk{
 	bool size_changed;
 	lv_obj_t*box,*ok,*cancel;
-	lv_obj_t*btn_source;
-	lv_obj_t*lbl_title,*lbl_source;
-	lv_obj_t*lbl_size,*lbl_mem_type,*lbl_type;
+	lv_obj_t*btn_source,*lbl_title;
 	lv_obj_t*source,*size,*mem_type,*type;
 };
 
@@ -213,12 +211,6 @@ static void ok_cb(lv_event_t*e){
 	guiact_do_back();
 }
 
-static void cancel_cb(lv_event_t*e){
-	struct add_ramdisk*am=e->user_data;
-	if(!am||e->target!=am->cancel)return;
-	guiact_do_back();
-}
-
 static int init(struct gui_activity*act){
 	struct add_ramdisk*am=malloc(sizeof(struct add_ramdisk));
 	if(!am)return -ENOMEM;
@@ -248,105 +240,28 @@ static int do_cleanup(struct gui_activity*act){
 }
 
 static int draw_add_ramdisk(struct gui_activity*act){
-	static lv_coord_t grid_2fr[]={
-		LV_GRID_FR(1),
-		LV_GRID_FR(1),
-		LV_GRID_TEMPLATE_LAST
-	},grid_row[]={
-		LV_GRID_CONTENT,
-		LV_GRID_TEMPLATE_LAST
-	},grid_dropdown[]={
-		LV_GRID_CONTENT,
-		LV_GRID_FR(1),
-		LV_GRID_TEMPLATE_LAST
-	};
 	struct add_ramdisk*am=act->data;
-
-	am->box=lv_obj_create(act->page);
-	lv_obj_set_flex_flow(am->box,LV_FLEX_FLOW_COLUMN);
-	lv_obj_set_style_pad_all(am->box,gui_font_size/2,0);
-	lv_obj_set_style_max_width(am->box,lv_pct(80),0);
-	lv_obj_set_style_max_height(am->box,lv_pct(80),0);
-	lv_obj_set_style_min_width(am->box,gui_dpi*2,0);
-	lv_obj_set_style_min_height(am->box,gui_font_size*2,0);
-	lv_obj_set_height(am->box,LV_SIZE_CONTENT);
-	lv_obj_center(am->box);
-
-	// Title
-	am->lbl_title=lv_label_create(am->box);
-	lv_obj_set_width(am->lbl_title,lv_pct(100));
-	lv_label_set_text(am->lbl_title,_("Add Ramdisk"));
-	lv_label_set_long_mode(am->lbl_title,LV_LABEL_LONG_WRAP);
-	lv_obj_set_style_text_align(am->lbl_title,LV_TEXT_ALIGN_CENTER,0);
-
+	am->box=lv_draw_dialog_box(act->page,&am->lbl_title,"Add Ramdisk");
 	lv_draw_input(am->box,"Image:",NULL,NULL,&am->source,&am->btn_source);
 	lv_draw_input(am->box,"Size:",NULL,NULL,&am->size,NULL);
-
 	lv_textarea_set_text(am->source,act->args?(char*)act->args:"");
 	lv_obj_add_event_cb(am->source,source_cb,LV_EVENT_DEFOCUSED,am);
 	lv_obj_add_event_cb(am->size,size_cb,LV_EVENT_DEFOCUSED,am);
 	lv_textarea_set_accepted_chars(am->size,HEX"x");
 	lv_textarea_set_text(am->size,"0");
-
-	lv_obj_t*fields=lv_obj_create(am->box);
-	lv_obj_set_style_border_width(fields,0,0);
-	lv_obj_set_style_pad_all(fields,gui_font_size/4,0);
-	lv_obj_set_grid_dsc_array(fields,grid_dropdown,grid_2fr);
-	lv_obj_set_size(fields,lv_pct(100),LV_SIZE_CONTENT);
-
-	// Memory Type
-	am->lbl_mem_type=lv_label_create(fields);
-	lv_label_set_text(am->lbl_mem_type,_("Memory Type:"));
-	lv_obj_set_grid_cell(am->lbl_mem_type,LV_GRID_ALIGN_START,0,1,LV_GRID_ALIGN_CENTER,0,1);
-
-	am->mem_type=lv_dropdown_create(fields);
-	lv_obj_set_user_data(am->mem_type,am);
-	lv_obj_add_event_cb(am->mem_type,lv_default_dropdown_cb,LV_EVENT_ALL,NULL);
-	lv_obj_set_grid_cell(am->mem_type,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_CENTER,0,1);
+	lv_draw_dropdown(am->box,"Memory Type:",&am->mem_type);
+	lv_draw_dropdown(am->box,"Disk Type:",&am->type);
+	lv_draw_btns_ok_cancel(am->box,&am->ok,&am->cancel,ok_cb,am);
 	lv_dropdown_clear_options(am->mem_type);
 	lv_dropdown_add_option(am->mem_type,_("Boot Services Data"),0);
 	lv_dropdown_add_option(am->mem_type,_("Reserved Memory"),1);
 	lv_dropdown_add_option(am->mem_type,_("Conventional Memory"),2);
-
-	// Disk Type
-	am->lbl_type=lv_label_create(fields);
-	lv_label_set_text(am->lbl_type,_("Disk Type:"));
-	lv_obj_set_grid_cell(am->lbl_type,LV_GRID_ALIGN_START,0,1,LV_GRID_ALIGN_CENTER,1,1);
-
-	am->type=lv_dropdown_create(fields);
-	lv_obj_set_user_data(am->type,am);
-	lv_obj_add_event_cb(am->type,lv_default_dropdown_cb,LV_EVENT_ALL,NULL);
-	lv_obj_set_grid_cell(am->type,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_CENTER,1,1);
 	lv_dropdown_clear_options(am->type);
 	lv_dropdown_add_option(am->type,_("Virtual CD"),0);
 	lv_dropdown_add_option(am->type,_("Virtual Disk"),1);
 	lv_dropdown_add_option(am->type,_("Persistent Virtual CD"),2);
 	lv_dropdown_add_option(am->type,_("Persistent Virtual Disk"),3);
 	lv_dropdown_set_selected(am->type,1);
-
-	lv_obj_t*btns=lv_obj_create(am->box);
-	lv_obj_set_style_border_width(btns,0,0);
-	lv_obj_set_style_pad_all(btns,gui_font_size/4,0);
-	lv_obj_set_grid_dsc_array(btns,grid_2fr,grid_row);
-	lv_obj_set_size(btns,lv_pct(100),LV_SIZE_CONTENT);
-
-	// OK Button
-	am->ok=lv_btn_create(btns);
-	lv_obj_set_enabled(am->ok,true);
-	lv_obj_add_event_cb(am->ok,ok_cb,LV_EVENT_CLICKED,am);
-	lv_obj_set_grid_cell(am->ok,LV_GRID_ALIGN_STRETCH,0,1,LV_GRID_ALIGN_STRETCH,0,1);
-	lv_obj_t*lbl_ok=lv_label_create(am->ok);
-	lv_label_set_text(lbl_ok,_("OK"));
-	lv_obj_center(lbl_ok);
-
-	// Cancel Button
-	am->cancel=lv_btn_create(btns);
-	lv_obj_set_enabled(am->cancel,true);
-	lv_obj_add_event_cb(am->cancel,cancel_cb,LV_EVENT_CLICKED,am);
-	lv_obj_set_grid_cell(am->cancel,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_STRETCH,0,1);
-	lv_obj_t*lbl_cancel=lv_label_create(am->cancel);
-	lv_label_set_text(lbl_cancel,_("Cancel"));
-	lv_obj_center(lbl_cancel);
 
 	if(act->args){
 		load_size(am);

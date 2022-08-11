9f8d6b83927fd8a83b3ecbe11c5ba59d72a0e3a0
BigfootACA <bigfoot@classfun.cn>
2022-08-12 01:25:03 +0800

    gui: interface: settings: mouse.c: simplify gui draw code

--- src/gui/interface/settings/mouse.c
+++ src/gui/interface/settings/mouse.c
@@ -50,42 +50,15 @@ static int mouse_menu_draw(struct gui_activity*act){
 		LV_GRID_TEMPLATE_LAST
 	};
 
-	box=lv_obj_create(act->page);
-	lv_obj_set_flex_flow(box,LV_FLEX_FLOW_COLUMN);
-	lv_obj_set_style_pad_all(box,gui_font_size/2,0);
-	lv_obj_set_style_pad_row(box,gui_font_size/2,0);
-	lv_obj_set_style_max_width(box,lv_pct(80),0);
-	lv_obj_set_style_max_height(box,lv_pct(80),0);
-	lv_obj_set_style_min_width(box,gui_dpi*2,0);
-	lv_obj_set_style_min_height(box,gui_font_size*2,0);
-	lv_obj_set_height(box,LV_SIZE_CONTENT);
-	lv_obj_center(box);
-
-	lv_obj_t*txt=lv_label_create(box);
-	lv_label_set_text(txt,_("Change mouse scale level"));
-	lv_label_set_long_mode(txt,LV_LABEL_LONG_WRAP);
-	lv_obj_set_style_text_align(txt,LV_TEXT_ALIGN_CENTER,0);
-	lv_obj_set_width(txt,lv_pct(100));
+	box=lv_draw_dialog_box(act->page,NULL,"Change mouse scale level");
 
 	value=lv_label_create(box);
 	lv_label_set_text_fmt(value,"%d",gui_mouse_scale);
 	lv_obj_set_style_text_align(value,LV_TEXT_ALIGN_CENTER,0);
 	lv_obj_set_width(value,lv_pct(100));
 
-	lv_obj_t*fields=lv_obj_create(box);
-	lv_obj_set_style_radius(fields,0,0);
-	lv_obj_set_style_border_width(fields,0,0);
-	lv_obj_set_style_bg_opa(fields,LV_OPA_0,0);
+	lv_obj_t*fields=lv_draw_line_wrapper(box,grid_col,grid_row);
 	lv_obj_set_style_pad_column(fields,gui_font_size,0);
-	lv_obj_set_size(fields,lv_pct(100),LV_SIZE_CONTENT);
-	lv_obj_set_grid_dsc_array(fields,grid_col,grid_row);
-
-	arr_left=lv_btn_create(fields);
-	lv_obj_add_event_cb(arr_left,mouse_scale_click,LV_EVENT_CLICKED,NULL);
-	lv_obj_set_grid_cell(arr_left,LV_GRID_ALIGN_START,0,1,LV_GRID_ALIGN_STRETCH,0,1);
-	lv_obj_t*lbl_arr_left=lv_label_create(arr_left);
-	lv_label_set_text(lbl_arr_left,LV_SYMBOL_LEFT);
-	lv_obj_center(lbl_arr_left);
 
 	slider=lv_slider_create(fields);
 	lv_slider_set_range(slider,1,64);
@@ -93,27 +66,31 @@ static int mouse_menu_draw(struct gui_activity*act){
 	lv_obj_add_event_cb(slider,mouse_scale_change,LV_EVENT_VALUE_CHANGED,NULL);
 	lv_obj_set_grid_cell(slider,LV_GRID_ALIGN_STRETCH,1,1,LV_GRID_ALIGN_CENTER,0,1);
 
-	arr_right=lv_btn_create(fields);
-	lv_obj_add_event_cb(arr_right,mouse_scale_click,LV_EVENT_CLICKED,NULL);
-	lv_obj_set_grid_cell(arr_right,LV_GRID_ALIGN_END,2,1,LV_GRID_ALIGN_STRETCH,0,1);
-	lv_obj_t*lbl_arr_right=lv_label_create(arr_right);
-	lv_label_set_text(lbl_arr_right,LV_SYMBOL_RIGHT);
-	lv_obj_center(lbl_arr_right);
+	lv_draw_buttons_arg(
+		fields,
+		#define BTN(tgt,title,x)&(struct button_dsc){\
+			&tgt,true,title,mouse_scale_click,NULL,x,1,0,1,NULL\
+		}
+		BTN(arr_left,  LV_SYMBOL_LEFT,  0),
+		BTN(arr_right, LV_SYMBOL_RIGHT, 2),
+		#undef BTN
+		NULL
+	);
 
 	return 0;
 }
 
 static int mouse_get_focus(struct gui_activity*d __attribute__((unused))){
-	lv_group_add_obj(gui_grp,slider);
 	lv_group_add_obj(gui_grp,arr_left);
+	lv_group_add_obj(gui_grp,slider);
 	lv_group_add_obj(gui_grp,arr_right);
 	lv_group_focus_obj(slider);
 	return 0;
 }
 
 static int mouse_lost_focus(struct gui_activity*d __attribute__((unused))){
-	lv_group_remove_obj(slider);
 	lv_group_remove_obj(arr_left);
+	lv_group_remove_obj(slider);
 	lv_group_remove_obj(arr_right);
 	return 0;
 }

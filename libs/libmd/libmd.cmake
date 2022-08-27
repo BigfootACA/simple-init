3d4c6137251baa98f4ce995357b086fdb37a08b0
BigfootACA <bigfoot@classfun.cn>
2022-08-27 12:53:15 +0800

    libs: add libmd

---
+++ libs/libmd/libmd.cmake
@@ -0,0 +1,9 @@
+add_library(md STATIC
+	libs/libmd/src/md2.c
+	libs/libmd/src/md4.c
+	libs/libmd/src/md5.c
+	libs/libmd/src/rmd160.c
+	libs/libmd/src/sha1.c
+	libs/libmd/src/sha2.c
+)
+include_directories(libs/libmd/include)

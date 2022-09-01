ba52d8e5adeacdae4198d2a6d79a5bd080d25082
BigfootACA <bigfoot@classfun.cn>
2022-09-01 21:21:51 +0800

    gui: drivers: http.c: force use software render

--- src/gui/drivers/http.c
+++ src/gui/drivers/http.c
@@ -251,6 +251,9 @@ static int http_register(){
 	disp_drv.flush_cb=http_flush;
 	disp_drv.hor_res=state.ww;
 	disp_drv.ver_res=state.hh;
+	disp_drv.draw_ctx_init=lv_draw_sw_init_ctx;
+	disp_drv.draw_ctx_deinit=lv_draw_sw_init_ctx;
+	disp_drv.draw_ctx_size=sizeof(lv_draw_sw_ctx_t);
 	switch(gui_rotate){
 		case 0:break;
 		case 90:disp_drv.sw_rotate=1,disp_drv.rotated=LV_DISP_ROT_90;break;

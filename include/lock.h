7e571fb201f3da36e0ca1475d9816362ea28e0bc
BigfootACA <bigfoot@classfun.cn>
2022-09-04 15:12:18 +0800

    include: lock.h: use dumb function for fake lock

--- include/lock.h
+++ include/lock.h
@@ -18,10 +18,15 @@ typedef pthread_mutex_t mutex_t;
 #define MUTEX_DESTROY(lock) pthread_mutex_destroy(&(lock))
 #else
 typedef char mutex_t;
-#define MUTEX_INIT(lock) (void)lock
-#define MUTEX_LOCK(lock) (void)lock
-#define MUTEX_UNLOCK(lock) (void)lock
-#define MUTEX_TRYLOCK(lock) (void)lock
-#define MUTEX_DESTROY(lock) (void)lock
+static inline __attribute__((used)) int dumb_lock_init(mutex_t*lock){(void)lock;return 0;}
+static inline __attribute__((used)) int dumb_lock_lock(mutex_t*lock){(void)lock;return 0;}
+static inline __attribute__((used)) int dumb_lock_unlock(mutex_t*lock){(void)lock;return 0;}
+static inline __attribute__((used)) int dumb_lock_trylock(mutex_t*lock){(void)lock;return 0;}
+static inline __attribute__((used)) int dumb_lock_destroy(mutex_t*lock){(void)lock;return 0;}
+#define MUTEX_INIT(lock) dumb_lock_init(&(lock))
+#define MUTEX_LOCK(lock) dumb_lock_lock(&(lock))
+#define MUTEX_UNLOCK(lock) dumb_lock_unlock(&(lock))
+#define MUTEX_TRYLOCK(lock) dumb_lock_trylock(&(lock))
+#define MUTEX_DESTROY(lock) dumb_lock_destroy(&(lock))
 #endif
 #endif

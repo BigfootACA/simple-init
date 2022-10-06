d7de02e48d63e1e86669c681820b5b826b7f98f9
BigfootACA <bigfoot@classfun.cn>
2022-10-06 17:21:07 +0800

    main: uefimain.c: add splash

--- src/main/uefimain.c
+++ src/main/uefimain.c
@@ -20,34 +20,46 @@
 #include"logger.h"
 #include"setjmp.h"
 #include"language.h"
+#include"gui.h"
+#include"gui/splash.h"
 #define TAG "main"
 
 int main_retval=0;
 jmp_buf main_exit;
-extern int bootmenu_main(int argc,char**argv);
+extern int bootmenu_draw();
 
-// simple-init uefi entry point
-EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ih,IN EFI_SYSTEM_TABLE*st){
-	REPORT_STATUS_CODE(EFI_PROGRESS_CODE,(EFI_SOFTWARE_DXE_BS_DRIVER|EFI_SW_PC_USER_SETUP));
+static void show_bootmenu(void*d __attribute__((unused))){
+	bootmenu_draw();
+}
 
-	// init uefi boot manager
-	EfiBootManagerConnectAll();
-	EfiBootManagerRefreshAllBootOption();
-	DEBUG((EFI_D_INFO,"Initialize SimpleInit GUI...\n"));
-
-	tlog_notice("initialize simple-init");
-	tlog_debug("simple init main arguments: %p %p",ih,st);
-	tlog_debug("simple init main entry point at %p",&UefiMain);
+static int post_main(){
+	int r;
 	logger_set_console(PcdGetBool(PcdLoggerdUseConsole));
 	confd_init();
 	logger_init();
+	tlog_notice("initialize simple-init");
+	tlog_debug("simple init main entry point at %p",&UefiMain);
+	if((r=gui_pre_init())!=0)return r;
+	if((r=gui_screen_init())!=0)return r;
+	gui_splash_draw();
+	lv_task_handler();
+
+	gui_splash_set_text(true,_("Initializing config store..."));
 	confd_dump(LEVEL_VERBOSE);
+
+	gui_splash_set_text(true,_("Loading extra UEFI Drivers..."));
 	boot_load_drivers();
+
+	gui_splash_set_text(true,_("Initializing I18N locale..."));
 	char*lang=confd_get_string("language",NULL);
 	if(lang)lang_set(lang);
 	lang_init_locale();
 
+	gui_splash_set_text(true,_("Initializing Boot Manager..."));
+	boot_init_configs();
+
 	#ifdef ENABLE_LUA
+	gui_splash_set_text(true,_("Initializing LUA Framework..."));
 	lua_State*L=xlua_init();
 	if(L){
 		xlua_run_confd(L,TAG,"lua.on_post_startup");
@@ -55,10 +67,24 @@ EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ih,IN EFI_SYSTEM_TABLE*st){
 	}
 	#endif
 
+	gui_splash_set_text(true,_("Starting boot menu..."));
+	gui_splash_exit(true,show_bootmenu);
+	if((r=gui_main())!=0)return r;
+	return 0;
+}
+
+// simple-init uefi entry point
+EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ih,IN EFI_SYSTEM_TABLE*st){
+	REPORT_STATUS_CODE(EFI_PROGRESS_CODE,(EFI_SOFTWARE_DXE_BS_DRIVER|EFI_SW_PC_USER_SETUP));
+
+	EfiBootManagerConnectAll();
+	EfiBootManagerRefreshAllBootOption();
+	DEBUG((EFI_D_INFO,"Initialize SimpleInit GUI...\n"));
+
 	errno=0;
 	main_retval=0;
 	if(setjmp(main_exit)==0)
-		main_retval=bootmenu_main(1,(char*[]){"bootmenu",NULL});
+		main_retval=post_main();
 	return main_retval;
 }
 #endif
